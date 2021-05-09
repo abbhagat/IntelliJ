0dd3b685c2b4e2cd663d3d723d5113d7b7514962
abbhagat <abhinaw.kumar7@gmail.com>
2021-05-09 20:44:38 +0530

    Interview

--- Algorithm/src/arrays/ArrayUnionIntersection.java
+++ Algorithm/src/arrays/ArrayUnionIntersection.java
@@ -6,22 +6,22 @@ import java.util.Map;
 public class ArrayUnionIntersection {
 
     public static void main(String[] args) {
-        Map<Integer, Integer> hm = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         int a[] = {12, 55, 60, 72, 89, 35, 35, 46, 12, 46, 100};
         int b[] = {12, 15, 30, 23, 10, 60, 35, 89, 92, 100, 10, 23, 45, 60};
         for (int i = 0; i < b.length; i++) {
-            hm.put(b[i], 1);
+            map.put(b[i], 1);
         }
         System.out.println("Array Intersection");
         for (int i = 0; i < a.length; i++) {
-            if (hm.get(a[i]) != null) {
+            if (map.get(a[i]) != null) {
                 System.out.print(a[i] + " ");
             }
         }
         System.out.println("\nArray Union");
         for (int i = 0; i < a.length; i++) {
-            hm.put(a[i], 1);
+            map.put(a[i], 1);
         }
-        hm.keySet().forEach(System.out::println);
+        map.keySet().forEach(System.out::println);
     }
 }
--- Algorithm/src/strings/StringAnagram.java
+++ Algorithm/src/strings/StringAnagram.java
@@ -3,12 +3,10 @@ package strings;
 public class StringAnagram {
 
     public static void main(String[] args) {
-
         String s1 = "Java";
         String s2 = "vaJa";
-
-        int[] temp = new int[128];
         boolean isAnagram = true;
+        int[] temp = new int[128];
         if (s1.length() == s2.length()) {
             for (int i = 0; i < s1.length(); i++) {
                 temp[s1.charAt(i)]++;
