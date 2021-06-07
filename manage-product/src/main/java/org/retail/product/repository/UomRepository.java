6d6953ae5e7dbf094ff695a968ea5bad82417288
abbhagat <39695698+abbhagat@users.noreply.github.com>
2021-06-07 09:44:09 +0530

    Delete manage-product directory

--- manage-product/src/main/java/org/retail/product/repository/UomRepository.java
+++
@@ -1,9 +0,0 @@
-package org.retail.product.repository;
-
-import org.retail.product.model.uom.Uom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UomRepository extends JpaRepository<Uom, Long> {
-}
