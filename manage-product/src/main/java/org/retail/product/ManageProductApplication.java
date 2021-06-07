1a6b830db5349d1a3bdf5ac5cfa9afbe16bedb3b
abbhagat <abhinaw.kumar7@gmail.com>
2021-06-07 09:50:41 +0530

    Interview

---
+++ manage-product/src/main/java/org/retail/product/ManageProductApplication.java
@@ -0,0 +1,13 @@
+package org.retail.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageProductApplication.class, args);
+    }
+
+}
