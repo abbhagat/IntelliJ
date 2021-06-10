158f8e8ca5cc28ace60d8531abd65c08a2d52f05
abbhagat <abhinaw.kumar7@gmail.com>
2021-06-10 07:30:22 +0530

    Interview

--- Algorithm/src/arrays/ClosestSumPairInAnArray.java
+++ Algorithm/src/arrays/ClosestSumPairInAnArray.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class ClosestSumPairInAnArray {
 
     public static void main(String[] args) {
-        int[] a = {3, 4, 5};
+        int[] a = {10, 22, 28, 29, 30, 40};
         Arrays.sort(a);
-        int i = 0, j = a.length - 1, x = 0, y = 0, n = 8;
+        int i = 0, j = a.length - 1, x = 0, y = 0, n = 54;
         int diff = Integer.MAX_VALUE;
         while (i < j) {
             int sum = Math.abs(a[i] + a[j] - n);
