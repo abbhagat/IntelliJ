92de99b84b92567386f6e5149f88b11d962d36a9
abbhagat <abhinaw.kumar7@gmail.com>
2021-06-11 20:51:08 +0530

    Interview

---
+++ Algorithm/src/geeks/MinFlipToMakeBinaryStringSame.java
@@ -0,0 +1,39 @@
+package geeks;
+
+/**
+ * Input: A = “10010010”, B = “00001000”
+ * Output: 3
+ * Explanation:
+ * Operation 1: Flipping A[0] modifies A to “00010010”.
+ * Operation 2: Flipping A[6] modifies A to “00010000”.
+ * Operation 3: Swapping A[3] and A[4] modifies A to “00001000”
+ * Therefore, the total number of operations is 3.
+ * <p>
+ * Input: A = “11”, B = “00”
+ * Output: 3
+ */
+public class MinFlipToMakeBinaryStringSame {
+
+    private static int minimumOperation(char[] a, char[] b) {
+        int minOperation = 0;
+        for (int i = 0; i < a.length; ) {
+            if (a[i] == b[i]) {
+                i++;
+                continue;
+            } else if (a[i] == b[i + 1] && a[i + 1] == b[i] && i < a.length) {
+                minOperation++;
+                i = i + 2;
+            } else if (a[i] != b[i]) {
+                minOperation++;
+                i++;
+            } else {
+                i++;
+            }
+        }
+        return minOperation;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minimumOperation("10010010".toCharArray(), "00001000".toCharArray()));
+    }
+}
