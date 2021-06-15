1c3d39b376b2abbc827e294a94c1e01cd4964085
abbhagat <abhinaw.kumar7@gmail.com>
2021-06-15 11:56:51 +0530

    Interview

---
+++ Algorithm/src/geeks/MoveAllPositiveToEndAndNegativeToBeginning.java
@@ -0,0 +1,35 @@
+package geeks;
+
+import java.util.stream.IntStream;
+
+public class MoveAllPositiveToEndAndNegativeToBeginning {
+
+    private static int[] a = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
+
+    private static void move() {
+        int low = 0, high = a.length - 1;
+        while (low <= high) {
+            if (a[low] < 0 && a[high] < 0) {
+                low++;
+            } else if (a[low] > 0 && a[high] > 0) {
+                high--;
+            } else if (a[low] > 0 && a[high] < 0) {
+                int t = a[low];
+                a[low] = a[high];
+                a[high] = t;
+                low++;
+                high--;
+            } else {
+                low++;
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        IntStream.range(0, a.length).forEach(i -> System.out.print(a[i] + " "));
+        move();
+        System.out.println();
+        IntStream.range(0, a.length).forEach(i -> System.out.print(a[i] + " "));
+    }
+}
--- Algorithm/src/recursion/MinStepsToReachDestination.java
+++ Algorithm/src/recursion/MinStepsToReachDestination.java
@@ -25,7 +25,7 @@ public class MinStepsToReachDestination {
     /*    Function to count number of steps required to reach a destination
           source -> source vertex
           step -> value of last step taken
-         dest -> destination vertex
+          dest -> destination vertex
     */
     private static int steps(int source, int step, int dest) {
         if (Math.abs(source) > dest) {  // base cases
