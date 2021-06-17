99c61db93ff69f9634f6948e8c8581601fb2b1c7
abbhagat <abhinaw.kumar7@gmail.com>
2021-06-17 12:16:12 +0530

    Interview

--- Java/src/string/SwitchDemo.java
+++ Java/src/string/SwitchDemo.java
@@ -9,13 +9,15 @@ public abstract class SwitchDemo {
     public static void main(String[] args) {
         final int y = 10;
         switch (y) {
-            default:
-                System.out.println("Default");
             case 1:
                 System.out.println("Case 1");
             case 2:
                 System.out.println("Case 2");
                 break;
+           case 10:
+                System.out.println("Case 10");
+            default:
+                System.out.println("Default");
         }
     }
 }
