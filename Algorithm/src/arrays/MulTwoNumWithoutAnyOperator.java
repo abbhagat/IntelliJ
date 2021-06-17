0110143c45ce8eeb9a3f205edb71f8ebad3925b3
abbhagat <abhinaw.kumar7@gmail.com>
2021-06-17 14:58:31 +0530

    Interview

--- Algorithm/src/arrays/MulTwoNumWithoutAnyOperator.java
+++ Algorithm/src/arrays/MulTwoNumWithoutAnyOperator.java
@@ -2,26 +2,11 @@ package arrays;
 
 public class MulTwoNumWithoutAnyOperator {
 
-    private static int mul(int sum, int x, int y, int itr) {
-        if (itr == 0) {
-            return sum;
-        } else {
-            while (y != 0) {
-                int carry = (x & y) << 1;
-                x = x ^ y;
-                y = carry;
-            }
-            return (mul(x, x, x, itr - 2));
-        }
-    }
-
     private static int add(int x, int y) {
         return y == 0 ? x : add(x ^ y, (x & y) << 1);
     }
 
-
     public static void main(String[] args) {
-        System.out.println(mul(0, 15, 15, 6));
         int x = 3;
         int y = 15;
         int mul = 0;
@@ -30,5 +15,4 @@ public class MulTwoNumWithoutAnyOperator {
         }
         System.out.println(mul);
     }
-
 }
