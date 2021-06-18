d652c23d444d96ed98ba972e8e89af04367571bd
abbhagat <abhinaw.kumar7@gmail.com>
2021-06-18 15:01:14 +0530

    Interview

--- Java/src/except/OverrideException.java
+++ Java/src/except/OverrideException.java
@@ -27,7 +27,9 @@ public class OverrideException {
     public static void main(String[] args) throws IOException {
         Super s = new Sub();
         System.out.println(s.x);
-        System.out.println(s.getInstance(s, "String").x); // s.getInstance(s, "String") -> returns an instanceof Super
+//        Sub s1 = s.getInstance(s, "String"); // Error
+        Super s1 = s.getInstance(s, "String");
+        System.out.println(s1.x); // s.getInstance(s, "String") -> returns an instanceof Super
         Sub sub = new Sub();
         System.out.println(sub instanceof Super);
         System.out.println(sub.x);
