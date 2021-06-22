c6097b88061434d5d3e33560e6517d18939b3bd1
abbhagat <abhinaw.kumar7@gmail.com>
2021-06-22 08:05:05 +0530

    Interview

--- Hibernate/src/main/java/callingProcedure/Main.java
+++ Hibernate/src/main/java/callingProcedure/Main.java
@@ -13,10 +13,20 @@ public class Main {
     public static void main(String[] args) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
+
         Query query = session.getNamedNativeQuery("userDetailsProcedure");
         query.setParameter("userId", 1);
         query.setParameter("userName", "V705417");
         List<UserDetails> userDetailsList = query.getResultList();
+
+        Query query1 = session.createSQLQuery("CALL PROCEDURE_NAME(:userId,:userName)").addEntity(UserDetails.class);
+        query1.setParameter("userId", 1);
+        query1.setParameter("userName", "V705417");
+        List<UserDetails> userDetailsList1 = query1.getResultList();
+
+        transaction.commit();
+        session.close();
         userDetailsList.forEach(userDetails -> System.out.println(userDetails.getUserId() + "\t" + userDetails.getName()));
+        userDetailsList1.forEach(userDetails -> System.out.println(userDetails.getUserId() + "\t" + userDetails.getName()));
     }
 }
