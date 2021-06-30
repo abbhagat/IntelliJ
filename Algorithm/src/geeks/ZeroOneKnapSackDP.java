c7b4055b37d7478b1802ff154610aef20188fb4b
abbhagat <abhinaw.kumar7@gmail.com>
2021-06-30 22:58:39 +0530

    Interview

--- Algorithm/src/geeks/ZeroOneKnapSackDP.java
+++ Algorithm/src/geeks/ZeroOneKnapSackDP.java
@@ -66,19 +66,19 @@ import static java.lang.Math.max;
 public class ZeroOneKnapSackDP {
 
     private static int knapSack(int W, int wt[], int val[], int n) {
-        int K[][] = new int[n + 1][W + 1];
+        int DP[][] = new int[n + 1][W + 1];
         for (int i = 0; i <= n; i++) {
-            for (int w = 0; w <= W; w++) {
-                if (i == 0 || w == 0) {
-                    K[i][w] = 0;
-                } else if (wt[i - 1] <= w) {
-                    K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+            for (int j = 0; j <= W; j++) {
+                if (i == 0 || j == 0) {
+                    DP[i][j] = 0;
+                } else if (wt[i - 1] <= j) {
+                    DP[i][j] = max(val[i - 1] + DP[i - 1][j - wt[i - 1]], DP[i - 1][j]);
                 } else {
-                    K[i][w] = K[i - 1][w];
+                    DP[i][j] = DP[i - 1][j];
                 }
             }
         }
-        return K[n][W];
+        return DP[n][W];
     }
 
     public static void main(String args[]) {
