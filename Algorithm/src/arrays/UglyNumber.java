b38c3643d0ff0176e1a6fd3e339fd8890a9cde0b
abbhagat <abhinaw.kumar7@gmail.com>
2021-07-04 11:51:28 +0530

    Interview

--- Algorithm/src/arrays/UglyNumber.java
+++ Algorithm/src/arrays/UglyNumber.java
@@ -2,14 +2,14 @@ package arrays;
 
 public class UglyNumber {
 
-    static boolean isUgly(int n) {
+    private static boolean isUgly(int n) {
         n = maxDivide(n, 2);
         n = maxDivide(n, 3);
         n = maxDivide(n, 5);
-        return (n == 1) ? true : false;
+        return n == 1 ? true : false;
     }
 
-    static int maxDivide(int a, int b) {
+    private static int maxDivide(int a, int b) {
         while (a % b == 0) {
             a /= b;
         }
@@ -17,17 +17,9 @@ public class UglyNumber {
     }
 
     public static void main(String[] args) {
-        int count = 0;
-        int i = 1;
-        while (true) {
-            if (isUgly(i)) {
-                count++;
-            }
-            if (count == 7) {
-                System.out.println(i);
-                break;
-            }
-            i++;
+        int[] a = new int[]{6, 8, 14};
+        for (int x : a) {
+            System.out.println(isUgly(x) ? x + "Ugly" : x + "Not Ugly");
         }
     }
 }
---
+++ Algorithm/src/leetcode/RemoveInValidBracket.java
@@ -0,0 +1,69 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class RemoveInValidBracket {
+
+    private static void removeInvalidParenthesis(String str) {
+        if (str.isEmpty()) {
+            return;
+        }
+        Set<String> set = new HashSet<>();              // set set to ignore already visited string
+        Queue<String> queue = new LinkedList<>();      // queue to maintain BFS
+        boolean level = false;
+        queue.add(str);                              // pushing given string as starting node into queue
+        set.add(str);
+
+        while (!queue.isEmpty()) {
+            str = queue.poll();
+            if (isValidString(str)) {
+                System.out.println(str);
+                level = true;                     // If answer is found, make level true so that valid string of only that level are processed.
+            }
+            if (level) {
+                continue;
+            }
+            for (int i = 0; i < str.length(); i++) {
+                if (!isParenthesis(str.charAt(i))) {
+                    continue;
+                }
+                String temp = str.substring(0, i) + str.substring(i + 1);  // Removing parenthesis from str and pushing into queue,if not visited already
+                if (!set.contains(temp)) {
+                    queue.add(temp);
+                    set.add(temp);
+                }
+            }
+        }
+    }
+
+    private static boolean isParenthesis(char c) {  // method checks if character is parenthesis(open or closed)
+        return ((c == '(') || (c == ')'));
+    }
+
+
+    private static boolean isValidString(String str) {   // method returns true if string contains valid parenthesis
+        int count = 0;
+        for (char x : str.toCharArray()) {
+            if (x == '(') {
+                count++;
+            } else if (x == ')') {
+                count--;
+            }
+            if (count < 0) {
+                return false;
+            }
+        }
+        return count == 0;
+    }
+
+    public static void main(String[] args) {
+        String expression = "()())()";
+        removeInvalidParenthesis(expression);
+        expression = "()v)";
+        removeInvalidParenthesis(expression);
+    }
+}
+
