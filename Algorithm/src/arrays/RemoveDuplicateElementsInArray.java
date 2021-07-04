60d3ef95bb1e18e67b841a4410e90656992cfc04
abbhagat <abhinaw.kumar7@gmail.com>
2021-07-04 12:24:45 +0530

    Interview

--- Algorithm/src/arrays/RemoveDuplicateElementsInArray.java
+++ Algorithm/src/arrays/RemoveDuplicateElementsInArray.java
@@ -1,39 +1,36 @@
 package arrays;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.IntStream;
-
 /*
- * Soln 1 :  using a hashmap
- * Soln 2 :  using a BST
+ * Soln 1 :  using nums hashmap
+ * Soln 2 :  using nums BST
  * Soln 3 :  O(n^2)
  * Soln 4 :  using sorting
  *
  */
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class RemoveDuplicateElementsInArray {
 
     public static void main(String[] args) {
-
-        int[] a = {1, 2, 3, 4, 1, 3, 2, 3, 4, 5, 6, 1, 2, 8, 9, 11, 10, 1, 8, 9, 10, 7, 7, 11};
-        Set<Integer> hs = new HashSet<>();
-        Arrays.sort(a); // a = {1 1 1 1 2 2 2 3 3 3 4 4 5 6 7 7 8 8 9 9 10 10 11 11}
-        IntStream.range(0, a.length).forEach(i -> System.out.print(a[i] + " "));
-        System.out.println();
-        int i;
-        for (i = 1; i < a.length; i++) {
-            if (a[i - 1] == a[i]) {
-                hs.add(a[i]);
-            } else {
-                System.out.print(a[i - 1] + " ");
+        int[] nums = {1, 2, 2, 3};
+        Arrays.sort(nums);
+        if (nums.length > 0) {
+            int i, j = 0;
+            for (i = 1; i < nums.length; i++) {
+                if (nums[i - 1] != nums[i]) {
+                    nums[j++] = nums[i - 1];
+                }
             }
+            i--;
+            nums[j++] = nums[i];
+            IntStream.range(0, j).forEach(k -> System.out.print(nums[k] + " "));
+            System.out.println();
+            System.out.println(j);
+        } else {
+            System.out.println(0);
         }
-        i--;
-        System.out.println(a[i]);
-        System.out.println();
-        System.out.print("Repeated elemnts in the array are : ");
-        System.out.println(hs);
+
     }
 }
