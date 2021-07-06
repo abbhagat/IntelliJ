dc69fd3ec4adfd46020c3a7dad04e578b23f578d
abbhagat <abhinaw.kumar7@gmail.com>
2021-07-06 23:41:24 +0530

    Interview

--- Algorithm/src/geeks/LongestPalindromicString.java
+++ Algorithm/src/geeks/LongestPalindromicString.java
@@ -17,11 +17,11 @@ public class LongestPalindromicString {
         int n = str.length();
         boolean table[][] = new boolean[n][n];
         int maxLength = 1;  // All substrings of length 1 are palindromes
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             table[i][i] = true;
         }
-        int start = 0;  // check for sub-string of length 2.
-        for (int i = 0; i < n - 1; ++i) {
+        int start = 0;  // check for sub-string of length 2
+        for (int i = 0; i < n - 1; i++) {
             if (str.charAt(i) == str.charAt(i + 1)) {
                 table[i][i + 1] = true;
                 start = i;
@@ -29,10 +29,10 @@ public class LongestPalindromicString {
             }
         }
 
-        for (int k = 3; k <= n; ++k) {  // Check for lengths greater than 2  is length of substring
-            for (int i = 0; i < n - k + 1; ++i) {              // Fix the starting index
+        for (int k = 3; k <= n; k++) {                          // Check for lengths greater than 2  is length of substring
+            for (int i = 0; i < n - k + 1; i++) {              // Fix the starting index
                 int j = i + k - 1;                            // Get the ending index of substring from starting index i and length k
-                // checking for sub-string from ith index to index iff str.charAt(i+1) to str.charAt(j-1) is a palindrome
+                // checking for sub-string from ith index to index if str.charAt(i+1) to str.charAt(j-1) is a palindrome
                 if (table[i + 1][j - 1] && str.charAt(i) == str.charAt(j)) {
                     table[i][j] = true;
                     if (k > maxLength) {
@@ -42,12 +42,12 @@ public class LongestPalindromicString {
                 }
             }
         }
-        System.out.print("Longest palindrome substring is : " + str.substring(start, start + maxLength - 1));
+        System.out.print("Longest palindrome substring is : " + str.substring(start, start + maxLength));
         return maxLength;
     }
 
     public static void main(String[] args) {
-        String str = "forgeeksskeegfor";
+        String str = "babad";
         System.out.println("\nLength is: " + longestPalSubstr(str));
     }
 }
