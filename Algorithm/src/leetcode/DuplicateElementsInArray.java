4509d160271107a542acc6b3307445566c2599d1
abbhagat <abhinaw.kumar7@gmail.com>
2021-07-07 12:36:22 +0530

    Interview

---
+++ Algorithm/src/leetcode/DuplicateElementsInArray.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * Approach: The basic idea is to use a HashMap to solve the problem. But there is a catch, the numbers in the array are from 0 to n-1,
+ * and the input array has length n. So, the input array can be used as a HashMap. While Traversing the array, if an element ‘a’
+ * is encountered then increase the value of a%n‘th element by n. The frequency can be retrieved by dividing the a % n’th element by n.
+ * Algorithm:
+ * Traverse the given array from start to end.
+ * For every element in the array increment the arr[i]%n‘th element by n.
+ * Now traverse the array again and print all those indexes i for which arr[i]/n is greater than 1.
+ * Which guarantees that the number n has been added to that index
+ * This approach works because all elements are in the range from 0 to n-1 and arr[i]
+ * would be greater than n only if a value “i” has appeared more than once.
+ */
+
+public class DuplicateElementsInArray {
+
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 2, 3, 4};
+        for (int i = 0; i < a.length; i++) {
+            int j = a[i] % a.length;
+            a[j] += a.length;
+        }
+        System.out.println("The repeating elements are : ");
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] >= a.length * 2) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
