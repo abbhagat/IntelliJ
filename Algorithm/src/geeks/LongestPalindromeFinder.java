92361b478ed87e82ffd142a6fc3730e129293fec
abbhagat <abhinaw.kumar7@gmail.com>
2021-07-11 23:31:15 +0530

    Interview

---
+++ Algorithm/src/geeks/LongestPalindromeFinder.java
@@ -0,0 +1,37 @@
+package geeks;
+
+public class LongestPalindromeFinder {
+
+    public static String intermediatePalindrome(String s, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
+    }
+
+    public static String longestPalindromeString(String s) {
+        if (s == null) {
+            return null;
+        }
+        String longest = s.substring(0, 1);
+        for (int i = 0; i < s.length() - 1; i++) {
+            String palindrome = intermediatePalindrome(s, i, i);  //odd cases like 121
+            if (palindrome.length() > longest.length()) {
+                longest = palindrome;
+            }
+            palindrome = intermediatePalindrome(s, i, i + 1);  //even cases like 1221
+            if (palindrome.length() > longest.length()) {
+                longest = palindrome;
+            }
+        }
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindromeString("babad"));
+    }
+}
