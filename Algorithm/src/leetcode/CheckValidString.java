713e5ea6642ffce3dfb9b40ecb1c9e79863381f8
abbhagat <abhinaw.kumar7@gmail.com>
2021-07-12 23:38:13 +0530

    Interview

---
+++ Algorithm/src/leetcode/CheckValidString.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class CheckValidString {
+
+    public static boolean checkValidString(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char x : s.toCharArray()) {
+            if (x == '(' || x == '*') {
+                stack.push(x);
+            } else {
+                if (!stack.isEmpty()) {
+                    char c = stack.pop();
+                    if (c == '*' || c == '(') {
+                        continue;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (stack.isEmpty()) {
+            return true;
+        }
+        while (!stack.isEmpty()) {
+            if (stack.pop() != '*') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "(*)";
+        System.out.println(checkValidString(s));
+    }
+}
---
+++ Algorithm/src/leetcode/FormLargestNumberFromGivenArray.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+/*
+A simple solution that comes to our mind is to sort all numbers in descending order, but simply sorting doesn’t work.
+For example, 548 is greater than 60, but in output 60 comes before 548.
+As a second example, 98 is greater than 9, but 9 comes before 98 in output.
+
+So how do we go about it? The idea is to use any comparison based sorting algorithm.
+In the used sorting algorithm, instead of using the default comparison,
+write a comparison function myCompare() and use it to sort numbers.
+
+Given two numbers X and Y, how should myCompare() decide which number to put first –
+we compare two numbers XY (Y appended at the end of X) and YX (X appended at the end of Y).
+If XY is larger, then X should come before Y in output, else Y should come before.
+For example, let X and Y be 542 and 60.
+To compare X and Y, we compare 54260 and 60542.
+Since 60542 is greater than 54260, we put Y first.
+
+Following is the implementation of the above approach.
+To keep the code simple, numbers are considered as strings, the vector is used instead of a normal array.
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FormLargestNumberFromGivenArray {
+
+    public static String largestNumber(int[] nums) {
+        List<String> list = new ArrayList<>();
+        int countZero = 0;
+        for (int x : nums) {
+            list.add(Integer.toString(x));
+            if(x == 0){
+                countZero++;
+            }
+        }
+        if(countZero == nums.length){
+            return "0";
+        }
+        Collections.sort(list, (x, y) -> {
+            String xy = x + y;
+            String yx = y + x;
+            return yx.compareTo(xy);
+        });
+        String result = "";
+        for (String s : list) {
+            result += s;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(largestNumber(new int[]{3, 30, 34, 5, 9}));
+    }
+}
