d432e2a986dc04dc0e040972af032457207f51b2
abbhagat <abhinaw.kumar7@gmail.com>
2021-07-25 14:38:35 +0530

    Interview

--- Algorithm/src/stack/MinBracketRemovalToMakeBracketBalanced.java
+++ Algorithm/src/stack/MinBracketRemovalToMakeBracketBalanced.java
@@ -15,25 +15,21 @@ The balanced expression is "{{{}}{}}"
 public class MinBracketRemovalToMakeBracketBalanced {
 
     private static int countMinReversals(char[] exp) {
-        int len = exp.length;                                  // length of expression must be even to make it balanced by using reversals.
-        if (len % 2 != 0) {
+        if (exp.length % 2 != 0) {  // length of expression must be even to make it balanced by using reversals.
             return -1;
         }
         int open = 0, close = 0;
-        for (int i = 0; i < len; i++) {
-            if (exp[i] == '{') {                               // If current bracket is open then increment open count.
+        for (char c : exp) {
+            if (c == '{') {                               // If current bracket is open then increment open count.
                 open++;
             } else {                                         // If current bracket is close, check if it balances opening bracket
                 int j = open == 0 ? close++ : open--;      // If yes then decrement count of unbalanced opening bracket
             }                                             // If no increment count of closing bracket.
         }
-        int result = open / 2 + close / 2;
-        close %= 2;    // For the case: "}{" or when one closing and one opening bracket remains for pairing, then both need to be reversed.
-        result += close != 0 ? 2 : 0;
-        return result;
+        return (int)(Math.ceil((0.0 + open) / 2) + Math.ceil((0.0 + close) / 2));
     }
 
     public static void main(String args[]) {
-        System.out.println(countMinReversals("}{{}}{{{".toCharArray()));
+        System.out.println(countMinReversals("}{".toCharArray()));
     }
 }
