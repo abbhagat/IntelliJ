f17b56f975524a0ab4ac72bbbd3e2512a814efec
abbhagat <abhinaw.kumar7@gmail.com>
2021-07-25 09:53:00 +0530

    Interview

--- Algorithm/src/arrays/SumOfAllConsecutiveNumInString.java
+++ Algorithm/src/arrays/SumOfAllConsecutiveNumInString.java
@@ -2,14 +2,14 @@ package arrays;
 
 public class SumOfAllConsecutiveNumInString {
     public static void main(String[] args) {
-        String s = "11aa220bb33dd44";
-        int n = 0, sum = 0;
-        for (char x : s.toCharArray()) {
-            if (x >= 48 && x <= 57) {
-                n = n * 10 + x - 48;
+        char[] c = "11aa22bb33dd44".toCharArray();
+        int j = 0, sum = 0;
+        for (int i = c.length - 1; i >= 0; i--) {
+            if (c[i] >= 48 && c[i] <= 57) {
+                sum += (c[i] - 48) * Math.pow(10, j);
+                j++;
             } else {
-                sum += n;
-                n = 0;
+                j = 0;
             }
         }
         System.out.println(sum);
