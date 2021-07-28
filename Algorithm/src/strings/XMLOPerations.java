9bd10aba8856dba1f51658e1cb88ab092cfdb438
abbhagat <abhinaw.kumar7@gmail.com>
2021-07-28 22:29:43 +0530

    Interview

--- Algorithm/src/strings/XMLOPerations.java
+++ Algorithm/src/strings/XMLOPerations.java
@@ -10,11 +10,12 @@ public class XMLOPerations {
             + "<requestqueue>emailrequest</requestqueue>"
             + "<responsequeue>emailresponse</responsequeue>"
             + "<ns3:encodedKey>~1234-ACD~124~</ns3:encodedKey>"
+            + "<ns3:encodedKey>acd~</ns3:encodedKey>"
             + "</Request>"
             + "<Request name=\"CleanEmail\">"
             + "<requestqueue>Cleanrequest</requestqueue>"
             + "<responsequeue>Cleanresponse</responsequeue>"
-            + "<ns3:encodedKey>~1234-ACD~124~</ns3:encodedKey>"
+            + "<ns3:encodedKey>1234</ns3:encodedKey>"
             + " </Request>"
             + "</config>";
 
