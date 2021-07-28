f6ebc917e4c2b4af14187ef2b0a35365f10fc9a7
abbhagat <abhinaw.kumar7@gmail.com>
2021-07-28 22:27:52 +0530

    Interview

---
+++ Algorithm/src/strings/XMLOPerations.java
@@ -0,0 +1,28 @@
+package strings;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class XMLOPerations {
+    static String xml = "<?xml version=\"1.0\"?>"
+            + "<config>"
+            + "<Request name=\"ValidateEmailRequest\">"
+            + "<requestqueue>emailrequest</requestqueue>"
+            + "<responsequeue>emailresponse</responsequeue>"
+            + "<ns3:encodedKey>~1234-ACD~124~</ns3:encodedKey>"
+            + "</Request>"
+            + "<Request name=\"CleanEmail\">"
+            + "<requestqueue>Cleanrequest</requestqueue>"
+            + "<responsequeue>Cleanresponse</responsequeue>"
+            + "<ns3:encodedKey>~1234-ACD~124~</ns3:encodedKey>"
+            + " </Request>"
+            + "</config>";
+
+    public static void main(String argv[]) {
+        String patternString = "(encodedKey>)(.*)(<.*encodedKey>)";
+        Pattern pattern = Pattern.compile(patternString);
+        xml = xml.replaceAll("><",">\n<");
+        Matcher matcher = pattern.matcher(xml);
+        System.out.println(matcher.replaceAll("XXX"));
+    }
+}
