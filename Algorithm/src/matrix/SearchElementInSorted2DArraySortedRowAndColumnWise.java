df5720f3ed4ca66c5e986aef8087bac86402db9a
abbhagat <abhinaw.kumar7@gmail.com>
2021-08-01 11:16:14 +0530

    Interview

--- Algorithm/src/matrix/SearchElementInSorted2DArraySortedRowAndColumnWise.java
+++ Algorithm/src/matrix/SearchElementInSorted2DArraySortedRowAndColumnWise.java
@@ -17,7 +17,7 @@ public class SearchElementInSorted2DArraySortedRowAndColumnWise {
     static boolean search(int[][] a, int key) {
         int R1 = 0;
         int C1 = 0;
-        int R2 = a[0].length - 1;
+        int R2 = a.length - 1;
         int C2 = a[0].length - 1;
         while (R1 <= R2 && C1 <= C2) {
             if (key < a[R1][C2]) {
@@ -35,5 +35,6 @@ public class SearchElementInSorted2DArraySortedRowAndColumnWise {
     public static void main(String[] args) {
         int[][] a = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
         System.out.println(search(a, 5) ? "Present" : "Not Present");
+        System.out.println(search(new int[][]{{1,1}}, 5) ? "Present" : "Not Present");
     }
 }
