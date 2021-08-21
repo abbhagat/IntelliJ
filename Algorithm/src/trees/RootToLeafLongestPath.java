635304a2aa8bc824681989b8204642bc96a4bbe9
abbhagat <abhinaw.kumar7@gmail.com>
2021-08-21 15:20:22 +0530

    Interview

--- Algorithm/src/trees/RootToLeafLongestPath.java
+++ Algorithm/src/trees/RootToLeafLongestPath.java
@@ -1,20 +1,20 @@
 package trees;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static trees.CreateBST.createBST;
 
 public class RootToLeafLongestPath {
 
-    public static ArrayList<Integer> longestPath(Node root) {
-        if (root == null) {   // If root is null means there is no binary tree so return a empty vector
+    public static List<Integer> longestPath(Node root) {
+        if (root == null) {                                  // If root is null means there is no binary tree so return a empty list
             return new ArrayList<>();
         }
-        ArrayList<Integer> left = longestPath(root.left);      // Recursive call on root.left
-        ArrayList<Integer> right = longestPath(root.right);   // Recursive call on root.right
-        // Compare the size of the two ArrayList and insert current node accordingly
-        boolean b = right.size() < left.size() ? left.add(root.num) : right.add(root.num);
-        return (left.size() > right.size() ? left : right);  // Return the appropriate ArrayList
+        List<Integer> left = longestPath(root.left);      // Recursive call on root.left
+        List<Integer> right = longestPath(root.right);   // Recursive call on root.right
+        boolean b = right.size() < left.size() ? left.add(root.num) : right.add(root.num); // Compare the size of the two ArrayList and insert current node accordingly
+        return left.size() > right.size() ? left : right;  // Return the appropriate ArrayList
     }
 
     public static void main(String[] args) {
