5336c1b1e90b0deddf7feb7299b6cc26662c2bc6
abbhagat <abhinaw.kumar7@gmail.com>
2021-08-21 15:08:22 +0530

    Interview

---
+++ Algorithm/src/trees/RootToLeafLongestPath.java
@@ -0,0 +1,28 @@
+package trees;
+
+import java.util.ArrayList;
+
+import static trees.CreateBST.createBST;
+
+public class RootToLeafLongestPath {
+
+    public static ArrayList<Integer> longestPath(Node root) {
+        if (root == null) {   // If root is null means there is no binary tree so return a empty vector
+            return new ArrayList<>();
+        }
+        ArrayList<Integer> left = longestPath(root.left);      // Recursive call on root.left
+        ArrayList<Integer> right = longestPath(root.right);   // Recursive call on root.right
+        // Compare the size of the two ArrayList and insert current node accordingly
+        boolean b = right.size() < left.size() ? left.add(root.num) : right.add(root.num);
+        return (left.size() > right.size() ? left : right);  // Return the appropriate ArrayList
+    }
+
+    public static void main(String[] args) {
+        int[] a = {20, 10, 30, 5};
+        Node root = null;
+        for (int i = 0; i < a.length; i++) {
+            root = createBST(root, a[i]);
+        }
+        System.out.println(longestPath(root));
+    }
+}
