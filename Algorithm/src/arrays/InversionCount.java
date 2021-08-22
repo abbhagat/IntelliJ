816ff07d4553bf1f629b2974f9f80400adda1679
abbhagat <abhinaw.kumar7@gmail.com>
2021-08-22 13:57:28 +0530

    Interview

--- Algorithm/src/arrays/InversionCount.java
+++ Algorithm/src/arrays/InversionCount.java
@@ -1,21 +1,15 @@
 package arrays;
 
 public class InversionCount {
-    static int[] a = { 10, 15, 8, 4, 2, 1 };
-    static int count;
-
-    static void countInversion(int i, int j, int k) {
-        if (a[i] > a[j] && a[j] > a[k]) {
-            System.out.println(a[i] + " " + a[j] + " " + a[k]);
-            count++;
-        }
-    }
 
     public static void main(String[] args) {
-        int i = 0;
-        int j = 0;
-        for (i = 0; i < a.length && j + 2 < a.length; i++, j++) {
-            countInversion(j, j + 1, j + 2);
+        int[] a = {10, 15, 8, 4, 2, 1};
+        int count = 0;
+        for (int i = 0, j = 0; i < a.length && j + 2 < a.length; i++, j++) {
+            if (a[j] > a[j + 1] && a[j + 1] > a[j + 2]) {
+                System.out.println(a[j] + " " + a[j + 1] + " " + a[j + 2]);
+                count++;
+            }
         }
         System.out.println("Total Inversion Count :" + count);
     }
