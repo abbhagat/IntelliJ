e97e2f44f4fa1f09fff35157f8aac02f27c796e3
abbhagat <abhinaw.kumar7@gmail.com>
2021-08-23 23:06:53 +0530

    Interview

--- Algorithm/src/leetcode/RemoveAGivenRepeatedElementFromArray.java
+++ Algorithm/src/leetcode/RemoveAGivenRepeatedElementFromArray.java
@@ -1,23 +1,30 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class RemoveAGivenRepeatedElementFromArray {
 
     public static void main(String[] args) {
-        int[] nums = {0,1,2,2,3,0,4,2};
+        int[] a = {0, 1, 2, 2, 3, 0, 4, 2};
         int val = 2;
-        int n = nums.length;
-        for (int i = 0; i < n;) {
-            if (nums[i] == val) {
-                for (int j = i; j < n - 1; j++) {
-                    nums[j] = nums[j + 1];
-                }
-                n--;
-            }else{
-                i++;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == val) {
+                list.add(i);
             }
         }
-        IntStream.range(0, n).forEach(i -> System.out.print(nums[i] + " "));
+        int j = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (!list.contains(i)) {
+                a[j] = a[i];
+                j++;
+            }
+        }
+        while (j < a.length) {
+            a[j++] = 0;
+        }
+        IntStream.range(0, j).forEach(i -> System.out.print(a[i] + " "));
     }
 }
