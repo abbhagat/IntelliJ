2d0fdb8e14b45202be2909c14acf1922f6122f34
abbhagat <abhinaw.kumar7@gmail.com>
2021-08-23 19:53:49 +0530

    Interview

--- Algorithm/src/arrays/ColindromeString.java
+++ Algorithm/src/arrays/ColindromeString.java
@@ -13,21 +13,23 @@ Output : String is Colindrome
  */
 public class ColindromeString {
 
-    public static void main(String[] args) {
-        String s = "mollomaappaa111";
-        boolean isColindromeString = true;
+    private static boolean isColindromeString(String s) {
         if (s.length() % 6 == 0) {
             for (int i = 0, j = 3; i < s.length() && j < s.length(); i += 6, j = i + 3) {
                 String s1 = s.substring(i, j);
                 String s2 = new StringBuffer(s.substring(j, j + 3)).reverse().toString();
                 if (!s1.equals(s2)) {
-                    isColindromeString = false;
+                    return false;
                 }
             }
-        System.out.println(isColindromeString ? "ColindromeString" : "Not ColindromeString");
-        } else {
-            System.out.println("String is not ColindromeString");
+            return true;
         }
+        return false;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(isColindromeString("mollomaappaa111") ? "ColindromeString" : "Not ColindromeString");
+        System.out.println(isColindromeString("cappaccappac") ? "ColindromeString" : "Not ColindromeString");
+        System.out.println(isColindromeString("mollomaappaa") ? "ColindromeString" : "Not ColindromeString");
     }
 }
