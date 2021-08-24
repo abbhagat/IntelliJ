e3cf55cb424e622e0099d782cdb391408ece5503
abbhagat <abhinaw.kumar7@gmail.com>
2021-08-24 19:48:35 +0530

    Interview

--- Algorithm/src/geeks/MinCoinToMakeAValue.java
+++ Algorithm/src/geeks/MinCoinToMakeAValue.java
@@ -2,25 +2,23 @@ package geeks;
 
 public class MinCoinToMakeAValue {
 
-    static int minCoins(int coins[], int n, int value) {
+    private static int minCoins(int coins[], int value) {
         if (value == 0) {
             return 0;
         }
         int result = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {  // Try every coin that has smaller value than value
+        for (int i = 0; i < coins.length; i++) {
             if (coins[i] <= value) {
-                int sub_result = minCoins(coins, n, value - coins[i]);
-                if (sub_result != Integer.MAX_VALUE && sub_result + 1 < result)  // Check for INT_MAX to avoid overflow and see if result can minimized
-                    result = sub_result + 1;
+                int min = minCoins(coins, value - coins[i]);
+                result = min < result ? min + 1 : result;
             }
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int coins[] = {9, 6, 5, 1};
-        int n = coins.length;
-        int value = 11;
-        System.out.println("Minimum coins required is " + minCoins(coins, n, value));
+        System.out.println("Minimum coins required is " + minCoins(new int[]{9, 6, 5, 1}, 11));
+        System.out.println("Minimum coins required is " + minCoins(new int[]{25, 10, 5}, 30));
+        System.out.println("Minimum coins required is " + minCoins(new int[]{9, 6, 5, 1}, 21));
     }
 }
