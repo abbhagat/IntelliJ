d604754a53d9fd5720b9eb93dd4ae0cb570351d1
abbhagat <abhinaw.kumar7@gmail.com>
2021-08-24 12:00:31 +0530

    Interview

--- Algorithm/src/geeks/CountPairsWithPerfectSquareSum.java
+++ Algorithm/src/geeks/CountPairsWithPerfectSquareSum.java
@@ -9,25 +9,24 @@ public class CountPairsWithPerfectSquareSum {
 
     private static int a[] = {2, 3, 6, 9, 10, 20};
 
-    public static List<Integer> getPerfectSquares(int n) {
+    public static List<Integer> getPerfectSquares(int sum) {
         List<Integer> perfectSquares = new ArrayList<>();
-        int current = 1, i = 1;
-        while (current <= n) {     // while current perfect square is less than or equal to n
-            perfectSquares.add(current);
-            current = (int) Math.pow(++i, 2);
+        int n = 1;
+        for (int i = 1; n < sum; ) {   // while current perfect square is less than or equal to n
+            perfectSquares.add(n);
+            n = (int) Math.pow(++i, 2);
         }
         return perfectSquares;
     }
 
     // Function to print the sum of maximum two elements from the array
     public static int maxPairSum() {
-        int max = a[0], secondMax = a[0];
+        int max = Integer.MIN_VALUE, secondMax = Integer.MIN_VALUE;
         for (int x : a) {
-            max = max < x ? x : max;
-            secondMax = secondMax > x ? x : secondMax;
-        }
-        for (int x : a) {
-            if (x < max && x >= secondMax) {
+            if (x > max) {
+                secondMax = max;
+                max = x;
+            } else if (x > secondMax) {
                 secondMax = x;
             }
         }
@@ -35,29 +34,28 @@ public class CountPairsWithPerfectSquareSum {
     }
 
     // Function to return the count of numbers that when added with n give a perfect square
-    public static int countPairsWith(int n, List<Integer> perfectSquares, Set<Integer> set) {
+    public static int countPairsWith(int x, List<Integer> perfectSquares, Set<Integer> set) {
         int count = 0;
         for (int i = 0; i < perfectSquares.size(); i++) {
-            int temp = perfectSquares.get(i) - n;
-            if (temp > n && set.contains(temp)) {  // temp > n is checked so that pairs (x, y) and (y, x) don't get counted twice
-                System.out.println(n + "," + temp);
+            int temp = perfectSquares.get(i) - x;
+            if (temp > x && set.contains(temp)) {  // temp > n is checked so that pairs (x, y) and (y, x) don't get counted twice
+                System.out.println(x + "," + temp);
                 count++;
             }
         }
         return count;
     }
 
-    // Function to count the pairs whose sum is a perfect square
     public static int countPairs() {
-        int max = maxPairSum();   // Sum of the maximum two elements from the array
-        List<Integer> perfectSquares = getPerfectSquares(max);  // List of perfect squares upto max
-        Set<Integer> set = new HashSet<>();  // Contains all the array elements
-        for (int i = 0; i < a.length; i++) {
-            set.add(a[i]);
+        int sum = maxPairSum();                                  // Sum of the maximum two elements from the array
+        List<Integer> perfectSquares = getPerfectSquares(sum);  // List of perfect squares upto max
+        Set<Integer> set = new HashSet<>();                    // Contains all the array elements
+        for (int x : a) {
+            set.add(x);
         }
         int count = 0;
-        for (int n : a) {
-            count += countPairsWith(n, perfectSquares, set);  // Add count of the elements that when added with a[i] give a perfect square
+        for (int x : a) {
+            count += countPairsWith(x, perfectSquares, set);  // Add count of the elements that when added with a[i] give a perfect square
         }
         return count;
     }
