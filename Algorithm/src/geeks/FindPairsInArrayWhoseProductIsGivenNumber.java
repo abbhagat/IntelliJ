b161fcd0ed84d3d85bf079d03f7543a70fcd6985
abbhagat <abhinaw.kumar7@gmail.com>
2021-08-24 15:52:43 +0530

    Interview

---
+++ Algorithm/src/geeks/FindPairsInArrayWhoseProductIsGivenNumber.java
@@ -0,0 +1,31 @@
+package geeks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindPairsInArrayWhoseProductIsGivenNumber {
+
+    private static int countUniquePairs(int[] a, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x : a) {
+            Integer k = map.get(x) == null ? map.put(x, 1) : map.put(x, map.get(x) + 1);
+        }
+        int count = 0;
+        for (int x : a) {
+            int y = target / x;
+            if ((x == y && (map.get(x) != null && map.get(x) > 1)) || map.containsKey(y)) {
+                map.remove(x);
+                map.remove(y);
+                System.out.println("(" + x + "," + y + ")");
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 3, 6, 12, 8, 3, 4, 12, 12, 144, 1};
+        int target = 144;
+        System.out.println(countUniquePairs(nums, target));
+    }
+}
