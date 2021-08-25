76468ce3dd27b26287b9a6ceddb1f5c6d0a92ece
abbhagat <abhinaw.kumar7@gmail.com>
2021-08-25 09:44:06 +0530

    Interview

--- Algorithm/src/geeks/LongestEvenLengthSubStringSumOf1stHalfAnd2ndHalfIsSame.java
+++ Algorithm/src/geeks/LongestEvenLengthSubStringSumOf1stHalfAnd2ndHalfIsSame.java
@@ -36,7 +36,7 @@ public class LongestEvenLengthSubStringSumOf1stHalfAnd2ndHalfIsSame {
 
     private static int findLength(String str, int n) {
         int maxLength = 0;                       // Initialize result
-        for (int i = 0; i < n - 1; i++) {      // Consider all possible midpoints one by one
+        for (int i = 0; i < n - 1; i++) {       // Consider all possible midpoints one by one
             int l = i, r = i + 1;              // For current midpoint 'i', keep expanding substring on both sides, if sum of both sides becomes equal update maxLength
             int lsum = 0, rsum = 0;           // initialize left and right sum
             while (l >= 0 && r < n) {        // move on both sides till indexes go out of bounds
