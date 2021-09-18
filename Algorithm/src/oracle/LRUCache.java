346ec52cdd1c9d4c8fdf77cb0ffba3413eb2c5e3
abbhagat <abhinaw.kumar7@gmail.com>
2021-09-18 11:33:05 +0530

    Interview

--- Algorithm/src/oracle/LRUCache.java
+++ Algorithm/src/oracle/LRUCache.java
@@ -1,6 +1,7 @@
 package oracle;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /*
 LRU cache is implemented using a doubly linked list and a hash map.
@@ -10,17 +11,17 @@ page at tail being the least recently used page in the list.
 Hash Map (key: page number, value: page) is used for O(1) access to pages in cache
 
 When a page is accessed, there can be 2 cases:
-1. Page is present in the cache - If the page is already present in the cache, we move the page to the start
-of the list.
+1. Page is present in the cache - If the page is already present in the cache, we move the page to the start of the list.
 2. Page is not present in the cache - If the page is not present in the cache, we add the page to the list.
 How to add a page to the list:
-   a. If the cache is not full, add the new page to the start of the list.
-   b. If the cache is full, remove the last node of the linked list and move the new page to the start of the list.
+a. If the cache is not full, add the new page to the start of the list.
+b. If the cache is full, remove the last node of the linked list and move the new page to the start of the list.
  */
+
 public class LRUCache {
 
     private DoublyLinkedList pageList;
-    private HashMap<Integer, Node> pageMap;
+    private Map<Integer, Node> pageMap;
     private final int cacheSize;
 
     public LRUCache(int cacheSize) {
@@ -29,21 +30,17 @@ public class LRUCache {
         pageMap = new HashMap<>();
     }
 
-    public void accessPage(int pageNumber) {
-        Node pageNode = null;
-        if (pageMap.containsKey(pageNumber)) {
-            // If page is present in the cache, move the page to the start of list
-            pageNode = pageMap.get(pageNumber);
+    public void accessPage(int num) {
+        Node pageNode;
+        if (pageMap.containsKey(num)) {   // If page is present in the cache, move the page to the start of list
+            pageNode = pageMap.get(num);
             pageList.movePageToHead(pageNode);
         } else {
-            // If the page is not present in the cache, add the page to the cache
-            if (pageList.getCurrSize() == pageList.getSize()) {
-                // If cache is full, we will remove the tail from the cache pageList
-                // Remove it from map too
-                pageMap.remove(pageList.getTail().getPageNumber());
+            if (pageList.currSize == pageList.size) {  // If cache is full, we will remove the tail from the cache pageList Remove it from map too
+                pageMap.remove(pageList.tail.num);
             }
-            pageNode = pageList.addPageToList(pageNumber);
-            pageMap.put(pageNumber, pageNode);
+            pageNode = pageList.addPageToList(num); // If the page is not present in the cache, add the page to the cache
+            pageMap.put(num, pageNode);
         }
     }
 
@@ -76,132 +73,69 @@ public class LRUCache {
     }
 }
 
+class Node {
+    int num;
+    Node prev, next;
+
+    public Node(int num) {
+        this.num = num;
+    }
+}
+
 class DoublyLinkedList {
 
-    private final int size;
-    private int currSize;
-    private Node head;
-    private Node tail;
+    final int size;
+    int currSize;
+    Node head;
+    Node tail;
 
     public DoublyLinkedList(int size) {
         this.size = size;
         currSize = 0;
     }
 
-    public Node getTail() {
-        return tail;
-    }
-
     public void printList() {
-        if (head == null) {
-            return;
-        }
-        Node tmp = head;
-        while (tmp != null) {
-            System.out.print(tmp);
-            tmp = tmp.getNext();
+        for (Node temp = head; temp != null; temp = temp.next) {
+            System.out.print(temp.num + " ");
         }
     }
 
-    public Node addPageToList(int pageNumber) {
-        Node pageNode = new Node(pageNumber);
+    public Node addPageToList(int num) {
+        Node node = new Node(num);
         if (head == null) {
-            head = pageNode;
-            tail = pageNode;
+            head = node;
+            tail = node;
             currSize = 1;
-            return pageNode;
+            return node;
         } else if (currSize < size) {
             currSize++;
         } else {
-            tail = tail.getPrev();
-            tail.setNext(null);
+            tail = tail.prev;
+            tail.next = null;
         }
-        pageNode.setNext(head);
-        head.setPrev(pageNode);
-        head = pageNode;
-        return pageNode;
+        node.next = head;
+        head.prev = node;
+        head = node;
+        return node;
     }
 
     public void movePageToHead(Node pageNode) {
         if (pageNode == null || pageNode == head) {
             return;
         }
-
         if (pageNode == tail) {
-            tail = tail.getPrev();
-            tail.setNext(null);
+            tail = tail.prev;
+            tail.next = null;
         }
-
-        Node prev = pageNode.getPrev();
-        Node next = pageNode.getNext();
-        prev.setNext(next);
-
+        Node prev = pageNode.prev;
+        Node next = pageNode.next;
+        prev.next = next;
         if (next != null) {
-            next.setPrev(prev);
+            next.prev = (prev);
         }
-
-        pageNode.setPrev(null);
-        pageNode.setNext(head);
-        head.setPrev(pageNode);
+        pageNode.prev = null;
+        pageNode.next = head;
+        head.prev = pageNode;
         head = pageNode;
-    }
-
-    public int getCurrSize() {
-        return currSize;
-    }
-
-    public void setCurrSize(int currSize) {
-        this.currSize = currSize;
-    }
-
-    public Node getHead() {
-        return head;
-    }
-
-    public void setHead(Node head) {
-        this.head = head;
-    }
-
-    public int getSize() {
-        return size;
-    }
-}
-
-class Node {
-
-    private int pageNumber;
-    private Node prev;
-    private Node next;
-
-    public Node(int pageNumber) {
-        this.pageNumber = pageNumber;
-    }
-
-    public int getPageNumber() {
-        return pageNumber;
-    }
-
-    public void setPageNumber(int data) {
-        this.pageNumber = data;
-    }
-
-    public Node getPrev() {
-        return prev;
-    }
-
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    public String toString() {
-        return pageNumber + "  ";
     }
 }
