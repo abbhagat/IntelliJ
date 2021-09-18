d88bfd78097e8c5d9cc45b51fc62a8b644030123
abbhagat <abhinaw.kumar7@gmail.com>
2021-09-18 22:11:40 +0530

    Interview

--- Algorithm/src/leetcode/LongestSubstring.java
+++ Algorithm/src/leetcode/LongestSubstring.java
@@ -5,19 +5,19 @@ import static java.lang.Integer.max;
 public class LongestSubstring {
 
     private static int lengthOfLongestSubstring(char[] c) {
-        int result = 0;
+        int result = 0, j = 0;
         int[] temp = new int[128];
-        for (int i = 0, j = 0; j < c.length; j++) {
-            i = max(i, temp[c[j]]);
-            result = max(result, j - i + 1);
-            temp[c[j]] = j + 1;
+        for (int i = 0; i < c.length; i++) {
+            j = max(j, temp[c[i]]);
+            result = max(result, i - j + 1);
+            temp[c[i]] = i + 1;
         }
         return result;
     }
 
     public static void main(String[] args) {
         System.out.println("abcabcbb" + " " + lengthOfLongestSubstring("abcabcbb".toCharArray()));
-        System.out.println("bbbbb"    + " " + lengthOfLongestSubstring("bbbbb".toCharArray()));
-        System.out.println("pwwkew"   + " " + lengthOfLongestSubstring("pwwkew".toCharArray()));
+        System.out.println("bbbbb" + " " + lengthOfLongestSubstring("bbbbb".toCharArray()));
+        System.out.println("pwwkew" + " " + lengthOfLongestSubstring("pwwkew".toCharArray()));
     }
 }
