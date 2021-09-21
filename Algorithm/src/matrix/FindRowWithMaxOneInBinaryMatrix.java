0776e7a8732f87f5e0a18ad61fefd45c1817aaf9
abbhagat <abhinaw.kumar7@gmail.com>
2021-09-22 00:53:54 +0530

    Interview

---
+++ Algorithm/src/matrix/FindRowWithMaxOneInBinaryMatrix.java
@@ -0,0 +1,77 @@
+package matrix;
+
+/**
+ * Given a boolean 2D array, where each row is sorted. Find the row with the maximum number of 1s.
+ * <p>
+ * Example:
+ * <p>
+ * Input matrix
+ * 0 1 1 1
+ * 0 0 1 1
+ * 1 1 1 1  // this row has maximum 1s
+ * 0 0 0 0
+ * <p>
+ * Output: 2
+ */
+
+public class FindRowWithMaxOneInBinaryMatrix {
+
+    private static int M[][] = {
+            {0, 0, 0, 1},
+            {0, 1, 1, 1},
+            {1, 1, 1, 1},
+            {0, 0, 0, 0}
+    };
+    private static int R = M.length, C = M[0].length;
+
+    private static int binarySearch(int a[], int low, int high) {
+        if (low <= high) {
+            int mid = (low + high) / 2;
+            if ((mid == 0 || (a[mid - 1] == 0)) && a[mid] == 1) {
+                return mid;
+            }
+            return a[mid] == 0 ? binarySearch(a, mid + 1, high) : binarySearch(a, low, mid - 1);
+        }
+        return -1;
+    }
+
+    private static int rowWithMax1s(int mat[][]) {
+        int max_row_index = 0, max = -1;
+        for (int i = 0; i < R; i++) {
+            int index = binarySearch(mat[i], 0, C - 1);
+            if (index != -1 && C - index > max) {
+                max = C - index;
+                max_row_index = i;
+            }
+        }
+        return max_row_index;
+    }
+
+    private static int binarySearch0(int a[], int low, int high) {
+        if (low <= high) {
+            int mid = (low + high) / 2;
+            if ((mid == 0 || (a[mid - 1] == 0)) && a[mid] == 0) {
+                return mid;
+            }
+            return a[mid] == 0 ? binarySearch(a, mid + 1, high) : binarySearch(a, low, mid - 1);
+        }
+        return -1;
+    }
+
+    private static int rowWithMax0s(int mat[][]) {
+        int max_row_index = 0, max = -1;
+        for (int i = 0; i < R; i++) {
+            int index = binarySearch0(mat[i], 0, C - 1);
+            if (index != -1 && index >= max) {
+                max = index;
+                max_row_index = i;
+            }
+        }
+        return max_row_index;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Index of row with maximum 1s is " + rowWithMax1s(M));
+        System.out.println("Index of row with maximum 0s is " + rowWithMax0s(M));
+    }
+}
