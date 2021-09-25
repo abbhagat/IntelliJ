4c298ff2fb7ca6518c9ec209044f2e95d7fdd7e4
abbhagat <abhinaw.kumar7@gmail.com>
2021-09-25 22:36:52 +0530

    Interview

--- Algorithm/src/strings/GroupStringAnagrams.java
+++ Algorithm/src/strings/GroupStringAnagrams.java
@@ -1,11 +1,11 @@
 package strings;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GroupStringAnagrams {
 
-    static boolean isAnagram(String s1, String s2) {
+    private static boolean isAnagram(String s1, String s2) {
         int[] temp = new int[128];
         if (s1.length() == s2.length()) {
             for (int i = 0; i < s1.length(); i++) {
@@ -17,37 +17,25 @@ public class GroupStringAnagrams {
                     return false;
                 }
             }
-        } else {
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 
     public static void main(String[] args) {
-        List<List<String>> list = new ArrayList<>();
         String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-        label:
+        Set<String> set = new HashSet<>();
         for (int i = 0; i < strs.length; i++) {
-            List<String> al = new ArrayList<>();
-            if (!list.isEmpty()) {
-                for (List k : list) {
-                    if (k.contains(strs[i])) {
-                        continue label;
-                    }
-                }
+            if (!set.isEmpty()) {
+                System.out.println(set);
             }
-            al.add(strs[i]);
+            set = new HashSet<>();
             for (int j = i + 1; j < strs.length; j++) {
                 if (isAnagram(strs[i], strs[j])) {
-                    if(!al.contains(strs[i]))
-                    al.add(strs[i]);
-                    al.add(strs[j]);
+                    set.add(strs[i]);
+                    set.add(strs[j]);
                 }
             }
-            if (!al.isEmpty()) {
-                list.add(al);
-            }
         }
-        System.out.println(list);
     }
 }
--- Algorithm/src/strings/PalindromeStringCheck.java
+++ Algorithm/src/strings/PalindromeStringCheck.java
@@ -3,7 +3,7 @@ package strings;
 public class PalindromeStringCheck {
 
     private static boolean isPalindrome(char[] c) {
-        for (int i = 0, j = c.length - 1; i < c.length / 2; i++, j--) {
+        for (int i = 0, j = c.length - 1; i <= j; i++, j--) {
             if (c[i] != c[j]) {
                 return false;
             }
@@ -12,6 +12,7 @@ public class PalindromeStringCheck {
     }
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome("MADAM".toCharArray()) ? "Palindrome" : "Not Palindrome");
+        System.out.println(isPalindrome("MADAM".toCharArray())  ? "Palindrome" : "Not Palindrome");
+        System.out.println(isPalindrome("MADDAM".toCharArray()) ? "Palindrome" : "Not Palindrome");
     }
 }
