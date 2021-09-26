fa1fd718e4a5c7af7869ad51fec3c4e0222c6870
abbhagat <abhinaw.kumar7@gmail.com>
2021-09-26 08:19:49 +0530

    Interview

--- Algorithm/src/cisco/LargestContiguousProductArrayPrint.java
+++ Algorithm/src/cisco/LargestContiguousProductArrayPrint.java
@@ -15,10 +15,10 @@ public class LargestContiguousProductArrayPrint {
         for (int i = 0; i < a.length; i++) {
             /* If this element is positive, update max_end. Update min_end only if min_end is negative */
             if (a[i] > 0) {
-                max_end = max_end * a[i];
+                max_end = max(max_end * a[i], 1);
                 min_end = min(min_end * a[i], 1);
             } else if (a[i] == 0) {  // If this element is 0, then the maximum product cannot end here,
-                max_end = 1;  // make both max_end and min_ending _here 0. Assumption: Output is alway greater than or equal to 1.
+                max_end = 1;        // make both max_end and min_ending _here 0. Assumption: Output is always greater than or equal to 1.
                 min_end = 1;
             }
             /* If element is negative. This is tricky max_end can either be 1 or positive.
@@ -30,7 +30,7 @@ public class LargestContiguousProductArrayPrint {
                 max_end = max(min_end * a[i], 1);
                 min_end = temp * a[i];
             }
-            max_product = max_product < max_end ? max_end : max_product;   // update max_product, if needed
+            max_product = max(max_product, max_end);   // update max_product, if needed
         }
         return max_product;
     }
