dc4ab8a3778bc2fda1b1786b09b7e367898c1cf7
abbhagat <abhinaw.kumar7@gmail.com>
2021-09-27 20:09:29 +0530

    Interview

--- Algorithm/src/trees/LCA.java
+++ Algorithm/src/trees/LCA.java
@@ -2,16 +2,29 @@ package trees;
 
 public class LCA {
 
-    Node LCA(Node root, int num1, int num2) {
-
-        if (root != null && num1 < root.num && num2 > root.num) {
-            return root;
-        } else if (num1 < root.num && num2 < root.num) {
-            return LCA(root.left, num1, num2);
-        } else if (num1 > root.num && num2 > root.num) {
-            return LCA(root.right, num1, num2);
-        } else {
-            return root;
+    private static Node LCA(Node root, int num1, int num2) {
+        if (root != null) {
+            if (num1 < root.num && num2 > root.num) {
+                return root;
+            }
+            if (num1 < root.num && num2 < root.num) {
+                return LCA(root.left, num1, num2);
+            }
+            if (num1 > root.num && num2 > root.num) {
+                return LCA(root.right, num1, num2);
+            }
         }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {30, 10, 50, 5, 20, 40, 60, 1};
+        Node root = null;
+        CreateBST bst = new CreateBST();
+        for (int i = 0; i < a.length; i++) {
+            root = bst.createBST(root, a[i]);
+        }
+        Node lca = LCA(root, 5, 40);
+        System.out.println(lca.num);
     }
 }
