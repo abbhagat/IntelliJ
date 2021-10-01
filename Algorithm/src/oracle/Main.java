53190855e19006ce7700bad0353dd1f2031a010e
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-01 21:00:41 +0530

    Interview

--- Algorithm/src/oracle/Main.java
+++ Algorithm/src/oracle/Main.java
@@ -17,10 +17,24 @@ public class Main {
         }
     }
 
-
+    private static int countWaysDP(int n){
+        int[] dp = new int[n+1];
+        if(n == 1){
+            dp[1] = 1;
+        }
+        if(n >= 2){
+            dp[1] = 1;
+            dp[2] = 2;
+        }
+        for(int i = 3; i <=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
 
     public static void main(String[] args) {
         int n = 1;
         System.out.println(countWays(n));
+        System.out.println(countWaysDP(n));
     }
 }
