e7f80b21a00785d2ed2324a9839242a54bbc71d7
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-02 21:19:57 +0530

    Interview

--- Algorithm/src/strings/PrintAllKLengthStrings.java
+++ Algorithm/src/strings/PrintAllKLengthStrings.java
@@ -2,19 +2,17 @@ package strings;
 
 public class PrintAllKLengthStrings {
 
-    private static char set[] = {'A', 'B', 'C'};
-
-    private static void printAllKLength(String prefix, int itr) {
+    private static void printAllKLength(char[] set, String prefix, int itr) {
         if (itr == 0) {
             System.out.println(prefix);
         } else {
             for (int i = 0; i < set.length; i++) {
-                printAllKLength(prefix + set[i], itr - 1);
+                printAllKLength(set, prefix + set[i], itr - 1);
             }
         }
     }
 
     public static void main(String[] args) {
-        printAllKLength("", 3);
+        printAllKLength(new char[]{'A', 'B', 'C'}, "", 3);
     }
 }
