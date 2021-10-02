5a720bb426959a93fbde5d613c19c8d9ac074e39
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-02 22:26:12 +0530

    Interview

--- Algorithm/src/trees/CreateBST.java
+++ Algorithm/src/trees/CreateBST.java
@@ -5,12 +5,10 @@ public class CreateBST {
     public static Node createBST(Node root, int num) {
         if (root == null) {
             root = new Node(num);
-        } else {
-            if (num < root.num) {
-                root.left = createBST(root.left, num);
-            } else if (num > root.num) {
-                root.right = createBST(root.right, num);
-            }
+        } else if (num < root.num) {
+            root.left = createBST(root.left, num);
+        } else if (num > root.num) {
+            root.right = createBST(root.right, num);
         }
         return root;
     }
