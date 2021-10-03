fe2df717603f9c30c768532c108df613ef03de60
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-03 11:37:31 +0530

    Interview

--- Algorithm/src/trees/IterativeSearchBST.java
+++ Algorithm/src/trees/IterativeSearchBST.java
@@ -11,4 +11,14 @@ public class IterativeSearchBST {
         }
         return null;
     }
+
+    public static void main(String[] args) {
+        int[] a = {30, 10, 50, 5, 20, 40, 60, 1};
+        Node root = null;
+        CreateBST bst = new CreateBST();
+        for (int i = 0; i < a.length; i++) {
+            root = bst.createBST(root, a[i]);
+        }
+        System.out.println(searchBST(root, 50));
+    }
 }
--- Algorithm/src/trees/LevelOrderTreeTraversal.java
+++ Algorithm/src/trees/LevelOrderTreeTraversal.java
@@ -1,5 +1,7 @@
 package trees;
 
+import static trees.CreateBST.createBST;
+
 public class LevelOrderTreeTraversal {
 
     private static TreeHeight th = new TreeHeight();
@@ -19,17 +21,16 @@ public class LevelOrderTreeTraversal {
             System.out.print(root.num + " ");
         }
         if (level > 0) {
-            printLevelOrder(root.left, level - 1);
+            printLevelOrder(root.left,  level - 1);
             printLevelOrder(root.right, level - 1);
         }
     }
 
     public static void main(String[] args) {
-        BinaryTree BT = new BinaryTree();
         Node root = null;
         int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         for (int x : a) {
-            root = BT.createBT(x, root);
+            root = createBST(root, x);
         }
         System.out.println("Level Order Or BFS Traversal : ");
         levelOrderTreeTraversal(root);
