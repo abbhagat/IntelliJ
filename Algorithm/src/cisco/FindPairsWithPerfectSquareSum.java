2ad08362438a139b3830baea0b286cff1a8a5cd4
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-03 20:17:03 +0530

    Interview

--- Algorithm/src/cisco/FindPairsWithPerfectSquareSum.java
+++ Algorithm/src/cisco/FindPairsWithPerfectSquareSum.java
@@ -12,14 +12,13 @@ public class FindPairsWithPerfectSquareSum {
     public static List<Integer> getPerfectSquares(int sum) {
         List<Integer> perfectSquares = new ArrayList<>();
         int n = 1;
-        for (int i = 1; n < sum; ) {   // while current perfect square is less than or equal to n
+        for (int i = 1; n < sum; ) {
             perfectSquares.add(n);
             n = (int) Math.pow(++i, 2);
         }
         return perfectSquares;
     }
 
-    // Function to print the sum of maximum two elements from the array
     public static int maxPairSum() {
         int max = Integer.MIN_VALUE, secondMax = Integer.MIN_VALUE;
         for (int x : a) {
@@ -33,7 +32,6 @@ public class FindPairsWithPerfectSquareSum {
         return (max + secondMax);
     }
 
-    // Function to return the count of numbers that when added with n give a perfect square
     public static int countPairsWith(int x, List<Integer> perfectSquares, Set<Integer> set) {
         int count = 0;
         for (int target : perfectSquares) {
@@ -47,15 +45,15 @@ public class FindPairsWithPerfectSquareSum {
     }
 
     public static int countPairs() {
-        int sum = maxPairSum();                                  // Sum of the maximum two elements from the array
-        List<Integer> perfectSquares = getPerfectSquares(sum);  // List of perfect squares upto max
-        Set<Integer> set = new HashSet<>();                    // Contains all the array elements
+        int sum = maxPairSum();
+        List<Integer> perfectSquares = getPerfectSquares(sum);
+        Set<Integer> set = new HashSet<>();
         for (int x : a) {
             set.add(x);
         }
         int count = 0;
         for (int x : a) {
-            count += countPairsWith(x, perfectSquares, set);  // Add count of the elements that when added with a[i] give a perfect square
+            count += countPairsWith(x, perfectSquares, set);
         }
         return count;
     }
