6924ba536d915f5c51cb9f52f1436abe84a980db
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-03 21:55:24 +0530

    Interview

--- Algorithm/src/geeksforgeeks/MedianOfTwoArrays.java
+++ Algorithm/src/geeksforgeeks/MedianOfTwoArrays.java
@@ -36,5 +36,6 @@ public class MedianOfTwoArrays {
 
     public static void main(String[] args) {
         System.out.println(findMedian(new int[]{1, 2}, new int[]{3, 4}));
+        System.out.println(findMedian(new int[]{10, 20}, new int[]{30, 40}));
     }
 }
