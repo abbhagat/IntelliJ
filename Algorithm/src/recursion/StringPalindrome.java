100f7e1b935d507e397bf765a1f512412cceb6fd
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-05 00:08:10 +0530

    Interview

---
+++ Algorithm/src/recursion/StringPalindrome.java
@@ -0,0 +1,15 @@
+package recursion;
+
+public class StringPalindrome {
+
+    private static boolean isPalindrome(String s) {
+        if (s.isEmpty() || s.length() == 1) {
+            return true;
+        }
+        return s.charAt(0) == s.charAt(s.length() - 1) ? isPalindrome(s.substring(1, s.length() - 1)) : false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("MADAM"));
+    }
+}
