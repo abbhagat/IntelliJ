b43a661c5124de019e9c22ea13c5c99700059772
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-06 21:47:41 +0530

    Interview

---
+++ Algorithm/src/linkedlist/PrintCommonElementsInThreeSortedArray.java
@@ -0,0 +1,25 @@
+package linkedlist;
+
+public class PrintCommonElementsInThreeSortedArray {
+
+    public static void main(String[] args) {
+        int a[] = {1, 5, 10, 20, 40, 80};
+        int b[] = {6, 7, 20, 80, 100};
+        int c[] = {3, 4, 15, 20, 30, 70, 80, 120};
+        int i = 0, j = 0, k = 0;
+        while (i < a.length && j < b.length && k < c.length) {
+            if (a[i] == b[j] && b[j] == c[k]) {
+                System.out.print(a[i] + " ");
+                i++;
+                j++;
+                k++;
+            } else if (a[i] < b[j]) {
+                i++;
+            } else if (b[j] < c[k]) {
+                j++;
+            } else {
+                k++;
+            }
+        }
+    }
+}
---
+++ Algorithm/src/trees/PairWithGivenSumInBalancedBST.java
@@ -0,0 +1,50 @@
+package trees;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static trees.CreateBST.createBST;
+
+/**
+ *
+ *                    15
+ *                 /     \
+ *               10      20
+ *              / \     /  \
+ *             8  12   16  25
+ */
+
+public class PairWithGivenSumInBalancedBST {
+
+    private static List<Integer> inorder(Node root, List<Integer> list) {
+        if (root != null) {
+            inorder(root.left, list);
+            list.add(root.num);
+            inorder(root.right, list);
+        }
+        return list;
+    }
+
+    private static boolean isPairPresent(Node root, int target) {
+        List<Integer> list = inorder(root, new ArrayList<>());
+        for (int x : list) {
+            int y = target - x;
+            if (list.contains(y)) {
+                System.out.println(x + "," + y);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {15, 10, 20, 8, 12, 16, 25};
+        Node root = null;
+        for (int x : a) {
+            root = createBST(root, x);
+        }
+        int target = 33;
+        System.out.println(isPairPresent(root, target));
+    }
+}
