7a0a2860c4615ad22803bbbf1e6c6f153b78c3e4
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-07 11:48:14 +0530

    Interview

--- Algorithm/src/gfg/FindPairsInArrayWhoseDiffIsGivenNumber.java
+++ Algorithm/src/gfg/FindPairsInArrayWhoseDiffIsGivenNumber.java
@@ -23,5 +23,7 @@ public class FindPairsInArrayWhoseDiffIsGivenNumber {
         findPairs(new int[]{2, 6, 7, 1, 8, 3, 5, 5, 4, 5}, 4);
         System.out.println();
         findPairs(new int[]{10, 5, 15, 5, 0}, 5);
+        System.out.println();
+        findPairs(new int[]{10, 5, 15}, 5);
     }
 }
