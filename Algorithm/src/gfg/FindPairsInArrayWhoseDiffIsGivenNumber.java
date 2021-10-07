6e22f82b209ba8a18efddfb3b07320a5210a1591
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-07 11:47:45 +0530

    Interview

--- Algorithm/src/gfg/FindPairsInArrayWhoseDiffIsGivenNumber.java
+++ Algorithm/src/gfg/FindPairsInArrayWhoseDiffIsGivenNumber.java
@@ -1,29 +1,27 @@
 package gfg;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FindPairsInArrayWhoseDiffIsGivenNumber {
 
     private static void findPairs(int[] a, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
         for (int x : a) {
-            Integer k = map.containsKey(x) ? map.put(x, map.get(x) + 1) : map.put(x, 1);
+            set.add(x);
         }
         for (int x : a) {
             int y = x - target;
-            if (map.containsKey(y)) {
-                if (x == y && map.get(x) == 1) {
-                    continue;
-                }
+            if (set.contains(y)) {
                 System.out.println("(" + x + "," + y + ")");
-                map.remove(x);
-                map.remove(y);
+                set.remove(y);
             }
         }
     }
 
     public static void main(String[] args) {
         findPairs(new int[]{2, 6, 7, 1, 8, 3, 5, 5, 4, 5}, 4);
+        System.out.println();
+        findPairs(new int[]{10, 5, 15, 5, 0}, 5);
     }
 }
