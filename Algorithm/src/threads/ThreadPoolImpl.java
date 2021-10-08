745440a29ba0ff5ea2c9100255aaa6a703e09fcd
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-08 19:12:10 +0530

    Interview

---
+++ Algorithm/src/threads/ThreadPoolImpl.java
@@ -0,0 +1,59 @@
+package threads;
+
+
+import java.util.Vector;
+
+class WorkersThread implements Runnable {
+
+    public WorkersThread() {
+        Thread thread = new Thread(this, "");
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
+
+public class ThreadPoolImpl {
+
+    private final int POOL_SIZE = 10;
+    private Vector<WorkersThread> threadPool = new Vector<>();
+
+    public ThreadPoolImpl() {
+        initializeThreadPool();
+    }
+
+    private void initializeThreadPool() {
+        while (!isThreadPoolFull()) {
+            threadPool.add(createNewThread());
+        }
+        System.out.println("Thread Pool is Full");
+    }
+
+    private boolean isThreadPoolFull() {
+        return threadPool.size() <= POOL_SIZE ? true : false;
+    }
+
+    private WorkersThread createNewThread() {
+        return new WorkersThread();
+    }
+
+    public WorkersThread getConnectionFromPool() {
+        WorkersThread thread = null;
+        if (threadPool.size() > 0) {
+            thread = threadPool.firstElement();
+            threadPool.removeElementAt(0);
+        }
+        return thread;
+    }
+
+    public void returnConnectionToPool(WorkersThread thread) {
+        threadPool.add(thread);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
