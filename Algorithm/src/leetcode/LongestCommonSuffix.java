48073741e0201299a6d3c3296d0ae08b58d35e68
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-08 21:46:00 +0530

    Interview

--- Algorithm/src/leetcode/LongestCommonSuffix.java
+++ Algorithm/src/leetcode/LongestCommonSuffix.java
@@ -1,29 +1,19 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class LongestCommonSuffix {
 
-    private static String longestCommonSuffix(List<String> list) {
-        if (list.isEmpty()) {
-            return "";
-        }
-        return longestCommonSuffix(list, 0, list.size() - 1);
-    }
-
-    private static String longestCommonSuffix(List<String> list, int low, int high) {
+    private static String longestCommonSuffix(String[] str, int low, int high) {
         if (low == high) {
-            return list.get(low);
+            return str[low];
         }
         int mid = (low + high) / 2;
-        String lcsLeft  = longestCommonSuffix(list, low, mid);
-        String lcsRight = longestCommonSuffix(list, mid + 1, high);
+        String lcsLeft  = longestCommonSuffix(str, low, mid);
+        String lcsRight = longestCommonSuffix(str, mid + 1, high);
         return longestCommonSuffix(lcsLeft, lcsRight);
     }
 
     private static String longestCommonSuffix(String left, String right) {
-        left  = new StringBuilder(left).reverse().toString();
+        left = new StringBuilder(left).reverse().toString();
         right = new StringBuilder(right).reverse().toString();
         int min = Math.min(left.length(), right.length());
         for (int i = 0; i < min; i++) {
@@ -35,7 +25,7 @@ public class LongestCommonSuffix {
     }
 
     public static void main(String[] args) {
-        System.out.println(longestCommonSuffix(Arrays.asList("ABCDEBUILD", "OUCTBUILD")));
-        System.out.println(longestCommonSuffix(Arrays.asList("ABCDEBUILD", "ABCDEBUILD")));
+        System.out.println(longestCommonSuffix(new String[]{"ABCDEBUILD", "QOUCTBUILD"}, 0, 1));
+        System.out.println(longestCommonSuffix(new String[]{"ABCDEBUILD", "ABCDEBUILD"}, 0, 1));
     }
 }
