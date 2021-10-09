3c3ea4e4f2235288926a7433ce393e2f83861b7a
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-09 08:23:17 +0530

    Interview

--- Algorithm/src/trees/BinaryTreeDiameter.java
+++ Algorithm/src/trees/BinaryTreeDiameter.java
@@ -18,10 +18,10 @@ public class BinaryTreeDiameter {
     }
 
     public static void main(String[] args) {
-        Node root = new Node(1);
-        root.left = new Node(2);
+        Node root  = new Node(1);
+        root.left  = new Node(2);
         root.right = new Node(3);
-        root.left.left = new Node(4);
+        root.left.left  = new Node(4);
         root.left.right = new Node(5);
         System.out.println("The diameter of given binary tree is : " + diameter(root));
     }
