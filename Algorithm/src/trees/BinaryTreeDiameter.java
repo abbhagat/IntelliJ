67f32ed472dc07edea7cda6bc7533f3c357413b1
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-10 14:05:07 +0530

    Interview

--- Algorithm/src/trees/BinaryTreeDiameter.java
+++ Algorithm/src/trees/BinaryTreeDiameter.java
@@ -1,28 +1,35 @@
 package trees;
 
+
+// Time Complexity: O(n)
+
 import static java.lang.Integer.max;
-import static trees.TreeHeight.treeHeight;
+
+class Height {
+    int h;
+}
 
 public class BinaryTreeDiameter {
 
-    private static int diameter(Node root) {
+    private static int diameter(Node root, Height height) {
+        Height lH = new Height();
+        Height rH = new Height();
         if (root == null) {
+            height.h = 0;
             return 0;
         }
-        int leftHeight  = treeHeight(root.left);
-        int rightHeight = treeHeight(root.right);
-
-        int leftDiameter  = diameter(root.left);
-        int rightDiameter = diameter(root.right);
-        return max(leftHeight + rightHeight + 1, max(leftDiameter, rightDiameter));
+        int leftDiameter  = diameter(root.left, lH);
+        int rightDiameter = diameter(root.right, rH);
+        height.h = Math.max(lH.h, rH.h) + 1;
+        return max(lH.h + rH.h + 1, max(leftDiameter, rightDiameter));
     }
 
     public static void main(String[] args) {
-        Node root  = new Node(1);
-        root.left  = new Node(2);
+        Node root = new Node(1);
+        root.left = new Node(2);
         root.right = new Node(3);
-        root.left.left  = new Node(4);
+        root.left.left = new Node(4);
         root.left.right = new Node(5);
-        System.out.println("The diameter of given binary tree is : " + diameter(root));
+        System.out.println("The diameter of given binary tree is : " + diameter(root, new Height()));
     }
 }
