5dc6eeb99ba4788aae5e43c1a45928cc974a70f9
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-13 23:24:36 +0530

    Interview

--- Algorithm/src/vmware/ReArrangeArrayToOddEvenPlace.java
+++ Algorithm/src/vmware/ReArrangeArrayToOddEvenPlace.java
@@ -1,4 +1,25 @@
 package vmware;
 
+import java.util.stream.IntStream;
+
 public class ReArrangeArrayToOddEvenPlace {
+
+    public static void main(String[] args) {
+        int[] a = {2, 1, 3, 4, 7, 9, 24, 98};
+        int oddIdx = 0, evenIdx = 1;
+        while (oddIdx < a.length - 1 && evenIdx < a.length) {
+            if (a[oddIdx] % 2 == 1) {
+                oddIdx += 2;
+            } else {
+                if (a[evenIdx] % 2 == 1) {
+                    int t = a[oddIdx];
+                    a[oddIdx] = a[evenIdx];
+                    a[evenIdx] = t;
+                } else {
+                    evenIdx += 2;
+                }
+            }
+        }
+        IntStream.range(0, a.length).forEach(i -> System.out.print(a[i] + " "));
+    }
 }
--- Algorithm/src/vmware/StringCompareToImpl.java
+++ Algorithm/src/vmware/StringCompareToImpl.java
@@ -1,4 +1,28 @@
 package vmware;
 
 public class StringCompareToImpl {
+
+    private static int compareTo(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return 1;
+        } else if (s1.length() < s2.length()) {
+            return -1;
+        } else {
+            int i = 0;
+            while (i < s1.length()) {
+                if (s1.charAt(i) == s2.charAt(i)) {
+                    i++;
+                    continue;
+                }
+                return s1.charAt(i) > s2.charAt(i) ? 1 : -1;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Java.compareTo(Java)  " + compareTo("Java" , "Java"));
+        System.out.println("Javaa.compareTo(Java) " + compareTo("Javaa", "Java"));
+        System.out.println("Java.compareTo(Javaa) " + compareTo("Java" , "Javaa"));
+    }
 }
