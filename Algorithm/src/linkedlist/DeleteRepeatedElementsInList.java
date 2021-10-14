7cf54818b76726195b3e48ebd7ac0308a7919c84
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-14 08:48:43 +0530

    Interview

--- Algorithm/src/linkedlist/DeleteRepeatedElementsInList.java
+++ Algorithm/src/linkedlist/DeleteRepeatedElementsInList.java
@@ -1,6 +1,35 @@
 package linkedlist;
 
+import static linkedlist.LinkList.add;
+import static linkedlist.TraverseList.traverseList;
+
 public class DeleteRepeatedElementsInList {
 
+    private static Node deleteRepeatedElement(Node first) {
+        Node prev_slow = first;
+        Node slow      = first;
+        Node fast      = first.next;
+        while (true) {
+            if (slow.num == fast.num) {
+                if (slow == prev_slow) {
+                    return slow.next;
+                }
+                prev_slow.next = slow.next;
+                return first;
+            }
+            prev_slow = slow;
+            slow = slow.next;
+            fast = fast.next == null || fast.next.next == null ? first : fast.next.next;
+        }
+    }
 
+    public static void main(String[] args) {
+        int[] a = {1, 1, 2, 4, 3, 5};
+        Node first = null;
+        for (int x : a) {
+            first = add(first, x);
+        }
+        first = deleteRepeatedElement(first);
+        traverseList(first);
+    }
 }
--- Algorithm/src/vmware/PalindromeIndexing.java
+++ Algorithm/src/vmware/PalindromeIndexing.java
@@ -7,6 +7,21 @@ package vmware;
  */
 public class PalindromeIndexing {
 
+    private static int palindromeIndexing(String s) {
+        int j = s.length() - 1;
+        for (int i = 0; i <= j / 2; i++) {
+            if (s.charAt(i) != s.charAt(j - i)) {
+                if (s.charAt(i + 1) == s.charAt(j - i) && s.charAt(i + 2) == s.charAt(j - i - 1)) {
+                    return i;
+                }
+                if (s.charAt(i) == s.charAt(j - i - 1)) {
+                    return j - i;
+                }
+            }
+        }
+        return -1;
+    }
+
     private static boolean isPalindrome(String s, int low, int high) {
         while (low <= high) {
             if (s.charAt(low) != s.charAt(high)) {
