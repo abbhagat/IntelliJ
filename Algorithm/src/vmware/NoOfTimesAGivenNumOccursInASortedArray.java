4e82b24b90b9980fe4f645db3490cf2bd067b2d5
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-15 08:19:22 +0530

    Interview

--- Algorithm/src/vmware/NoOfTimesAGivenNumOccursInASortedArray.java
+++ Algorithm/src/vmware/NoOfTimesAGivenNumOccursInASortedArray.java
@@ -2,24 +2,24 @@ package vmware;
 
 public class NoOfTimesAGivenNumOccursInASortedArray {
 
-    private static int binarySearch(int[] a, int low, int high, int n) {
+    private static int binarySearchStartIndex(int[] a, int low, int high, int n) {
         if (low <= high) {
             int mid = (low + high) / 2;
             if ((mid == 0 || a[mid - 1] != n) && a[mid] == n) {
                 return mid;
             }
-            return a[mid] < n ? binarySearch(a, mid + 1, high, n) : binarySearch(a, low, mid - 1, n);
+            return a[mid] < n ? binarySearchStartIndex(a, mid + 1, high, n) : binarySearchStartIndex(a, low, mid - 1, n);
         }
         return a.length - 1;
     }
 
-    private static int binarySearch1(int[] a, int low, int high, int n) {
+    private static int binarySearchEndIndex(int[] a, int low, int high, int n) {
         if (low <= high) {
             int mid = (low + high) / 2;
             if ((mid == 0 || a[mid - 1] == n) && a[mid] != n) {
                 return mid - 1;
             }
-            return a[mid] <= n ? binarySearch1(a, mid + 1, high, n) : binarySearch1(a, low, mid - 1, n);
+            return a[mid] <= n ? binarySearchEndIndex(a, mid + 1, high, n) : binarySearchEndIndex(a, low, mid - 1, n);
         }
         return a.length - 1;
     }
@@ -27,8 +27,8 @@ public class NoOfTimesAGivenNumOccursInASortedArray {
     public static void main(String[] args) {
         int[] a = {1, 1, 2, 2, 2, 2, 2, 2, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5};
         int n = 4;
-        int startIndex = binarySearch (a, 0, a.length - 1, n);
-        int endIndex   = binarySearch1(a, startIndex + 1, a.length - 1, n);
+        int startIndex = binarySearchStartIndex(a, 0, a.length - 1, n);
+        int endIndex   = binarySearchEndIndex(a, startIndex + 1, a.length - 1, n);
         System.out.println(startIndex == endIndex ? 1 : endIndex - startIndex + 1);
     }
 }
