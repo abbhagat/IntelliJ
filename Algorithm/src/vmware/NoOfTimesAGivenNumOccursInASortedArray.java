2e03d7db41918f4844c2a591f3a13a92f4f77c98
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-15 08:45:59 +0530

    Interview

--- Algorithm/src/vmware/NoOfTimesAGivenNumOccursInASortedArray.java
+++ Algorithm/src/vmware/NoOfTimesAGivenNumOccursInASortedArray.java
@@ -4,9 +4,6 @@ public class NoOfTimesAGivenNumOccursInASortedArray {
 
     private static int binarySearchStartIndex(int[] a, int low, int high, int n) {
         if (low <= high) {
-            if (a[low] == a[high] & a[low] == n) {
-                return low;
-            }
             int mid = (low + high) / 2;
             if ((mid == 0 || a[mid - 1] != n) && a[mid] == n) {
                 return mid;
@@ -18,27 +15,29 @@ public class NoOfTimesAGivenNumOccursInASortedArray {
 
     private static int binarySearchEndIndex(int[] a, int low, int high, int n) {
         if (low <= high) {
-            if (a[low] == a[high] & a[high] == n) {
-                return high;
-            }
             int mid = (low + high) / 2;
             if ((mid == 0 || a[mid - 1] == n) && a[mid] != n) {
                 return mid - 1;
             }
             return a[mid] <= n ? binarySearchEndIndex(a, mid + 1, high, n) : binarySearchEndIndex(a, low, mid - 1, n);
         }
-        return -1;
+        return a.length - 1;
     }
 
     public static void main(String[] args) {
-        int[] a = {1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8 , 8, 8, 8, 9, 9, 9, 10};
-        int n = 9;
-        int startIndex = binarySearchStartIndex(a, 0, a.length - 1, n);
-        int endIndex   = binarySearchEndIndex(a, startIndex + 1, a.length - 1, n);
-        if (startIndex == endIndex & startIndex == -1) {
-            System.out.println("Element Not Found");
+        int[] a = {1, 1, 2, 2, 2};
+        int n = 2;
+        int low = 0, high = a.length - 1;
+        if (a[low] == a[high] & a[low] == n) {
+            System.out.println(high - low + 1);
         } else {
-            System.out.println(startIndex == endIndex ? 1 : endIndex - startIndex + 1);
+            int startIndex = binarySearchStartIndex(a, 0, a.length - 1, n);
+            if (startIndex == -1) {
+                System.out.println("Element Not Found");
+            } else {
+                int endIndex   = binarySearchEndIndex(a, startIndex + 1, a.length - 1, n);
+                System.out.println(startIndex == endIndex ? 1 : endIndex - startIndex + 1);
+            }
         }
     }
 }
