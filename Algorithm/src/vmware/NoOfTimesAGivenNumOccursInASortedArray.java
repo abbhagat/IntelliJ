ccc07cee78d31f99674ea88199e4e98c846662fa
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-15 08:34:39 +0530

    Interview

--- Algorithm/src/vmware/NoOfTimesAGivenNumOccursInASortedArray.java
+++ Algorithm/src/vmware/NoOfTimesAGivenNumOccursInASortedArray.java
@@ -4,31 +4,41 @@ public class NoOfTimesAGivenNumOccursInASortedArray {
 
     private static int binarySearchStartIndex(int[] a, int low, int high, int n) {
         if (low <= high) {
+            if (a[low] == a[high] & a[low] == n) {
+                return low;
+            }
             int mid = (low + high) / 2;
             if ((mid == 0 || a[mid - 1] != n) && a[mid] == n) {
                 return mid;
             }
             return a[mid] < n ? binarySearchStartIndex(a, mid + 1, high, n) : binarySearchStartIndex(a, low, mid - 1, n);
         }
-        return a.length - 1;
+        return -1;
     }
 
     private static int binarySearchEndIndex(int[] a, int low, int high, int n) {
         if (low <= high) {
+            if (a[low] == a[high] & a[high] == n) {
+                return high;
+            }
             int mid = (low + high) / 2;
             if ((mid == 0 || a[mid - 1] == n) && a[mid] != n) {
                 return mid - 1;
             }
             return a[mid] <= n ? binarySearchEndIndex(a, mid + 1, high, n) : binarySearchEndIndex(a, low, mid - 1, n);
         }
-        return a.length - 1;
+        return -1;
     }
 
     public static void main(String[] args) {
-        int[] a = {1, 1, 2, 2, 2, 2, 2, 2, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5};
-        int n = 4;
+        int[] a = {1, 1, 1, 1, 1, 1, 1};
+        int n = 1;
         int startIndex = binarySearchStartIndex(a, 0, a.length - 1, n);
         int endIndex   = binarySearchEndIndex(a, startIndex + 1, a.length - 1, n);
-        System.out.println(startIndex == endIndex ? 1 : endIndex - startIndex + 1);
+        if (startIndex == endIndex & startIndex == -1) {
+            System.out.println("Element Not Found");
+        } else {
+            System.out.println(startIndex == endIndex ? 1 : endIndex - startIndex + 1);
+        }
     }
 }
