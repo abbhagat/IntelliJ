67d206c81731a3a76ba462056ea0aa26e56df82d
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-20 15:48:21 +0530

    Interview

--- Algorithm/src/vmware/NextPalindromeNumber.java
+++ Algorithm/src/vmware/NextPalindromeNumber.java
@@ -2,15 +2,20 @@ package vmware;
 
 public class NextPalindromeNumber {
 
+    private static String reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
     private static String findNextPalindrome(int n) {
         String s = Integer.toString(n);
         int mid = (s.length() - 1) / 2;
         s = s.substring(0, mid + 1);
-        String rev =  s.length() % 2 == 1 ? new StringBuilder(s.substring(0, mid)).reverse().toString() : new StringBuilder(s).reverse().toString();
+        String rev = s.length() % 2 == 1 ? reverse(s.substring(0, mid)) : reverse(s);
         if (s.charAt(mid) == '9' || Integer.parseInt(s + rev) < n) {
             s = "" + (Integer.parseInt(s) + 1);
         }
-        return s += s.length() % 2 == 1 ? new StringBuilder(s.substring(0, mid)).reverse().toString() : new StringBuilder(s).reverse().toString();
+        s += s.length() % 2 == 1 ? reverse(s.substring(0, mid)) : reverse(s);
+        return s;
     }
 
     public static void main(String[] args) {
