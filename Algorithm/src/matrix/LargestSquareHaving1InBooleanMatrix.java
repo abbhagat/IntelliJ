d633493b32b47f53e5e8da0d1bf504915bafc7ad
abbhagat <abhinaw.kumar7@gmail.com>
2021-10-28 09:01:08 +0530

    Interview

---
+++ Algorithm/src/matrix/LargestSquareHaving1InBooleanMatrix.java
@@ -0,0 +1,53 @@
+package matrix;
+
+import static java.lang.Integer.min;
+
+public class LargestSquareHaving1InBooleanMatrix {
+
+    private static void findMatrix(int[][] M) {
+        int R = M.length, C = M[0].length;
+        int[][] S = new int[R][C];
+
+        for (int i = 0; i < R; i++) {
+            S[i][0] = M[i][0];
+        }
+
+        for (int i = 0; i < C; i++) {
+            S[0][i] = M[0][i];
+        }
+
+        for (int i = 1; i < R; i++) {
+            for (int j = 1; j < C; j++) {
+                S[i][j] = M[i][j] == 1 ? min(S[i][j - 1], min(S[i - 1][j], S[i - 1][j - 1])) + 1 : 0;
+            }
+        }
+        int max = S[0][0], max_i = 0, max_j = 0;
+        for (int i = 1; i < R; i++) {
+            for (int j = 1; j < C; j++) {
+                if (max < S[i][j]) {
+                    max = S[i][j];
+                    max_i = i;
+                    max_j = j;
+                }
+            }
+        }
+
+        for (int i = max_i; i > max_i - max; i--) {
+            for (int j = max_j; j > max_j - max; j--) {
+                System.out.print(M[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int M[][] = {{0, 1, 1, 0, 1},
+                     {1, 1, 0, 1, 0},
+                     {0, 1, 1, 1, 0},
+                     {1, 1, 1, 1, 0},
+                     {1, 1, 1, 1, 1},
+                     {0, 0, 0, 0, 0}
+                    };
+        findMatrix(M);
+    }
+}
