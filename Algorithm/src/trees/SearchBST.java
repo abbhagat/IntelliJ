145c8d62abac8f83f9f6cfcc76c8b7a358ecf421
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-06 22:31:48 +0530

    Interview

--- Algorithm/src/trees/SearchBST.java
+++ Algorithm/src/trees/SearchBST.java
@@ -1,5 +1,7 @@
 package trees;
 
+import static trees.CreateBST.createBST;
+
 public class SearchBST {
 
     public static Node searchBST(Node root, int num) {
@@ -12,7 +14,22 @@ public class SearchBST {
         return null;
     }
 
-    public static void main(String[] args) {
+    public static boolean searchBST(int num, Node root) {
+        if (root != null) {
+            if (root.num == num) {
+                return true;
+            }
+            return num < root.num ? searchBST(num, root.left) : searchBST(num, root.right);
+        }
+        return false;
+    }
 
+    public static void main(String[] args) {
+        int[] a = {15, 10, 20, 8, 12, 16, 25};
+        Node root = null;
+        for (int x : a) {
+            root = createBST(root, x);
+        }
+        System.out.println(searchBST(25, root));
     }
 }
