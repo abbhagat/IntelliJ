9c05bf17cb179f4efbf3222009a40f52d8bd5bc5
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-06 20:55:07 +0530

    Interview

--- Algorithm/src/trees/MaxDistanceBetweenAnyNodes.java
+++ Algorithm/src/trees/MaxDistanceBetweenAnyNodes.java
@@ -7,19 +7,14 @@ public class MaxDistanceBetweenAnyNodes {
     private static int max_sum = Integer.MIN_VALUE;
 
     public static int height(Node root) {
-        int leftHeight = 0, rightHeight = 0;
         if (root == null) {
             return -1;
+        }else{
+            int leftHeight = height(root.left);
+            int rightHeight = height(root.right);
+            int height = leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+            return max(max_sum, height);
         }
-        if (root.left != null) {
-            leftHeight = height(root.left) + 1;
-
-        }
-        if (root.right != null) {
-            rightHeight = height(root.right) + 1;
-
-        }
-        return max(max_sum, leftHeight + rightHeight);
     }
 
     public static void main(String[] args) {
@@ -31,6 +26,6 @@ public class MaxDistanceBetweenAnyNodes {
         root.left.left.left.right = new Node(7);
         root.left.right = new Node(5);
         root.left.right.right = new Node(15);
-        System.out.println(height(root) - 1);
+        System.out.println(height(root));
     }
 }
