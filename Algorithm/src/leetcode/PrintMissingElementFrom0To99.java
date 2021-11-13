8454674a3c3df3178201778521f99856329afafc
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-13 20:19:47 +0530

    Interview

--- Algorithm/src/leetcode/PrintMissingElementFrom0To99.java
+++ Algorithm/src/leetcode/PrintMissingElementFrom0To99.java
@@ -13,11 +13,11 @@ public class PrintMissingElementFrom0To99 {
         int i = 0;
         while (i < isPresent.length) {
             if (!isPresent[i]) {
-                int j = i;
+                int j = i + 1;
                 while (j < isPresent.length && !isPresent[j]) {
                     j++;
                 }
-                System.out.println(j == (i + 1) ? i : i + "-" + (j - 1));
+                System.out.println(j == i + 1 ? i : i + "-" + (j - 1));
                 i = j;
             } else {
                 i++;
