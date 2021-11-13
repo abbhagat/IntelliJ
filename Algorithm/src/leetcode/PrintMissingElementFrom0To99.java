0c4ed951369703faf5ce076489720d06f7c26eab
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-13 12:57:34 +0530

    Interview

--- Algorithm/src/leetcode/PrintMissingElementFrom0To99.java
+++ Algorithm/src/leetcode/PrintMissingElementFrom0To99.java
@@ -4,17 +4,17 @@ public class PrintMissingElementFrom0To99 {
 
     public static void main(String[] args) {
         int[] a = {88, 105, 3, 2, 200, 0, 10};
-        boolean[] b = new boolean[100];
+        boolean[] isPresent = new boolean[100];
         for (int x : a) {
-            if (x >= 0 && x <= b.length) {
-                b[x] = true;
+            if (x >= 0 && x <= isPresent.length) {
+                isPresent[x] = true;
             }
         }
         int i = 0;
-        while (i < b.length) {
-            if (!b[i]) {
-                int j = i + 1;
-                while (j < b.length && !b[j]) {
+        while (i < isPresent.length) {
+            if (!isPresent[i]) {
+                int j = i;
+                while (j < isPresent.length && !isPresent[j]) {
                     j++;
                 }
                 System.out.println(j == (i + 1) ? i : i + "-" + (j - 1));
