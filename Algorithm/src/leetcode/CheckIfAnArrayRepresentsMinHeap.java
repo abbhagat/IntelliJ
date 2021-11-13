ad1a64b6136b9b98c81e8bca756bf1bd2900ad0b
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-13 10:12:56 +0530

    Interview

--- Algorithm/src/leetcode/CheckIfAnArrayRepresentsMinHeap.java
+++ Algorithm/src/leetcode/CheckIfAnArrayRepresentsMinHeap.java
@@ -1,14 +1,14 @@
 package leetcode;
 
-public class CheckIfAnArrayRepresentsMaxHeap {
+public class CheckIfAnArrayRepresentsMinHeap {
 
-    private static boolean isMaxHeap(int[] a, int i, int n) {
+    private static boolean isMinHeap(int[] a, int i, int n) {
         if (i >= (n - 2) / 2) {
             return true;
         }
         int left  = 2 * i + 1;
         int right = 2 * i + 2;
-        if (a[i] >= a[left] && a[i] >= a[right] && isMaxHeap(a, left, n) && isMaxHeap(a, right, n)) {
+        if (a[i] <= a[left] && a[i] <= a[right] && isMinHeap(a, left, n) && isMinHeap(a, right, n)) {
             return true;
         }
         return false;
@@ -17,7 +17,7 @@ public class CheckIfAnArrayRepresentsMaxHeap {
     public static void main(String[] args) {
         int a[] = {90, 15, 10, 7, 12, 2, 7, 3};
         int b[] = {10, 50, 80, 55, 57, 85, 90};
-        System.out.println(isMaxHeap(a, 0, a.length - 1) ? "Yes" : "No");
-        System.out.println(isMaxHeap(b, 0, a.length - 1) ? "Yes" : "No");
+        System.out.println(isMinHeap(b, 0, a.length - 1) ? "Yes" : "No");
+        System.out.println(isMinHeap(a, 0, a.length - 1) ? "Yes" : "No");
     }
 }
