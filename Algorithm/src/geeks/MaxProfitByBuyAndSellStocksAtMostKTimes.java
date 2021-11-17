a1030e9f3cc6efa39d6714d35683949077987927
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-17 08:38:39 +0530

    Interview

--- Algorithm/src/geeks/MaxProfitByBuyAndSellStocksAtMostKTimes.java
+++ Algorithm/src/geeks/MaxProfitByBuyAndSellStocksAtMostKTimes.java
@@ -2,13 +2,13 @@ package geeks;
 
 public class MaxProfitByBuyAndSellStocksAtMostKTimes {
 
-    private static int maxProfit(int price[], int n, int k) {
-        int profit[][] = new int[k + 1][n + 1];
+    private static int maxProfit(int[] price, int n, int k) {
+        int[][] profit = new int[k + 1][n + 1];
         for (int i = 0; i <= k; i++) {    // For day 0, you can't earn money irrespective of how many times you trade
             profit[i][0] = 0;
         }
-        for (int j = 0; j <= n; j++) {  // profit is 0 if we don't do any transaction (i.e. k =0)
-            profit[0][j] = 0;
+        for (int i = 0; i <= n; i++) {  // profit is 0 if we don't do any transaction (i.e. k =0)
+            profit[0][i] = 0;
         }
         for (int i = 1; i <= k; i++) {
             int prevDiff = Integer.MIN_VALUE;
@@ -22,6 +22,12 @@ public class MaxProfitByBuyAndSellStocksAtMostKTimes {
 
     public static void main(String[] args) {
         int[] price = {100, 30, 15, 10, 8, 25, 80};
+        System.out.println("Maximum profit is: " + maxProfit(price, price.length, 2));
+        price = new int[]{90, 80, 70, 60, 50};
+        System.out.println("Maximum profit is: " + maxProfit(price, price.length, 2));
+        price = new int[]{2, 30, 15, 10, 8, 25, 80};
+        System.out.println("Maximum profit is: " + maxProfit(price, price.length, 2));
+        price = new int[]{10, 22, 5, 75, 65, 80};
         System.out.println("Maximum profit is: " + maxProfit(price, price.length, 2));
     }
 }
