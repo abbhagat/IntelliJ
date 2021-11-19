fd23d56d0f69238096aa2a62ad4c4821ea98e9a3
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-19 08:49:05 +0530

    Interview

--- Algorithm/src/vmware/StringCompareToImpl.java
+++ Algorithm/src/vmware/StringCompareToImpl.java
@@ -3,11 +3,7 @@ package vmware;
 public class StringCompareToImpl {
 
     private static int compareTo(String s1, String s2) {
-        if (s1.length() > s2.length()) {
-            return 1;
-        } else if (s1.length() < s2.length()) {
-            return -1;
-        } else {
+        if (s1.length() == s2.length()) {
             int i = 0;
             while (i < s1.length()) {
                 if (s1.charAt(i) == s2.charAt(i)) {
@@ -16,13 +12,14 @@ public class StringCompareToImpl {
                 }
                 return s1.charAt(i) > s2.charAt(i) ? 1 : -1;
             }
+            return 0;
         }
-        return 0;
+        return s1.length() > s2.length() ? 1 : -1;
     }
 
     public static void main(String[] args) {
-        System.out.println("Java.compareTo(Java)  " + compareTo("Java" , "Java"));
+        System.out.println("Java.compareTo(Java)  " + compareTo("Java",  "Java"));
         System.out.println("Javaa.compareTo(Java) " + compareTo("Javaa", "Java"));
-        System.out.println("Java.compareTo(Javaa) " + compareTo("Java" , "Javaa"));
+        System.out.println("Java.compareTo(Javaa) " + compareTo("Java",  "Javaa"));
     }
 }
