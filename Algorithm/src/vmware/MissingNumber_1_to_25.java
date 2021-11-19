a616a87a3bb399793553ac3c9eb29a1c95b818eb
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-19 07:57:13 +0530

    Interview

--- Algorithm/src/vmware/MissingNumber_1_to_25.java
+++ Algorithm/src/vmware/MissingNumber_1_to_25.java
@@ -6,9 +6,10 @@ public class MissingNumber_1_to_25 {
 
     public static void main(String[] args) {
         int[] a = {1, 2, 3, 3, 5};
+        int n = a.length;
         Arrays.sort(a);
         int y = 0;
-        for (int i = 1; i < a.length; i++) {
+        for (int i = 1; i < n; i++) {
             if (a[i - 1] == a[i]) {
                 y = a[i];
                 break;
@@ -19,7 +20,7 @@ public class MissingNumber_1_to_25 {
             sum1 += x;
         }
         sum1 -= y;
-        int sum2 = ((a[0] + a[a.length - 1]) * (a.length)) / 2;
+        int sum2 = ((a[0] + a[n - 1]) * n) / 2;
         System.out.println(sum2 - sum1);
     }
 }
