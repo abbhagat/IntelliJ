5442e9d26b2f0751a1a7e93f8ffb04b31d16f5e8
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-21 11:03:55 +0530

    Interview

--- Algorithm/src/geeks/ArrayUnionIntersection.java
+++ Algorithm/src/geeks/ArrayUnionIntersection.java
@@ -7,8 +7,8 @@ public class ArrayUnionIntersection {
 
     public static void main(String[] args) {
         Set<Integer> set = new HashSet<>();
-        int a[] = {12, 55, 60, 72, 89, 35, 35, 46, 12, 46, 100};
-        int b[] = {12, 15, 30, 23, 10, 60, 35, 89, 92, 100, 10, 23, 45, 60};
+        int[] a = {12, 55, 60, 72, 89, 35, 35, 46, 12, 46, 100};
+        int[] b = {12, 15, 30, 23, 10, 60, 35, 89, 92, 100, 10, 23, 45, 60};
         for (int x : a) {
             set.add(x);
         }
