4075d1bac8c0f6029549563341568cbcdf44dfd3
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-21 11:13:33 +0530

    Interview

--- Algorithm/src/geeks/CountAllPermutationsOfVowels.java
+++ Algorithm/src/geeks/CountAllPermutationsOfVowels.java
@@ -8,29 +8,33 @@ public class CountAllPermutationsOfVowels {
         long[] current = {1, 1, 1, 1, 1};
         for (int i = 1; i < n; i++) {
             long[] next = {0, 0, 0, 0, 0};
-            // Each vowel 'a' may only be followed by an 'e'.
-            next[1] += current[0];
-            // Each vowel 'e' may only be followed by an 'a' or an 'i'.
-            next[0] += current[1];
+
+            next[1] += current[0];           // Each vowel 'a' may only be followed by an 'e'.
+
+            next[0] += current[1];          // Each vowel 'e' may only be followed by an 'a' or an 'i'.
             next[2] += current[1];
-            // Each vowel 'i' may not be followed by another 'i'.
-            next[0] += current[2];
+
+            next[0] += current[2];         // Each vowel 'i' may not be followed by another 'i'.
             next[1] += current[2];
             next[3] += current[2];
             next[4] += current[2];
-            // Each vowel 'o' may only be followed by an 'i' or'u'.
-            next[2] += current[3];
+
+            next[2] += current[3];        // Each vowel 'o' may only be followed by an 'i' or'u'.
             next[4] += current[3];
-            // Each vowel 'u' may only be followed by an 'a'.
-            next[0] += current[4];
-            // assign
+
+            next[0] += current[4];       // Each vowel 'u' may only be followed by an 'a'.
+
             current[0] = next[0] % MOD;
             current[1] = next[1] % MOD;
             current[2] = next[2] % MOD;
             current[3] = next[3] % MOD;
             current[4] = next[4] % MOD;
         }
-        return (int) ((current[0] + current[1] + current[2] + current[3] + current[4]) % MOD);
+        long sum = 0;
+        for(long x : current){
+            sum += x;
+        }
+        return (int) (sum % MOD);
     }
 
     public static void main(String[] args) {
