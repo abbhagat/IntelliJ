c1ccdd9bb84ccab38ee5123f9a2953b6c7b07634
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-22 09:51:42 +0530

    Interview

--- Algorithm/src/amazon/AddTwoBinaryStringAndPrintTheResultInBinaryString.java
+++ Algorithm/src/amazon/AddTwoBinaryStringAndPrintTheResultInBinaryString.java
@@ -1,8 +1,4 @@
-package arrays;
-
-import static arrays.ToDecimalFromBinary.toDecimal;
-import static geeks.ParseIntImplementation.parseInt;
-import static geeks.ToBinaryFromDecimal.toBinary;
+package amazon;
 
 public class AddTwoBinaryStringAndPrintTheResultInBinaryString {
 
@@ -23,11 +19,6 @@ public class AddTwoBinaryStringAndPrintTheResultInBinaryString {
     public static void main(String[] args) {
         String x = "1111";
         String y = "1010";
-        int a = toDecimal(parseInt(x));
-        int b = toDecimal(parseInt(y));
-        int c = a + b;
-        String z = toBinary(c);
-        System.out.println(z);
         System.out.println(add(x, y));
     }
 }
