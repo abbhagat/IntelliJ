2350d31aaadccfca6aa9b0f1cb107e7501cb79bf
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-23 23:47:24 +0530

    Interview

--- Algorithm/src/amazon/CheckOddOrEvenWithoutModulus.java
+++ Algorithm/src/amazon/CheckOddOrEvenWithoutModulus.java
@@ -1,10 +1,12 @@
 package amazon;
 
-//a%b = a - int(a/b)*b
+// a%b = a - int(a/b)*b
+// n&1 == 0 ? Even : Odd
 
 public class CheckOddOrEvenWithoutModulus {
     public static void main(String[] args) {
         int n = 5;
         System.out.println((n - (n / 2) * 2 == 0) ? "EVEN" : "ODD");
+        System.out.println((n & 1) == 0 ? "EVEN" : "ODD");
     }
 }
