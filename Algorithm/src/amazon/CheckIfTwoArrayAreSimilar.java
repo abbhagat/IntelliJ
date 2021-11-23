e6de452ae03c12485ca38ad11be693882a016c62
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-23 23:43:18 +0530

    Interview

--- Algorithm/src/amazon/CheckIfTwoArrayAreSimilar.java
+++ Algorithm/src/amazon/CheckIfTwoArrayAreSimilar.java
@@ -10,17 +10,16 @@ public class CheckIfTwoArrayAreSimilar {
     public static void main(String[] args) {
         int[] a = {1, 2, 4, 3, 5, 5, 4, 7};
         int[] b = {7, 5, 4, 5, 4, 3, 2, 1};
-        if (a.length != b.length) {
-            System.out.println("Different Array");
-            System.exit(0);
+        List<Integer> al = null;
+        if (a.length == b.length) {
+            al = new ArrayList<>();
+            for (int x : a) {
+                al.add(x);
+            }
+            for (int x : b) {
+                al.remove(Integer.valueOf(x));
+            }
         }
-        List<Integer> al = new ArrayList<>();
-        for (int x : a) {
-            al.add(x);
-        }
-        for (int x : b) {
-            al.remove(Integer.valueOf(x));
-        }
-        System.out.println(al.size() == 0 ? "Same Array" : "Different Array");
+        System.out.println(al != null ? al.isEmpty() ? "Same Array" : "Different Array" : "Different Array");
     }
 }
