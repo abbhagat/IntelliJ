be8232e5a7949e02a066b95d71394d5941755d18
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-25 00:22:36 +0530

    Interview

--- Algorithm/src/matrix/WordSearch.java
+++ Algorithm/src/matrix/WordSearch.java
@@ -3,25 +3,14 @@ package matrix;
 public class WordSearch {
 
     private static char[][] M = {
-            {'A', 'B', 'C', 'E'},
-            {'S', 'F', 'C', 'S'},
-            {'A', 'D', 'E', 'E'}
+            {'A', 'B', 'H', 'I'},
+            {'S', 'F', 'C', 'N'},
+            {'A', 'D', 'W', 'A'}
     };
     private static int ROW = M.length;
     private static int COL = M[0].length;
 
-    private static boolean exist(char[][] M, String word) {
-        for (int i = 0; i < ROW; i++) {
-            for (int j = 0; j < COL; j++) {
-                if (DFS(i, j, M, word, 0)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private static boolean DFS(int row, int col, char[][] M, String word, int index) {
+    private static boolean DFS(int row, int col, String word, int index) {
         if (index >= word.length()) {
             return true;
         }
@@ -33,14 +22,25 @@ public class WordSearch {
         int rowIdx[] = {0, 1, 0, -1};
         int colIdx[] = {1, 0, -1, 0};
         for (int k = 0; k < 4; k++) {
-            if (DFS(row + rowIdx[k], col + colIdx[k], M, word, index + 1))
+            if (DFS(row + rowIdx[k], col + colIdx[k], word, index + 1))
                 return true;
         }
         M[row][col] = t;
         return false;
     }
 
+    private static boolean exist(char[][] M, String word) {
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                if (DFS(i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String args[]) {
-        System.out.println(exist(M, "ABCCED"));
+        System.out.println(exist(M, "ABHINAW"));
     }
 }
