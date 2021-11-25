2973379da6a718ce9b38aaecfdb0415c356269e2
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-25 23:12:44 +0530

    Interview

--- Algorithm/src/stack/RemoveInValidBracket.java
+++ Algorithm/src/stack/RemoveInValidBracket.java
@@ -7,7 +7,10 @@ import java.util.Stack;
 public class RemoveInValidBracket {
 
     private static void removeInvalidParenthesis(String str) {
-        Set<String> set = new HashSet<>();                                             // set to ignore already visited string
+        if(str.isEmpty()){
+            return;
+        }
+        Set<String> set = new HashSet<>();       // set to ignore already visited string
         Stack<String> stack = new Stack<>();
         stack.push(str);
         set.add(str);
