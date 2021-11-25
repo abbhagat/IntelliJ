a3b6c8b3e432d36784871213cd35e22ba3b3bf0e
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-25 19:52:35 +0530

    Interview

---
+++ Algorithm/src/amazon/RomanToInteger.java
@@ -0,0 +1,37 @@
+package amazon;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+
+    private static final Map<Character, Integer> roman = new HashMap<>() {
+        {
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }
+    };
+
+    private static int romanToInt(String s) {
+        int sum = 0, n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
+                sum += roman.get(s.charAt(i + 1)) - roman.get(s.charAt(i));
+                i++;
+            } else {
+                sum += roman.get(s.charAt(i));
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) { ;
+        System.out.print(romanToInt("IX"));
+        System.out.print(romanToInt("MCMIV"));
+    }
+}
