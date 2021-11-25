8ec1bbc913bae4d2b0cf94e7a4fdb85aae4f94b4
abbhagat <abhinaw.kumar7@gmail.com>
2021-11-25 15:44:54 +0530

    Interview

--- Algorithm/src/linkedlist/DeleteRepeatedElementsInList.java
+++ Algorithm/src/linkedlist/DeleteRepeatedElementsInList.java
@@ -1,9 +1,26 @@
 package linkedlist;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static linkedlist.LinkList.add;
 import static linkedlist.TraverseList.traverseList;
 
 public class DeleteRepeatedElementsInList {
+
+    private static Node deleteRepeatedElementUsingMap(Node first) {
+        Map<Integer,Node> map = new HashMap<>();
+        Node prev = null;
+        for(Node temp = first; temp != null; temp = temp.next){
+            if(map.containsKey(temp.num)){
+                prev.next = temp.next;
+                return first;
+            }
+            map.put(temp.num, temp);
+            prev = temp;
+        }
+        return first;
+    }
 
     private static Node deleteRepeatedElement(Node first) {
         Node prev_slow = first;
@@ -29,7 +46,7 @@ public class DeleteRepeatedElementsInList {
         for (int x : a) {
             first = add(first, x);
         }
-        first = deleteRepeatedElement(first);
+        first = deleteRepeatedElementUsingMap(first);
         traverseList(first);
     }
 }
