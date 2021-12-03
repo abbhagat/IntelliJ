fd99b7357e6799b346afd246377627c6cc08839a
abbhagat <abhinaw.kumar7@gmail.com>
2021-12-03 11:08:57 +0530

    Interview

--- Algorithm/src/searching/FindMinInARotatedSortedArray.java
+++ Algorithm/src/searching/FindMinInARotatedSortedArray.java
@@ -16,22 +16,40 @@ public class FindMinInARotatedSortedArray {
         return a[high];
     }
 
+    private static int findMax(int[] a, int low, int high) {
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (a[mid] == a[high]) {
+                high--;
+            } else if (a[mid] > a[high]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return a[high];
+    }
+
     public static void main(String[] args) {
         int arr1[] = {5, 6, 1, 2, 3, 4};
         int n1 = arr1.length;
         System.out.println("The minimum element is " + findMin(arr1, 0, n1 - 1));
+        System.out.println("The maximum element is " + findMax(arr1, 0, n1 - 1));
 
         int arr2[] = {1, 2, 3, 4};
         int n2 = arr2.length;
         System.out.println("The minimum element is " + findMin(arr2, 0, n2 - 1));
+        System.out.println("The maximum element is " + findMax(arr2, 0, n2 - 1));
 
         int arr3[] = {1};
         int n3 = arr3.length;
         System.out.println("The minimum element is " + findMin(arr3, 0, n3 - 1));
+        System.out.println("The maximum element is " + findMax(arr3, 0, n3 - 1));
 
         int arr4[] = {1, 2};
         int n4 = arr4.length;
         System.out.println("The minimum element is " + findMin(arr4, 0, n4 - 1));
+        System.out.println("The maximum element is " + findMax(arr4, 0, n4 - 1));
 
         int arr5[] = {2, 1};
         int n5 = arr5.length;
