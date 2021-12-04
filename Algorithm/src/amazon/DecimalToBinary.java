b99511989aeb07d36666333b2b8691453d70b961
abbhagat <abhinaw.kumar7@gmail.com>
2021-12-04 10:45:20 +0530

    Interview

--- Algorithm/src/amazon/DecimalToBinary.java
+++ Algorithm/src/amazon/DecimalToBinary.java
@@ -1,19 +1,19 @@
 package amazon;
 
-public class BinaryToDecimal {
+public class DecimalToBinary {
 
     private static int carry = 0;
 
-    private static StringBuilder binaryToDecimal(int n, StringBuilder result) {
+    private static StringBuilder decimalToBinary(int n, StringBuilder result) {
         if (n != 0) {
             carry = n / 2;
             result.insert(0, n % 2);
-            binaryToDecimal(n / 2, result);
+            decimalToBinary(n / 2, result);
         }
         return result;
     }
 
-    private static String binaryToDecimal(int n) {
+    private static String decimalToBinary(int n) {
         StringBuilder result = new StringBuilder();
         int carry = 0;
         while (n != 0) {
@@ -25,17 +25,17 @@ public class BinaryToDecimal {
     }
 
     public static void main(String[] args) {
-        System.out.println(binaryToDecimal(5));
-        System.out.println(binaryToDecimal(15));
-        System.out.println(binaryToDecimal(32));
+        System.out.println(decimalToBinary(5));
+        System.out.println(decimalToBinary(15));
+        System.out.println(decimalToBinary(32));
         carry = 0;
-        StringBuilder result = binaryToDecimal(5, new StringBuilder());
+        StringBuilder result = decimalToBinary(5, new StringBuilder());
         System.out.println(carry == 0 ? result.toString() : "1" + result);
         carry = 0;
-        result = binaryToDecimal(15, new StringBuilder());
+        result = decimalToBinary(15, new StringBuilder());
         System.out.println(carry == 0 ? result.toString() : "1" + result);
         carry = 0;
-        result = binaryToDecimal(32, new StringBuilder());
+        result = decimalToBinary(32, new StringBuilder());
         System.out.println(carry == 0 ? result.toString() : "1" + result);
     }
 }
