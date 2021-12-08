2f30fb7d459157fa2dbbfd9becb2e843f91b2c3c
abbhagat <abhinaw.kumar7@gmail.com>
2021-12-08 08:18:23 +0530

    Interview

--- Algorithm/src/matrix/AreaOfLargestIslandIn2DMatrix.java
+++ Algorithm/src/matrix/AreaOfLargestIslandIn2DMatrix.java
@@ -4,6 +4,11 @@ import static java.lang.Integer.max;
 
 /*
 
+Time complexity: O(ROW * COL).
+At worst case all the cells will be visited so time complexity is O(ROW * COL).
+Auxiliary Space:O(ROW * COL).
+To store the visited nodes O(ROW * COL) space is needed
+
 Consider a matrix with rows and columns, where each cell contains either a ‘0’ or a ‘1’ and any cell containing a 1 is called a filled cell.
 Two cells are said to be connected if they are adjacent to each other horizontally, vertically, or diagonally.
 If one or more filled cells are also connected, they form a region. find the length of the largest region.
--- Algorithm/src/matrix/SpiralMatrixPrint.java
+++ Algorithm/src/matrix/SpiralMatrixPrint.java
@@ -6,23 +6,29 @@ public class SpiralMatrixPrint {
         int[][] M = new int[][]{
                 {1, 2, 3, 4},
                 {0, 1, 1, 5},
-                {9, 8, 7, 6}
-                //{4, 5, 9, 10}
+                {9, 8, 7, 6},
+                {4, 5, 9, 10}
         };
         int R1 = 0, R2 = M.length - 1;
         int C1 = 0, C2 = M[0].length - 1;
+        int k = 1;
+        int N = M.length * M[0].length;
         while (R1 <= R2 && C1 <= C2) {
-            for (int i = C1; i <= C2; i++) {
+            for (int i = C1; i <= C2 && k <= N; i++) {
                 System.out.print(M[R1][i]);
+                k++;
             }
-            for (int i = R1 + 1; i <= R2; i++) {
+            for (int i = R1 + 1; i <= R2 && k <= N; i++) {
                 System.out.print(M[i][C2]);
+                k++;
             }
-            for (int i = C2 - 1; i >= C1; i--) {
+            for (int i = C2 - 1; i >= C1 && k <= N; i--) {
                 System.out.print(M[R2][i]);
+                k++;
             }
-            for (int i = R2 - 1; i > R1; i--) {
+            for (int i = R2 - 1; i > R1 && k <= N; i--) {
                 System.out.print(M[i][C1]);
+                k++;
             }
             R1++;
             R2--;
