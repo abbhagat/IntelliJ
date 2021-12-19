aed0149267f866bf4134472e714e83f2778f4a27
abbhagat <abhinaw.kumar7@gmail.com>
2021-12-19 13:35:54 +0530

    Interview

--- Algorithm/src/searching/FindMaxInARotatedSortedArray.java
+++ Algorithm/src/searching/FindMaxInARotatedSortedArray.java
@@ -12,7 +12,7 @@ public class FindMaxInARotatedSortedArray {
             } else if (a[mid] > a[mid + 1]) {
                 return a[mid];
             }else if(a[low] > a[mid]){
-                high = mid - 1;
+                high = mid;
             }else{
                 low = mid + 1;
             }
@@ -22,5 +22,6 @@ public class FindMaxInARotatedSortedArray {
 
     public static void main(String[] args) {
         System.out.println(findMax(new int[]{4, 5, 6, 7, 1, 2, 3}));
+        System.out.println(findMax(new int[]{5, 6, 1, 1, 1, 2, 3, 4, 4, 5}));
     }
 }
