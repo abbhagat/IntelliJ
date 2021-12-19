7e561cae0daae65b245fb4510fd3b7e4bfcfb5d6
abbhagat <abhinaw.kumar7@gmail.com>
2021-12-19 11:34:31 +0530

    Interview

--- Algorithm/src/vmware/StringCompareToImpl.java
+++ Algorithm/src/vmware/StringCompareToImpl.java
@@ -4,13 +4,10 @@ public class StringCompareToImpl {
 
     private static int compareTo(String s1, String s2) {
         if (s1.length() == s2.length()) {
-            int i = 0;
-            while (i < s1.length()) {
-                if (s1.charAt(i) == s2.charAt(i)) {
-                    i++;
-                    continue;
+            for (int i = 0; i < s1.length(); i++) {
+                if (s1.charAt(i) != s2.charAt(i)) {
+                    return s1.charAt(i) > s2.charAt(i) ? 1 : -1;
                 }
-                return s1.charAt(i) > s2.charAt(i) ? 1 : -1;
             }
             return 0;
         }
