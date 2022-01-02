0c00673a50e1b37004148ccce39d9db5d3b9d7bc
abbhagat <abhinaw.kumar7@gmail.com>
2022-01-02 09:38:18 +0530

    Interview

--- Algorithm/src/trees/TreeTraversal.java
+++ Algorithm/src/trees/TreeTraversal.java
@@ -80,7 +80,6 @@ public class TreeTraversal {
     }
 
     public static void main(String[] args) {
-        BinaryTree BT = new BinaryTree();
         Node root = null;
         int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         for (int x : a) {
