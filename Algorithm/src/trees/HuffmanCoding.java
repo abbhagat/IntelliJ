4550a520bbcded36413a3730f587ecf4aedbbbed
abbhagat <abhinaw.kumar7@gmail.com>
2022-01-05 08:31:38 +0530

    Interview

---
+++ Algorithm/src/trees/HuffmanCoding.java
@@ -0,0 +1,36 @@
+package trees;
+
+import java.util.LinkedList;
+
+public class HuffmanCoding {
+
+    private static String result = "";
+
+    public static void encode(Node root, String str) {
+        for (char x : str.toCharArray()) {
+            LinkedList<Character> list = new LinkedList<>();
+            findPath(root, x, list, ' ');
+        }
+        System.out.println(result);
+    }
+
+    public static void findPath(Node root, char c, LinkedList<Character> list, char x) {
+        if (root == null) {
+            return;
+        }
+        list.add(x);
+        if (root.num == c) {
+            list.removeFirst();
+            list.forEach(i -> result = result + i);
+            return;
+        }
+        findPath(root.left, c, list, '0');
+        list.removeLast();
+        findPath(root.right, c, list, '1');
+        list.removeLast();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
