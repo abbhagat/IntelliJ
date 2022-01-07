db752fbd41be23b5872c8115f25de43fd98427a9
abbhagat <abhinaw.kumar7@gmail.com>
2022-01-07 10:26:18 +0530

    Interview

--- Algorithm/src/oracle/ReplaceAllSpacesInAStringWithPercentage.java
+++ Algorithm/src/oracle/ReplaceAllSpacesInAStringWithPercentage.java
@@ -3,9 +3,9 @@ package oracle;
 public class ReplaceAllSpacesInAStringWithPercentage {
 
     public static void main(String[] args) {
-        String s = "";
-        for (char x : "Java is an OOP . ".toCharArray()) {
-            s += (x != ' ') ? x : "%20";
+        StringBuilder s = new StringBuilder();
+        for (char x : "Java is an OOP.".toCharArray()) {
+            s.append(x != ' ' ? x : "%20");
         }
         System.out.println(s);
     }
