fc53c75b18e28affd72bc81b17ce4699d49c601f
abbhagat <abhinaw.kumar7@gmail.com>
2022-01-07 22:05:24 +0530

    Interview

--- Algorithm/src/leetcode/SmallestSumAfterRemovingKDigit.java
+++ Algorithm/src/leetcode/SmallestSumAfterRemovingKDigit.java
@@ -1,23 +1,23 @@
 package leetcode;
 
-import static java.lang.Integer.max;
+import static java.lang.Integer.min;
 
 /**
  * Given a positive number N, the target is to find the largest number that can be formed after removing any K digits from N.
  * Examples:
  * Input: N = 6358, K = 1
- * Output: 658
+ * Output: 358
  * Input: N = 2589, K = 2
- * Output: 89
+ * Output: 25
  */
 public class SmallestSumAfterRemovingKDigit {
 
     private static int minNumber(int n, int k) {
         for (int j = 1; j <= k; j++) {
             int result = Integer.MAX_VALUE;
-            for (int i = 1; n / i > 0; i *= 10) {               // Remove the last digit after every iteration
+            for (int i = 1; n / i != 0; i *= 10) {               // Remove the last digit after every iteration
                 int temp = (n / (i * 10)) * i + (n % i);       // Store the numbers formed after removing every digit once
-                result = max(result, temp);
+                result = min(result, temp);
             }
             n = result;
         }
