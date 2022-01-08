42b38f35b6c72f073158da41a9063d23b6cfb003
abbhagat <abhinaw.kumar7@gmail.com>
2022-01-08 22:50:05 +0530

    Interview

--- Java/src/innerclass/I.java
+++
@@ -1,5 +0,0 @@
-package innerclass;
-
-interface I {
-    public void cook();
-}
--- Java/src/string/StringExpressionToMathExpression.java
+++
@@ -1,9 +0,0 @@
-package string;
-
-import javax.script.ScriptEngineManager;
-
-public class StringExpressionToMathExpression {
-    public static void main(String[] args) throws Exception {
-        System.out.println(new ScriptEngineManager().getEngineByName("nashorn").eval("40^3+2").toString());
-    }
-}
