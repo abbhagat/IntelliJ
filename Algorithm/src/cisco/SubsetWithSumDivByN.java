ccee35a2c20d489779d5184bb05d8ca986221598
abbhagat <abhinaw.kumar7@gmail.com>
2022-01-09 07:06:08 +0530

    Interview

---
+++ Algorithm/src/cisco/SubsetWithSumDivByN.java
@@ -0,0 +1,35 @@
+package cisco;
+
+import java.util.LinkedList;
+
+public class SubsetWithSumDivByN {
+
+    static class BooleanWrapper {
+        boolean flag;
+    }
+
+    private static void findSubset(int[] a, int index, LinkedList<Integer> list, int k, BooleanWrapper booleanWrapper) {
+        if (index == a.length) {
+            if (!list.isEmpty()) {
+                int sum = 0;
+                for (int x : list) {
+                    sum += x;
+                }
+                if (sum % k == 0) {
+                    booleanWrapper.flag = true;
+                }
+            }
+            return;
+        }
+        findSubset(a, index + 1, list, k, booleanWrapper);
+        list.add(a[index]);
+        findSubset(a, index + 1, list, k, booleanWrapper);
+        list.removeLast();
+    }
+
+    public static void main(String[] args) {
+        BooleanWrapper booleanWrapper = new BooleanWrapper();
+        findSubset(new int[]{1, 2, 4}, 0, new LinkedList<>(), 3, booleanWrapper);
+        System.out.println(booleanWrapper.flag);
+    }
+}
