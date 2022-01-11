328135ae32e429a25d24730f79c3f00f7b6960bc
abbhagat <abhinaw.kumar7@gmail.com>
2022-01-11 07:32:48 +0530

    Interview

--- Algorithm/src/dynamicProgramming/EggDroppingPuzzle.java
+++ Algorithm/src/dynamicProgramming/EggDroppingPuzzle.java
@@ -1,8 +1,5 @@
 package dynamicProgramming;
 
-import static java.lang.Integer.max;
-import static java.lang.Integer.min;
-
 public class EggDroppingPuzzle {
 
     private static int eggDrop(int eggs, int floor) {
@@ -12,16 +9,12 @@ public class EggDroppingPuzzle {
         if (eggs == 1) {
             return floor;
         }
-        int min = Integer.MAX_VALUE;
-        for (int i = 1; i <= floor; i++) {
-            int result = max(eggDrop(eggs - 1, i - 1), eggDrop(eggs, floor - i));
-            min = min(min, result);
-        }
-        return min + 1;
+        int mid = (floor + 1) / 2;
+        return 1 + (floor - mid > mid - 1 ? eggDrop(eggs, floor - mid) : eggDrop(eggs - 1, mid - 1));
     }
 
     public static void main(String args[]) {
-        int egg = 2, floor = 10;
-        System.out.print("Minimum number of trials in worst case with " + egg + " eggs and " + floor + " floors is " + eggDrop(egg, floor));
+        System.out.println(eggDrop(2, 10));
+        System.out.println(eggDrop(2, 36));
     }
 }
