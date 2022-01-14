ff26711572c3e8ac602e945ead9d3c13f96bc7b7
abbhagat <abhinaw.kumar7@gmail.com>
2022-01-14 08:47:21 +0530

    Interview

--- Algorithm/src/oracle/HCF.java
+++ Algorithm/src/oracle/HCF.java
@@ -9,12 +9,18 @@ public class HCF {
         return (y % x == 0) ? x : findHCF(y % x, x);
     }
 
+    private static int findGCD(int x, int y) {
+        return y == 0 ? x : findGCD(y, x % y);
+    }
+
     public static void main(String[] args) {
         int[] a = {10, 150, 30, 50, 40, 60};
-        int hcf = a[0];
+        int hcf = a[0], gcd = a[0];
         for (int i = 1; i < a.length; i++) {
             hcf = findHCF(min(hcf, a[i]), max(hcf, a[i]));
+            gcd = findGCD(min(hcf, a[i]), max(hcf, a[i]));
         }
         System.out.println("HCF := " + hcf);
+        System.out.println("HCF := " + gcd);
     }
 }
