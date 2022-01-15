489274e809c0934aa7c613a211fa79967c3748e5
abbhagat <abhinaw.kumar7@gmail.com>
2022-01-15 16:49:58 +0530

    Interview

--- Algorithm/src/trees/MaxDistanceBetweenAnyNodes.java
+++ Algorithm/src/trees/MaxDistanceBetweenAnyNodes.java
@@ -17,17 +17,17 @@ public class MaxDistanceBetweenAnyNodes {
     }
 
     public static void main(String[] args) {
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.right = new Node(3);
-        root.left.left = new Node(4);
-        root.left.left.left = new Node(6);
-        root.left.left.left.right = new Node(7);
-        root.left.left.left.right.left = new Node(17);
+        Node root                           = new Node(1);
+        root.left                           = new Node(2);
+        root.right                          = new Node(3);
+        root.left.left                      = new Node(4);
+        root.left.left.left                 = new Node(6);
+        root.left.left.left.right           = new Node(7);
+        root.left.left.left.right.left      = new Node(17);
         root.left.left.left.right.left.left = new Node(27);
-        root.left.right = new Node(5);
-        root.left.right.right = new Node(15);
-        root.left.right.right.right = new Node(25);
+        root.left.right                     = new Node(5);
+        root.left.right.right               = new Node(15);
+        root.left.right.right.right         = new Node(25);
         getMaxDist(root);
         System.out.println(max_dist);
     }
