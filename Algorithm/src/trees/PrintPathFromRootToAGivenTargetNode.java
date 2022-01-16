4ffde2cd455fee2da44ef7e4a227ca8137c4ba99
abbhagat <abhinaw.kumar7@gmail.com>
2022-01-16 18:48:31 +0530

    Interview

--- Algorithm/src/trees/PrintPathFromRootToAGivenTargetNode.java
+++ Algorithm/src/trees/PrintPathFromRootToAGivenTargetNode.java
@@ -31,12 +31,14 @@ public class PrintPathFromRootToAGivenTargetNode {
     }
 
     public static void main(String[] args) {
-        Node root           = new Node(1);
-        root.left           = new Node(2);
-        root.right          = new Node(3);
-        root.left.left      = new Node(4);
-        root.left.right     = new Node(5);
-        root.left.left.left = new Node(7);
-        printPath(root, 5);
+        Node root                = new Node(1);
+        root.left                = new Node(2);
+        root.right               = new Node(3);
+        root.left.left           = new Node(4);
+        root.left.right          = new Node(5);
+        root.right.left          = new Node(6);
+        root.right.right         = new Node(7);
+        root.right.left.right    = new Node(11);
+        printPath(root, 11);
     }
 }
