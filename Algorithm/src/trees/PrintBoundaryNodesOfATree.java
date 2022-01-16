f633821a9758c722a29677df9c50dbc2cf52c5ca
abbhagat <abhinaw.kumar7@gmail.com>
2022-01-16 17:47:24 +0530

    Interview

--- Algorithm/src/trees/PrintBoundaryNodesOfATree.java
+++ Algorithm/src/trees/PrintBoundaryNodesOfATree.java
@@ -26,18 +26,14 @@ public class PrintBoundaryNodesOfATree {
 
     private static void printLeftEdgeNodes(Node root) {
         if (root.left != null) {
-            if (root.left != null || root.right != null) {
-                System.out.print(root.num + " ");
-            }
+            System.out.print(root.num + " ");
             printLeftEdgeNodes(root.left);
         }
     }
 
     private static void printRightEdgeNodes(Node root) {
         if (root.right != null) {
-            if (root.left != null || root.right != null) {
-                System.out.print(root.num + " ");
-            }
+            System.out.print(root.num + " ");
             printRightEdgeNodes(root.right);
         }
     }
@@ -49,14 +45,17 @@ public class PrintBoundaryNodesOfATree {
     }
 
     public static void main(String[] args) {
-        Node root             = new Node(20);
-        root.left             = new Node(8);
-        root.right            = new Node(22);
-        root.left.left        = new Node(4);
-        root.left.right       = new Node(12);
-        root.left.right.left  = new Node(10);
-        root.left.right.right = new Node(14);
-        root.right.right      = new Node(25);
+        Node root                  = new Node(20);
+        root.left                  = new Node(8);
+        root.right                 = new Node(22);
+        root.left.left             = new Node(4);
+        root.left.right            = new Node(12);
+        root.left.right.left       = new Node(10);
+        root.left.right.right      = new Node(14);
+        root.right.left            = new Node(15);
+        root.right.right           = new Node(25);
+        root.right.left.left       = new Node(30);
+        root.right.left.right      = new Node(17);
         printBoundary(root);
     }
 }
