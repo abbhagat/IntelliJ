af26661f33c1df0d9f7763e1061530f5c03e39d2
abbhagat <abhinaw.kumar7@gmail.com>
2022-01-26 13:18:59 +0530

    Interview

--- spring-boot-kafka/src/main/java/com/boot/kafka/SpringBootKafkaApplication.java
+++ spring-boot-kafka/src/main/java/com/boot/kafka/SpringBootKafkaApplication.java
@@ -2,12 +2,13 @@ package com.boot.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class SpringBootKafkaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootKafkaApplication.class, args);
     }
-
 }
