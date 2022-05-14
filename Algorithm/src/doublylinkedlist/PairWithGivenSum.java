e39ca77f8d71c451ac57b1f41bfcb6ab73c41b20
abbhagat <abhinaw.kumar7@gmail.com>
2022-05-14 20:45:18 +0530

    Interview

---
+++ Algorithm/src/doublylinkedlist/PairWithGivenSum.java
@@ -0,0 +1,45 @@
+package doublylinkedlist;
+
+import static doublylinkedlist.CreateList.createList;
+import static doublylinkedlist.TraverseList.traverseList;
+
+// Time complexity is O(n)
+// Auxiliary space is O(1)
+public class PairWithGivenSum {
+
+    private static void pairSum(Node head, int x) {
+        Node first = head, second = head;
+        while (second.right != null) {
+            second = second.right;
+        }
+        boolean found = false;
+        // The loop terminates when they cross each other (second.right == first), or they become same (first == second)
+        while (first != second && second.right != first) {
+            if (first.num + second.num == x) {
+                found = true;
+                System.out.println("(" + first.num + ", " + second.num + ")");
+                first  = first.right;              // move first in forward direction
+                second = second.left;             // move second in backward direction
+            } else {
+                if (first.num + second.num < x) {
+                    first  = first.right;
+                } else {
+                    second = second.left;
+                }
+            }
+        }
+        if (found == false) {
+            System.out.println("No pair found");
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = null;
+        int[] a = {1, 2, 3, 4, 5, 6, 8, 9};
+        for (int x : a) {
+            head = createList(x, null);
+        }
+        traverseList(head);
+        pairSum(head, 7);
+    }
+}
---
+++ Algorithm/src/trees/BinaryTreeToCircularDLL.java
@@ -0,0 +1,4 @@
+package trees;
+
+public class BinaryTreeToCircularDLL {
+}
