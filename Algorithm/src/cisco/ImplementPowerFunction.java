7e23872822eca7892d63ff1af27a50f3f1e7a61a
abbhagat <abhinaw.kumar7@gmail.com>
2022-05-15 08:28:56 +0530

    Interview

--- Algorithm/src/cisco/ImplementPowerFunction.java
+++ Algorithm/src/cisco/ImplementPowerFunction.java
@@ -2,13 +2,13 @@ package cisco;
 
 public class ImplementPowerFunction {
 
-    private static double pow(float x, int n){
-        if(n < 0){
-            x = 1/x;
+    private static double pow(float x, int n) {
+        if (n < 0) {
+            x = 1 / x;
             n = -n;
         }
         double result = 1;
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             result *= x;
         }
         return result;
