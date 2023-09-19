ca9a664dbfb7e8e89824a146342fc5f676e4ab34
abbhagat <abhinaw.kumar7@gmail.com>
2023-09-19 15:57:15 +0530

    Interview

--- Algorithm/src/dynamicProgramming/DeleteOperationOnTwoStrings.java
+++ Algorithm/src/dynamicProgramming/DeleteOperationOnTwoStrings.java
@@ -21,15 +21,15 @@ This approach allows us to calculate the total number of deletions required to a
 public class DeleteOperationOnTwoStrings {
 
     public static int LCS(String str1, String str2) {
-        int n = str1.length();
-        int m = str2.length();
-        int[][] dp = new int[n + 1][m + 1];
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < m + 1; j++) {
+        int m = str1.length();
+        int n = str2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
                 dp[i][j] = str1.charAt(i - 1) == str2.charAt(j - 1) ? 1 + dp[i - 1][j - 1] : Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
-        return dp[n][m];
+        return dp[m][n];
     }
 
     private static int minDistance(String word1, String word2) {
