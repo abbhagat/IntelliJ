ff65afd73bfb90cbbbddf108c7a82eb0f8d889f7
abbhagat <abhinaw.kumar7@gmail.com>
2023-12-27 04:13:22 +0530

    Interview

--- Algorithm/src/vmware/IntegerBreak.java
+++ Algorithm/src/vmware/IntegerBreak.java
@@ -15,18 +15,16 @@ package vmware;
 public class IntegerBreak {
 
     private static int integerBreak(int n) {
-        if (n == 2) {
-            return 1;
+        switch(n) {
+            case 2 : return 1;
+            case 3 : return 2;
+            default: int product = 1;
+                     while (n > 4) {
+                         product *= 3;
+                         n -= 3;
+                     }
+                     return product * n;
         }
-        if (n == 3) {
-            return 2;
-        }
-        int product = 1;
-        while (n > 4) {
-            product *= 3;
-            n -= 3;
-        }
-        return product * n;
     }
 
     public static void main(String[] args) {
