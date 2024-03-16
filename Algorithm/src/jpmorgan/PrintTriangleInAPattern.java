2c4e856998ce10cafd4f1a509ebf8cc99bf52368
abbhagat <39695698+abbhagat@users.noreply.github.com>
2024-03-16 12:41:46 +0530

    Update PrintTriangleInAPattern.java

--- Algorithm/src/jpmorgan/PrintTriangleInAPattern.java
+++ Algorithm/src/jpmorgan/PrintTriangleInAPattern.java
@@ -3,15 +3,15 @@ package jpmorgan;
 public class PrintTriangleInAPattern {
     private static void printPattern(int n) {
         int low = 1, high = n, mid = n / 2;
-        while(low <= mid) {
-            for(int i = 1; i <= low; i++) {
+        while (low <= mid) {
+            for (int i = 1; i <= low; i++) {
                 System.out.print((i == 1 || i == low) ? "*" : ' ');
             }
             System.out.println();
             low++;
         }
-        while(mid <= high) {
-            for(int i = mid + 1; i <= high; i++) {
+        while (mid <= high) {
+            for (int i = mid + 1; i <= high; i++) {
                 System.out.print((i == mid + 1 || i == high) ? "*" : ' ');
             }
             System.out.println();
