cd08f76119cedcd0b4f7fb503361598f0c3187cc
abbhagat <39695698+abbhagat@users.noreply.github.com>
2024-03-17 13:08:00 +0530

    Update OneEditDistance.java

--- Algorithm/src/leetcode/OneEditDistance.java
+++ Algorithm/src/leetcode/OneEditDistance.java
@@ -2,47 +2,27 @@ package leetcode;
 
 public class OneEditDistance {
 
-    private static boolean isOneEditDistance(String s, String t) {
-        int m = s.length(), n = t.length();
+    private static boolean isOneEditDistance(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
         if (n - m > 1) {
             return false;
         }
         for (int i = 0; i < m; i++) {
-            if (s.charAt(i) != t.charAt(i)) {
+            if (s1.charAt(i) != s2.charAt(i)) {
                 if (m == n) {
-                    return s.substring(i + 1).equals(t.substring(i + 1));
+                    return s1.substring(i + 1).equals(s2.substring(i + 1));
                 }
                 if (m < n) {
-                    return s.substring(i).equals(t.substring(i + 1));
+                    return s1.substring(i).equals(s2.substring(i + 1));
                 }
             }
         }
         return m != n;
     }
 
-    private static boolean isOneEditDistance(char[] c1, char[] c2) {
-        if(c2.length - c1.length > 1) {
-            return false;
-        }
-        int[] temp = new int[128];
-        for (char x : c1) {
-            temp[x]++;
-        }
-        for (char x : c2) {
-            temp[x]--;
-        }
-        int count = 0;
-        for (int x : temp) {
-            if (x < 0) {
-                count++;
-            }
-        }
-        return count == 1;
-    }
-
     public static void main(String[] args) {
-        System.out.println(isOneEditDistance("ab", "acb") + "\t" + isOneEditDistance("ab".toCharArray(), "acb".toCharArray()));
-        System.out.println(isOneEditDistance("ad", "cab") + "\t" + isOneEditDistance("ad".toCharArray(), "cab".toCharArray()));
-        System.out.println(isOneEditDistance("1203", "1213") + "\t" + isOneEditDistance("1203".toCharArray(), "1213".toCharArray()));
+        System.out.println(isOneEditDistance("ab", "acb"));
+        System.out.println(isOneEditDistance("ad", "cab") );
+        System.out.println(isOneEditDistance("1203", "1213"));
     }
 }
