7b39f5d7c5a14a980d6f274a49169b1f05e4170c
abbhagat <39695698+abbhagat@users.noreply.github.com>
2024-03-17 13:39:36 +0530

    Update PrintMatrixDiagonalElements.java

--- Algorithm/src/matrix/PrintMatrixDiagonalElements.java
+++ Algorithm/src/matrix/PrintMatrixDiagonalElements.java
@@ -4,9 +4,10 @@ public class PrintMatrixDiagonalElements {
 
     public static void main(String[] args) {
         int[][] M = {
-                {1, 2, 3},
-                {4, 5, 6}
-        };
+                      {1, 2, 3},
+                      {4, 5, 6},
+                      {7, 8, 9}
+                    };
         int ROW = M.length, COL = M[0].length;
         for (int i = 0, j = 0; i < ROW && j < COL; j++, i++) {
             System.out.println(M[i][j]);
