626e8213fbae78e87933343c84aea93ac517ee8c
abbhagat <39695698+abbhagat@users.noreply.github.com>
2024-03-17 12:49:20 +0530

    Update GoldMineProblem.java

--- Algorithm/src/matrix/GoldMineProblem.java
+++ Algorithm/src/matrix/GoldMineProblem.java
@@ -46,7 +46,7 @@ public class GoldMineProblem {
         }
         int max = goldTable[0][0];
         for(int[] a : goldTable) {
-            max = Math.max(max, a[0]);   // The max amount of gold collected will be the max value in first column of all rows
+            max = max(max, a[0]);   // The max amount of gold collected will be the max value in first column of all rows
         }
         return max;
     }
