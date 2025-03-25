7fcc34cae50932092051d306969f3fc294164dd6
abbhagat <abhinaw.kumar7@gmail.com>
2025-03-25 07:38:36 +0530

    Interview

--- Hibernate/src/main/java/singleTableStrategy/FourWheeler.java
+++ Hibernate/src/main/java/singleTableStrategy/FourWheeler.java
@@ -1,14 +1,14 @@
 package singleTableStrategy;
 
 import lombok.Data;
-
+import lombok.EqualsAndHashCode;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
 @Data
 @DiscriminatorValue("Car")
+@EqualsAndHashCode(callSuper = true)
 public class FourWheeler extends Vehicle {
-
     private String steeringWheel;
 }
