0376d9436a962a3cb8c9f2fae20d371173a8acff
abbhagat <abhinaw.kumar7@gmail.com>
2025-03-25 07:42:53 +0530

    Interview

--- Hibernate/src/main/java/joinedTableStrategy/TwoWheeler.java
+++ Hibernate/src/main/java/joinedTableStrategy/TwoWheeler.java
@@ -1,12 +1,12 @@
 package joinedTableStrategy;
 
 import lombok.Data;
-
+import lombok.EqualsAndHashCode;
 import javax.persistence.Entity;
 
 @Entity
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class TwoWheeler extends Vehicle {
-
     private String steeringHandle;
 }
--- Hibernate/src/main/java/joinedTableStrategy/Vehicle.java
+++ Hibernate/src/main/java/joinedTableStrategy/Vehicle.java
@@ -1,7 +1,6 @@
 package joinedTableStrategy;
 
 import lombok.Data;
-
 import javax.persistence.*;
 
 @Entity
