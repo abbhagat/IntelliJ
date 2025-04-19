8f1c69bf484e5737aba654171b2b06eca617f2fe
abbhagat <abhinaw.kumar7@gmail.com>
2025-04-19 11:57:14 +0530

    Interview

--- Algorithm/src/designpattern/TemplateDesignPattern.java
+++ Algorithm/src/designpattern/TemplateDesignPattern.java
@@ -3,7 +3,7 @@ package designpattern;
 /**
  * In Template pattern, an abstract class exposes defined way(s)/template(s)
  * to execute its methods. Its subclasses can override the method implementation
- * as per need but the invocation is to be in the same way as defined by an
+ * as per need, but the invocation is to be in the same way as defined by an
  * abstract class.
  */
 
