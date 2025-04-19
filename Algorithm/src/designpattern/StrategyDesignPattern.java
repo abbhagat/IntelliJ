dbd04507340310d610b11e2a3b645e7780045a50
abbhagat <abhinaw.kumar7@gmail.com>
2025-04-19 11:56:17 +0530

    Interview

--- Algorithm/src/designpattern/StrategyDesignPattern.java
+++ Algorithm/src/designpattern/StrategyDesignPattern.java
@@ -1,9 +1,9 @@
 package designpattern;
 
-/* In this design pattern the behavior of a class can be changed at runtime.
+/* In this design pattern, the behavior of a class can be changed at runtime.
  * Here we create objects which represent various strategies and a context object
  * whose behavior varies as per its strategy object.
- * Comparable,Comparator and Collections.sort() is the strategy pattern in Java API.
+ * Comparable, Comparator and Collections.sort() is the strategy pattern in Java API.
  */
 
 interface Strategy {
