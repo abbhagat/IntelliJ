aa00775623eeb47be20b409ff7bb3844371d024f
abbhagat <abhinaw.kumar7@gmail.com>
2025-05-08 08:35:43 +0530

    Interview

--- Algorithm/src/jpmorgan/AngleBetweenHourAndMinHand.java
+++ Algorithm/src/jpmorgan/AngleBetweenHourAndMinHand.java
@@ -5,7 +5,7 @@ import static java.lang.Math.abs;
 
 public class AngleBetweenHourAndMinHand {
 
-    private static int calcAngle(double hr, double min) {
+    private static int calcAngle(int hr, int min) {
         if (hr < 0 || min < 0 || hr > 12 || min > 60) {
             System.out.println("Wrong input");
             return -1;
@@ -16,12 +16,9 @@ public class AngleBetweenHourAndMinHand {
         if (min == 60) {
             min = 0;
             hr++;
-            if (hr > 12) {
-                hr = hr - 12;
-            }
         }
         int hourAngle = (int) (0.5 * (hr * 60 + min));
-        int minAngle  = (int) (6 * min);
+        int minAngle  = 6 * min;
         int angle = abs(hourAngle - minAngle);
         return min(360 - angle, angle);
     }
@@ -31,5 +28,6 @@ public class AngleBetweenHourAndMinHand {
         System.out.println(calcAngle(3, 30)  + " degree");
         System.out.println(calcAngle(3,  0)  + " degree");
         System.out.println(calcAngle(12, 30) + " degree");
+        System.out.println(calcAngle(12, 60) + " degree");
     }
 }
