f710360abe91f7213c36beeeb7bb1649137c9adb
abbhagat <abhinaw.kumar7@gmail.com>
2025-05-13 09:50:03 +0530

    Interview

--- Algorithm/src/adobe/WordBreak.java
+++ Algorithm/src/adobe/WordBreak.java
@@ -5,34 +5,31 @@ import java.util.List;
 /**
  Given a string s and a dictionary of strings wordDict,
  return true if s can be segmented into a space-separated sequence of one or more dictionary words.
-
  Note that the same word in the dictionary may be reused multiple times in the segmentation.
-
  Example 1:
-
  Input: s = "leetcode", wordDict = ["leet","code"]
  Output: true
  Explanation: Return true because "leetcode" can be segmented as "leet code".
- Example 2:
 
+ Example 2:
  Input: s = "applepenapple", wordDict = ["apple","pen"]
  Output: true
  Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
  Note that you are allowed to reuse a dictionary word.
- Example 3:
 
+ Example 3:
  Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
  Output: false
  */
 
-// Time  Complexity: O(n^2 * m)  n = Length of given String AND m = no of words in the dictionary
+// Time  Complexity: O(n^2) n = Length of given String AND m = no of words in the dictionary
 // Space Complexity: O(n)
 public class WordBreak {
 
     public static boolean wordBreak(String str, List<String> list) {
         int n = str.length();
         boolean[] dp = new boolean[n + 1];  // To keep track of the words, whether a substring of str can be segmented into words from the dictionary
-        dp[0] = true;  // An empty string can always be segmented
+        dp[0] = true;                      // An empty string can always be segmented
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j <= i; j++) {
                 String s = str.substring(j, i);
