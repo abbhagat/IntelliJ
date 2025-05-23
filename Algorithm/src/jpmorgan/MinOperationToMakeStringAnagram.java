666cb82a984d3b87b1f009cab39d6b725139c3f7
abbhagat <abhinaw.kumar7@gmail.com>
2025-05-23 20:20:26 +0530

    Interview

--- Algorithm/src/jpmorgan/MinOperationToMakeStringAnagram.java
+++ Algorithm/src/jpmorgan/MinOperationToMakeStringAnagram.java
@@ -2,31 +2,21 @@ package jpmorgan;
 
 public class MinOperationToMakeStringAnagram {
 
-    public static int minOperationToMakeStringAnagram(String s) {
-        int n = s.length() / 2;
-        String s1 = s.substring(0, n);
-        String s2 = s.substring(n);
-        int[] temp = new int[128];
-        for (int i = 0; i < s1.length(); i++) {
-            temp[s1.charAt(i)]++;
-        }
-        for (int i = 0; i < s2.length(); i++) {
-            temp[s2.charAt(i)]--;
-        }
+    public static int minOpsToMakeStringAnagram(String s) {
         int count = 0;
-        for (int x : temp) {
-            if (x > 0) {
-                count += x;
+        for (int i = 0, j = s.length() - 1; i < s.length() / 2; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                count++;
             }
         }
         return count;
     }
 
     public static void main(String[] args) {
-        System.out.println(minOperationToMakeStringAnagram("12321"));
-        System.out.println(minOperationToMakeStringAnagram("12341"));
-        System.out.println(minOperationToMakeStringAnagram("12345"));
-        System.out.println(minOperationToMakeStringAnagram("123456"));
-        System.out.println(minOperationToMakeStringAnagram("1234567"));
+        System.out.println(minOpsToMakeStringAnagram("12321"));
+        System.out.println(minOpsToMakeStringAnagram("12341"));
+        System.out.println(minOpsToMakeStringAnagram("12345"));
+        System.out.println(minOpsToMakeStringAnagram("123456"));
+        System.out.println(minOpsToMakeStringAnagram("1234567"));
     }
 }
