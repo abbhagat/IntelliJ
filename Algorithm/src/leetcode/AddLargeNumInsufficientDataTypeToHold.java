445d67c89efb14a580a011cd485aeba2f8d31f5a
abbhagat <abhinaw.kumar7@gmail.com>
2025-05-24 15:35:42 +0530

    Interview

--- Algorithm/src/leetcode/AddLargeNumInsufficientDataTypeToHold.java
+++ Algorithm/src/leetcode/AddLargeNumInsufficientDataTypeToHold.java
@@ -5,14 +5,14 @@ import static java.lang.Integer.max;
 public class AddLargeNumInsufficientDataTypeToHold {
 
     private static String add(String num1, String num2) {
-        int carry = 0, l1 = num1.length() - 1, l2 = num2.length() - 1;
+        int carry = 0, l1 = num1.length(), l2 = num2.length();
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i <= max(l1,l2); i++) {
-            int x = i <= l1 ? num1.charAt(l1 - i) - '0' : 0;
-            int y = i <= l2 ? num2.charAt(l2 - i) - '0' : 0;
-            int sum = x + y + carry;
-            carry = sum / 10;
-            result.insert(0, sum % 10);
+        for (int i = 0; i < max(l1,l2); i++) {
+             int x = i < l1 ? num1.charAt(l1 - i - 1) - '0' : 0;
+             int y = i < l2 ? num2.charAt(l2 - i - 1) - '0' : 0;
+             int sum = x + y + carry;
+             carry = sum / 10;
+             result.insert(0, sum % 10);
         }
         return carry == 0 ? result.toString() : "1" + result;
     }
