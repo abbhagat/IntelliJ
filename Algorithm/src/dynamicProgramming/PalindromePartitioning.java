5767985355cb57e5a766d1619b571580e39e8355
abbhagat <abhinaw.kumar7@gmail.com>
2025-05-24 19:52:02 +0530

    Interview

--- Algorithm/src/dynamicProgramming/PalindromePartitioning.java
+++ Algorithm/src/dynamicProgramming/PalindromePartitioning.java
@@ -6,8 +6,8 @@ import static util.CommonUtils.isPalindrome;
 /**
 Given a string str, a partitioning of the string is a palindrome partitioning if every substring of the partition is a palindrome.
 Determine the fewest cuts needed for palindrome partitioning of the given string.
-Input: str = "ababbbabbababa" Output: 3  Explanation: After 3 partitioning substrings are "a", "babbbab", "b", "ababa".
-Input: str = "aaabba" Output: 1 Explanation: The substrings after 1 partitioning are "aa" and "abba".
+Input: str = "ababbbabbababa" Output: 3 Explanation: After 3 partitioning substrings are "a", "babbbab", "b", "ababa".
+Input: str = "aaabba"         Output: 1 Explanation: The substrings after 1 partitioning are "aa" and "abba".
 */
 
 public class PalindromePartitioning {
