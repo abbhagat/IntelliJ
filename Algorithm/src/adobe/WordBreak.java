d19c74adeae20386e475a824732179006ca54591
abbhagat <abhinaw.kumar7@gmail.com>
2025-05-27 08:14:33 +0530

    Interview

--- Algorithm/src/adobe/WordBreak.java
+++ Algorithm/src/adobe/WordBreak.java
@@ -3,24 +3,19 @@ package adobe;
 import java.util.List;
 
 /**
- Given a string s and a dictionary of strings wordDict,
- return true if s can be segmented into a space-separated sequence of one or more dictionary words.
- Note that the same word in the dictionary may be reused multiple times in the segmentation.
- Example 1:
- Input: s = "leetcode", wordDict = ["leet","code"]
- Output: true
- Explanation: Return true because "leetcode" can be segmented as "leet code".
+Given a string s and a dictionary of strings wordDict,
+return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
 
- Example 2:
- Input: s = "applepenapple", wordDict = ["apple","pen"]
- Output: true
- Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
- Note that you are allowed to reuse a dictionary word.
+Example 1: Input: s = "leetcode", wordDict = ["leet","code"] Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
 
- Example 3:
- Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
- Output: false
- */
+Example 2: Input: s = "applepenapple", wordDict = ["apple","pen"]  Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+
+Example 3: Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"] Output: false
+*/
 
 // Time  Complexity: O(n^2) n = Length of given String AND m = no of words in the dictionary
 // Space Complexity: O(n)
