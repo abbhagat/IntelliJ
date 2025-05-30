63f81b93da6062e84061bb7d967ee3130a36ed89
abbhagat <abhinaw.kumar7@gmail.com>
2025-05-30 10:15:15 +0530

    Interview

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static java.lang.Integer.min;
 
@@ -7,13 +6,11 @@ public class Main {
 
     private static void topKFrequent(int[] a, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int x : a) {
-            map.put(x, map.getOrDefault(x, 0) + 1);
-        }
+        Arrays.stream(a).forEach(x -> map.put(x, map.getOrDefault(x, 0) + 1));
         List<Map.Entry<Integer, Integer>> sortedList = map.entrySet()
-                .stream()
-                .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
-                .collect(Collectors.toList());
+                                                          .stream()
+                                                          .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+                                                          .toList();
         List<Map.Entry<Integer, Integer>> topKList = sortedList.subList(0, min(k, sortedList.size()));
         ListIterator<Map.Entry<Integer, Integer>> listIterator = topKList.listIterator(topKList.size());
         while (listIterator.hasPrevious()) {
@@ -21,7 +18,6 @@ public class Main {
         }
     }
 
-    // Driver program to test above function
     public static void main(String[] args) {
         topKFrequent(new int[]{5, 2, 1, 3, 2, 2, 1, 2, 1 , 5}, 2);
     }
