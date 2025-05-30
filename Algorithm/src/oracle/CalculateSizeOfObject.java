74d47b2dcfe90d2bd347124a5ad06edc59575986
abbhagat <abhinaw.kumar7@gmail.com>
2025-05-30 10:03:54 +0530

    Interview

--- Algorithm/src/oracle/CalculateSizeOfObject.java
+++ Algorithm/src/oracle/CalculateSizeOfObject.java
@@ -1,31 +1,18 @@
 package oracle;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.util.Date;
 
-class Employee implements Serializable {
-
+record Employee(int id, String name, Date dob) implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
-
-    private int id;
-    private String name;
-    private Date dob;
-
-    public Employee(int id, String name, Date dob) {
-        this.id = id;
-        this.name = name;
-        this.dob = dob;
-    }
 }
 
 public class CalculateSizeOfObject {
 
     public static void main(String[] args) throws Exception {
         Employee emp = new Employee(1, "Abhinaw", new Date("07-OCT-1986"));
-        FileOutputStream fos = new FileOutputStream("Student.txt");
+        FileOutputStream fos  = new FileOutputStream("Student.txt");
         ObjectOutputStream os = new ObjectOutputStream(fos);
         os.writeObject(emp);
         File file = new File("Student.txt");
