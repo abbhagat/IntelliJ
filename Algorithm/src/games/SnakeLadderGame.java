43211256491d32d744a820db41799ed15a04e6db
abbhagat <abhinaw.kumar7@gmail.com>
2025-05-31 15:17:57 +0530

    Interview

--- Algorithm/src/games/SnakeLadderGame.java
+++ Algorithm/src/games/SnakeLadderGame.java
@@ -6,32 +6,31 @@ import lombok.Getter;
 import java.util.*;
 
 /**
- * Design a games.Snake & games.Ladder games.Game
+ Design a Snake & Ladder Game
 
- * - Design Entity/Domain classes
- * - Design the games.Game Flow
- * - Use the Design Patterns wherever applicable
+ - Design Entity/Domain classes
+ - Design the Game Flow
+ - Use the Design Patterns wherever applicable
 
- * init()
- * 1. user create a game
- * 2. addPlayer()
- * 3. addSnakes()
- * 4. addLadders()
+ Init()
+ 1. User creates game
+ 2. addPlayer()
+ 3. addSnakes()
+ 4. addLadders()
 
- * start() {
- * roll the dice
- * randome number 1- 6;
- * check for snakes -> new position
- * check for ladder -> if 100 winner
- * run in loop() till the game ends
- * }
+ Start() {
+ roll the dice
+ random number 1-6;
+ check for snakes -> new position
+ check for ladder -> if 100 winners
+ run in loop() till the game ends
+ }
 
- * end() {
- * end the game
- * }
-
- * games.Game g = new games.Game.Builder().addSnake(34,12).addLadder(23,56).addPlayer("Sanjay").build();
- * g.play()
+ End() {
+ end the game
+ }
+ Game g = new Game.Builder().addSnake(34,12).addLadder(23,56).addPlayer("Sanjay").build();
+ g.play()
  */
 
 @Getter
@@ -52,9 +51,10 @@ class Ladder {
 }
 
 class Game {
+
     private final List<Player> playerList;
-    private final Map<Integer,Integer> ladderMap;
-    private final Map<Integer,Integer> snakeMap;
+    private final Map<Integer, Integer> ladderMap;
+    private final Map<Integer, Integer> snakeMap;
 
     private Game(Builder builder) {
         this.playerList = builder.playerList;
@@ -63,41 +63,42 @@ class Game {
     }
 
     public final void play() {
-        label: while (true) {
+        while (true) {
             for (Player player : playerList) {
                 int n = rollDice();
-                if (n >= 1 && n <= 6) {
+                System.out.println(player.name + " rolled " + n);
+                player.index += n;
+                while (player.index > 100) {
+                    player.index -= n;
+                    n = rollDice();
                     player.index += n;
-                    while (player.index > 100) {
-                        player.index -= n;
-                        n = rollDice();
-                        player.index += n;
-                    }
-                    if (ladderMap.containsKey(player.index)) {
-                        player.index = ladderMap.get(player.index);
-                    }
-                    if (snakeMap.containsKey(player.index)) {
-                        player.index = snakeMap.get(player.index);
-                    }
-                    if (player.index == 100) {
-                        System.out.println(player.name + " is winner");
-                        break label;
-                    }
-                } else {
-                    rollDice();
+                }
+                System.out.println(player.name + " moved to " + player.index);
+                if (ladderMap.containsKey(player.index)) {
+                    player.index = ladderMap.get(player.index);
+                    System.out.println(player.name + " climbed ladder to " + player.index);
+                }
+                if (snakeMap.containsKey(player.index)) {
+                    player.index = snakeMap.get(player.index);
+                    System.out.println(player.name + " bitten by snake to " + player.index);
+                }
+                if (player.index == 100) {
+                    System.out.println(player.name + " is winner");
+                    return;
                 }
             }
         }
     }
 
     public int rollDice() {
-        return new Random().nextInt(6);
+        return new Random().nextInt(6) + 1;  //  random.nextInt(6) generates a random integer from 0 to 5.
     }
 
     public static class Builder {
+
         private final List<Player> playerList = new ArrayList<>();
-        private final Map<Integer,Integer> ladderMap = new HashMap<>();
-        private final Map<Integer,Integer> snakeMap = new HashMap<>();
+        private final Map<Integer, Integer> ladderMap = new HashMap<>();
+        private final Map<Integer, Integer> snakeMap = new HashMap<>();
 
         Game build() {
             return new Game(this);
@@ -125,8 +126,14 @@ public class SnakeLadderGame {
 
     public static void main(String[] args) {
         Game g = new Game.Builder()
-                .addSnake(34,12).addLadder(23,56).addPlayer("X")
-                .addSnake(43,5).addLadder(11,100).addPlayer("Y")
+                .addSnake(99, 10).addSnake(52, 5)
+                .addSnake(35, 12).addSnake(68, 3)
+                .addSnake(75, 28).addSnake(86, 37)
+                .addLadder(10, 98).addLadder(12, 55)
+                .addLadder(25, 76).addLadder(36, 62)
+                .addLadder(45, 89).addLadder(66, 95)
+                .addPlayer("X")
+                .addPlayer("Y")
                 .build();
         g.play();
     }
