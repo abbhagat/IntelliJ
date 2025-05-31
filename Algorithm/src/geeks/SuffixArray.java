3f8f1d5b6cc37d876ada1a448d17297bc524869a
abbhagat <abhinaw.kumar7@gmail.com>
2025-05-31 12:06:15 +0530

    Interview

--- Algorithm/src/geeks/SuffixArray.java
+++ Algorithm/src/geeks/SuffixArray.java
@@ -1,21 +1,18 @@
 package geeks;
 
-/*
-    Let the given string be "banana".
-
-    0 banana                          5 a
-    1 anana     Sort the Suffixes     3 ana
-    2 nana      ---------------->     1 anana
-    3 ana        alphabetically       0 banana
-    4 na                              4 na
-    5 a                               2 nana
-
-    So the suffix array for "banana" is {5, 3, 1, 0, 4, 2}
- */
-
-
 import java.util.Map;
 import java.util.TreeMap;
+
+/**
+Let the given string be "banana"
+0 banana                          5 a
+1 anana     Sort the Suffixes     3 ana
+2 nana      ---------------->     1 anana
+3 ana        alphabetically       0 banana
+4 na                              4 na
+5 a                               2 nana
+So the suffix array for "banana" is {5, 3, 1, 0, 4, 2}
+*/
 
 public class SuffixArray {
 
@@ -23,7 +20,7 @@ public class SuffixArray {
         String str = "banana";
         Map<String, Integer> map = new TreeMap<>();
         for (int i = 0; i < str.length(); i++) {
-            map.put(str.substring(i), i);
+             map.put(str.substring(i), i);
         }
         map.forEach((k, v) -> System.out.println(k + " " + v));
     }
--- Algorithm/src/geeks/TrappingRainWater.java
+++ Algorithm/src/geeks/TrappingRainWater.java
@@ -1,21 +1,20 @@
 package geeks;
 
 /**
- * Approach: The idea is to traverse every array element and find the highest bars on the left and right sides.
- * Take the smaller of two heights. The difference between the smaller height and height of the current element
- * is the amount of water that can be stored in this array element.
- * Algorithm:
- * Traverse the array from start to end.
- * For every element, traverse the array from start to that index and find the maximum height (a)
- * and traverse the array from the current index to end, and find the maximum height (b).
- * The amount of water that will be stored in this column is min(a,b) – array[i], add this value to the total amount of water stored
- * Print the total amount of water stored.
- * <p>
- * Instead of maintaining two arrays of size n for storing the left and a right max of each element,
- * maintain two variables to store the maximum till that point.
- * Since water trapped at any element = min(maxLeft, maxRight) – arr[i].
- * Calculate water trapped on smaller elements out of A[low] and A[high] first and move the pointers till low doesn’t cross high.
- */
+Approach: The idea is to traverse every array element and find the highest bars on the left and right sides.
+Take the smaller of two heights. The difference between the smaller height and height of the current element
+is the amount of water that can be stored in this array element.
+Algorithm:
+Traverse the array from start to end.
+For every element, traverse the array from start to that index and find the maximum height (a)
+and traverse the array from the current index to end, and find the maximum height (b).
+The amount of water that will be stored in this column is min(a,b) – array[i], add this value to the total amount of water stored
+Print the total amount of water stored.
+Instead of maintaining two arrays of size n for storing the left and a right max of each element,
+maintain two variables to store the maximum till that point.
+Since water trapped at any element = min(maxLeft, maxRight) – arr[i].
+Calculate water trapped on smaller elements out of A[low] and A[high] first and move the pointers till low doesn’t cross high.
+*/
 
 // Time  Complexity: O(n)
 // Space Complexity: O(1)
