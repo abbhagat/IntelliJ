eb0f55fb1217102961908e3ee769cd06cf17cbbb
abbhagat <abhinaw.kumar7@gmail.com>
2025-06-01 09:41:08 +0530

    Interview

--- Algorithm/src/sorting/SortMapKeyAndByValues.java
+++ Algorithm/src/sorting/SortMapKeyAndByValues.java
@@ -5,7 +5,19 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class SortMapByValues {
+public class SortMapKeyAndByValues {
+
+    public static <K extends Comparable<K>, V > Map<K, V> sortByKey(Map<K, V> map) {
+        return map.entrySet()
+                .stream()
+                .sorted(Map.Entry.<K, V>comparingByKey())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1,
+                        LinkedHashMap::new
+                ));
+    }
 
     public static <K, V extends Comparable<V>> Map<K, V> sortByValues(Map<K, V> map) {
         return map.entrySet()
@@ -22,10 +34,10 @@ public class SortMapByValues {
     public static void main(String[] args) {
         Map<String, Integer> map = new HashMap<>();
         map.put("A", 1);
-        map.put("Z", 2);
         map.put("C", 3);
+        map.put("B", 2);
         System.out.println("Original map: " + map);
-        Map<String, Integer> sortedMap = sortByValues(map);
-        System.out.println("Sorted map: " + sortedMap);
+        System.out.println("Sorted Map By Key  : " + sortByKey(map));
+        System.out.println("Sorted Map By Value: " + sortByValues(map));
     }
 }
