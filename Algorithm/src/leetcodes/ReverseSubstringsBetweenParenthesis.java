bca272c814d8997cec1a6b858b8991ea265969a8
abbhagat <abhinaw.kumar7@gmail.com>
2025-06-01 11:05:03 +0530

    Interview

--- Algorithm/src/leetcodes/ReverseSubstringsBetweenParenthesis.java
+++ Algorithm/src/leetcodes/ReverseSubstringsBetweenParenthesis.java
@@ -3,8 +3,9 @@ package leetcodes;
 import java.util.Stack;
 
 /**
-Given a string str that consists of lowercase English letters and brackets. The task is to reverse the substrings in each pair of matching parentheses, starting from the innermost one. The result should not contain any brackets.
-
+Given a string str that consists of lowercase English letters and brackets.
+ The task is to reverse the substrings in each pair of matching parentheses, starting from the innermost one.
+ The result should not contain any brackets.
 Examples:
 Input: str = "(skeeg(for)skeeg)" Output: geeksforgeeks
 Input: str = "((ng)ipm(ca))"     Output: camping
