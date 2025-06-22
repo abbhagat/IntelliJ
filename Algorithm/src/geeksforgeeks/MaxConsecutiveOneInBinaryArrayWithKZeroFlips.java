e18f2759e9b6b1d9f0a3549540e69daea704e3c2
abbhagat <abhinaw.kumar7@gmail.com>
2025-06-23 05:24:13 +0530

    Interview

--- Algorithm/src/geeksforgeeks/MaxConsecutiveOneInBinaryArrayWithKZeroFlips.java
+++ Algorithm/src/geeksforgeeks/MaxConsecutiveOneInBinaryArrayWithKZeroFlips.java
@@ -1,22 +1,19 @@
 package geeksforgeeks;
 
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
-/*
+/**
 Given a binary array a and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
 
 Input: a = [1,1,1,0,0,0,1,1,1,1,0], k = 2
-Output: 6
-Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Output: 6 Explanation: [1,1,1,0,0,1,1,1,1,1,1]
 
 Input: a = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
-Output: 10
-Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
-
- */
+Output: 10 Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+*/
 public class MaxConsecutiveOneInBinaryArrayWithKZeroFlips {
 
-    private static int slidingWindow(int[] a, int k) {
+    private static void slidingWindow(int[] a, int k) {
         int wL, wR, startIndex, bestWindow, zeroCount;
         wL = wR = startIndex = bestWindow = zeroCount = 0;
         while (wR < a.length) {
@@ -33,20 +30,20 @@ public class MaxConsecutiveOneInBinaryArrayWithKZeroFlips {
                 startIndex = wL;
             }
         }
+        System.out.print("Index Flipped ");
         for (int i = startIndex; i < startIndex + bestWindow; i++) {
             if (a[i] == 0) {
                 a[i] = 1;
                 System.out.print(i + " ");
             }
         }
-        System.out.println("\nChanged Array After Flip");
-        IntStream.range(0, a.length).forEach(i -> System.out.print(a[i] + " "));
-        return bestWindow;
+        System.out.println("\nChanged Array After Flip " + Arrays.toString(a));
+        System.out.println("Max Consecutive 1's: " + bestWindow);
     }
 
     public static void main(String[] args) {
-        System.out.println(slidingWindow(new int[]{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0}, 2));
-        System.out.println(slidingWindow(new int[]{1, 0, 0, 1, 1, 0, 1, 0, 1, 1}, 2));
-        System.out.println(slidingWindow(new int[]{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3));
+        slidingWindow(new int[]{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0}, 2);
+        slidingWindow(new int[]{1, 0, 0, 1, 1, 0, 1, 0, 1, 1}, 2);
+        slidingWindow(new int[]{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3);
     }
 }
