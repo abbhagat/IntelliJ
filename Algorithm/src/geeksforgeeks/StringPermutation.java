0b3911f31a26f8409d3431aea187918e28f27b46
abbhagat <abhinaw.kumar7@gmail.com>
2025-06-23 06:54:41 +0530

    Interview

--- Algorithm/src/geeksforgeeks/StringPermutation.java
+++ Algorithm/src/geeksforgeeks/StringPermutation.java
@@ -1,5 +1,7 @@
 package geeksforgeeks;
 
+// Time  Complexity : O(n)
+// Space Complexity : O(1)
 public class StringPermutation {
 
     public static boolean checkInclusion(char[] c1, char[] c2) {
@@ -9,18 +11,18 @@ public class StringPermutation {
         int[] c1Data = new int[128];
         int[] c2Data = new int[128];
         for (int i = 0; i < c1.length; i++) {
-            c1Data[c1[i]]++;
-            c2Data[c2[i]]++;
+             c1Data[c1[i]]++;
+             c2Data[c2[i]]++;
         }
         if (matches(c1Data, c2Data)) {
             return true;
         }
         for (int i = 0, j = c1.length; j < c2.length; i++, j++) {
-            c2Data[c2[j]]++;
-            c2Data[c2[i]]--;
-            if (matches(c1Data, c2Data)) {
-                return true;
-            }
+             c2Data[c2[j]]++;
+             c2Data[c2[i]]--;
+             if (matches(c1Data, c2Data)) {
+                 return true;
+             }
         }
         return matches(c1Data, c2Data);
     }
