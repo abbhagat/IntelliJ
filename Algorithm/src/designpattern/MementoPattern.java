ff419d3146fe8662d7e0ad317d1cc215e871365c
abbhagat <abhinaw.kumar7@gmail.com>
2025-06-28 11:30:47 +0530

    Interview

--- Algorithm/src/designpattern/MementoPattern.java
+++ Algorithm/src/designpattern/MementoPattern.java
@@ -1,66 +1,54 @@
 package designpattern;
 
+import lombok.Getter;
+import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
- * Memento pattern is used to restore state of an object to a previous state.
+/**
+ * Memento pattern is used to restore the state of an object to a previous state.
  * Memento pattern uses three actor classes.
- * Memento contains state of an object to be restored.
- * Originator creates and stores states in Memento objects.
+ * The Memento contains the state of an object to be restored.
+ * The Originator creates and stores states in Memento objects.
  * Caretaker object restores object state from Memento.
  */
-class Memento {
+record Memento(String state) {
 
-    private String state;
-
-    Memento(String state) {
-        this.state = state;
-    }
-
-    public String getState() {
-        return state;
-    }
 }
 
+@Setter
+@Getter
 class Originator {
 
     private String state;
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getState() {
-        return state;
-    }
 
     public Memento saveStateToMemento() {
         return new Memento(state);
     }
 
     public void getStateFromMemento(Memento memento) {
-        state = memento.getState();
+        state = memento.state();
     }
 }
 
 class CareTaker {
 
-    List<Memento> al = new ArrayList<>();
+    List<Memento> list = new ArrayList<>();
 
-    public void add(Memento state) {
-        al.add(state);
+    public boolean add(Memento state) {
+        return list.add(state);
     }
 
     public Memento get(int index) {
-        return al.get(index);
+        return list.get(index);
     }
 }
 
 public class MementoPattern {
+
     public static void main(String[] args) {
         Originator originator = new Originator();
-        CareTaker careTaker = new CareTaker();
+        CareTaker careTaker   = new CareTaker();
 
         originator.setState("State #1");
         originator.setState("State #2");
