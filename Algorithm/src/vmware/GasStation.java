e7b50b8dbe0cb81f1208146649fe8aef332f4fce
abbhagat <abhinaw.kumar7@gmail.com>
2025-06-28 14:48:44 +0530

    Interview

--- Algorithm/src/vmware/GasStation.java
+++ Algorithm/src/vmware/GasStation.java
@@ -44,6 +44,6 @@ public class GasStation {
 
     public static void main(String[] args) {
         System.out.println(canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2}));
-        System.out.println(canCompleteCircuit(new int[]{2, 3, 4}, new int[]{3, 4, 3}));
+        System.out.println(canCompleteCircuit(new int[]{2, 3, 4}      , new int[]{3, 4, 3}));
     }
 }
