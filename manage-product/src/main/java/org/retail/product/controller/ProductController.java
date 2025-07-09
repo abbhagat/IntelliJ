829c3b0cbf875229be5035d0ec669e51a67a859c
abbhagat <abhinaw.kumar7@gmail.com>
2025-07-09 05:38:37 +0530

    Interview

--- manage-product/src/main/java/org/retail/product/controller/ProductController.java
+++ manage-product/src/main/java/org/retail/product/controller/ProductController.java
@@ -8,9 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
