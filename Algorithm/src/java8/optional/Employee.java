fd1cf8fe06d3fafb6bff273157b69d2849580dc9
abbhagat <abhinaw.kumar7@gmail.com>
2025-07-28 04:41:03 +0530

    Interview

--- Algorithm/src/java8/optional/Employee.java
+++ Algorithm/src/java8/optional/Employee.java
@@ -1,21 +1,4 @@
 package java8.optional;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Employee {
-
-    private int id;
-    private String name;
-
-    @Override
-    public String toString() {
-        return getId() + " " + getName();
-    }
+public record Employee(int id, String name) {
 }
