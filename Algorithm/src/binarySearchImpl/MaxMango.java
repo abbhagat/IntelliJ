85b2d1b9e7ecba6579bc1424e23098581d3323d6
abbhagat <abhinaw.kumar7@gmail.com>
2025-08-17 11:31:41 +0530

    Interview

--- Algorithm/src/binarySearchImpl/MaxMango.java
+++ Algorithm/src/binarySearchImpl/MaxMango.java
@@ -1,48 +1,48 @@
 package binarySearchImpl;
 
 /**
-Given two integers W and C, representing the number of watermelons and coins,
-the task is to find the maximum number of mangoes that can be bought given
-that each mango costs 1 watermelon and X coins and y coins can be earned selling a watermelon.
-Input: W = 10, C = 10, X = 1, Y = 1
-Output: 10
-Explanation: The most optimal way is to use 10 watermelons and 10 coins to buy 10 mangoes. Hence, the maximum number of mangoes that can be bought is 10.
-Input: W = 4, C = 8, X = 4, Y = 4
-Output: 3
-Explanation: The most optimal way is to sell one watermelon. Then, the number of coins increases by 4.
-Therefore, the total number of coins becomes 12. Therefore, 3 watermelons and 12 coins can be used to buy 3 mangoes.
-Hence, the maximum number of mangoes that can be bought is 3.
-Approach: This problem can be solved using binary search. The idea is to find the maximum number of mangoes in the search space.
-Follow the steps below to solve the problem:
-Initialize a variable ans as 0 to store the required result.
-Initialize two variables l as 0, r as W to store the boundary regions of the search space for binary search.
-Loop while l≤r and perform the following steps:
-Store the middle value in a variable mid as (l+r)/2.
-Check if mid-number of mangoes can be bought using the given value of W, C, x, and y.
-If true, then update ans to mid and search in the right part of mid by updating l to mid+1. Otherwise, update the value of r to mid-1.
-Print the value of ans as the result
-*/
+ * Given two integers W and C, representing the number of watermelons and coins,
+ * the task is to find the maximum number of mangoes that can be bought given
+ * that each mango costs 1 watermelon and X coins and y coins can be earned selling a watermelon.
+ * Input: W = 10, C = 10, X = 1, Y = 1
+ * Output: 10
+ * Explanation: The most optimal way is to use 10 watermelons and 10 coins to buy 10 mangoes. Hence, the maximum number of mangoes that can be bought is 10.
+ * Input: W = 4, C = 8, X = 4, Y = 4
+ * Output: 3
+ * Explanation: The most optimal way is to sell one watermelon. Then, the number of coins increases by 4.
+ * Therefore, the total number of coins becomes 12. Therefore, 3 watermelons and 12 coins can be used to buy 3 mangoes.
+ * Hence, the maximum number of mangoes that can be bought is 3.
+ * Approach: This problem can be solved using binary search. The idea is to find the maximum number of mangoes in the search space.
+ * Follow the steps below to solve the problem:
+ * Initialize a variable result as 0 to store the required result.
+ * Initialize two variables l as 0, r as W to store the boundary regions of the search space for binary search.
+ * Loop while l≤r and perform the following steps:
+ * Store the middle value in a variable mid as (l+r)/2.
+ * Check if mid-number of mangoes can be bought using the given value of W, C, x, and y.
+ * If true, then update result to mid and search in the right part of mid by updating l to mid+1. Otherwise, update the value of r to mid-1.
+ * Print the value of result.
+ */
 
 // Time Complexity: O log(W)
 // Auxiliary Space: O(1)
 public class MaxMango {
 
-    private static boolean check(int W, int C, int x, int y, int mid) {
+    private static boolean check(int W, int C, int X, int Y, int mid) {
         if (mid > W) {      // If watermelons needed are greater than given watermelons
             return false;
         }
         int remainingWaterMelon = W - mid;
-        int coinsEarned = remainingWaterMelon * y;
+        int coinsEarned = remainingWaterMelon * Y;
         C += coinsEarned;
-        int mangoes = C / x;
+        int mangoes = C / X;
         return mangoes >= mid;
     }
 
-    private static int maximizeMangoes(int W, int C, int x, int y) {
+    private static int maximizeMangoes(int W, int C, int X, int Y) {
         int low = 0, high = W, result = 0;
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (check(W, C, x, y, mid)) {      // Check if it is possible to buy mid-number of mangoes
+            if (check(W, C, X, Y, mid)) {      // Check if it is possible to buy mid-number of mangoes
                 result = mid;
                 low = mid + 1;
             } else {
@@ -53,7 +53,12 @@ public class MaxMango {
     }
 
     public static void main(String[] args) {
-        int W = 4, C = 8, x = 4, y = 4;
-        System.out.println(maximizeMangoes(W, C, x, y));
+        int W = 4, C = 8, X = 4, Y = 4;
+        System.out.println(maximizeMangoes(W, C, X, Y));
+        W = 10;
+        C = 10;
+        X = 1;
+        Y = 1;
+        System.out.println(maximizeMangoes(W, C, X, Y));
     }
 }
--- Algorithm/src/binarySearchImpl/PlacePrisonerToMaxMinDiffBetweenAnyTwoPrisoner.java
+++ Algorithm/src/binarySearchImpl/PlacePrisonerToMaxMinDiffBetweenAnyTwoPrisoner.java
@@ -3,24 +3,24 @@ package binarySearchImpl;
 import java.util.Arrays;
 
 /**
-Given an array a[] of N elements, which represent the positions of the cells in a prison.
-Also, given an integer P which is the number of prisoners, the task is to place all the prisoners
-in the cells in an ordered manner such that the minimum distance between any two prisoners is as large as possible.
-Finally, print the maximized distance.
-Examples:
-Input: a[] = {1, 2, 4, 8, 9}, P = 3 Output: 3
-The three prisoners will be placed at the cells numbered 1, 4 and 8 with the minimum distance 3 which is the maximum possible.
-Input: a[] = {10, 12, 18}, P = 2 Output: 8
-The three possible placements are {10, 12}, {10, 18} and {12, 18}.
-Approach: This problem can be solved using binary search. As the minimum distance between two cells in which prisoners will be kept has to be maximized,
-the search space will be of distance, starting from 0 (if two prisoners are kept in the same a) and ending at a[N – 1] – a[0]
-(if one prisoner is kept in the first a and the other one is kept in the last a).
-Initialize L = 0 and R = a[N – 1] – a[0] then apply the binary search.
-For every mid, check whether the prisoners can be placed such that the minimum distance between any two prisoners is at least mid
-If yes then try to increase this distance to maximize the answer and check again.
-If not, then try to decrease the distance.
-Finally, print the maximized distance.
-*/
+ * Given an array a[] of N elements, which represent the positions of the cells in a prison.
+ * Also, given an integer P which is the number of prisoners, the task is to place all the prisoners
+ * in the cells in an ordered manner such that the minimum distance between any two prisoners is as large as possible.
+ * Finally, print the maximized distance.
+ * Examples:
+ * Input: a[] = {1, 2, 4, 8, 9}, P = 3 Output: 3
+ * The three prisoners will be placed at the cells numbered 1, 4 and 8 with the minimum distance 3 which is the maximum possible.
+ * Input: a[] = {10, 12, 18}, P = 2 Output: 8
+ * The three possible placements are {10, 12}, {10, 18} and {12, 18}.
+ * Approach: This problem can be solved using binary search. As the minimum distance between two cells in which prisoners will be kept has to be maximized,
+ * the search space will be of distance, starting from 0 (if two prisoners are kept in the same a) and ending at a[N – 1] – a[0]
+ * (if one prisoner is kept in the first a and the other one is kept in the last a).
+ * Initialize L = 0 and R = a[N – 1] – a[0] then apply the binary search.
+ * For every mid, check whether the prisoners can be placed such that the minimum distance between any two prisoners is at least mid
+ * If yes then try to increase this distance to maximize the answer and check again.
+ * If not, then try to decrease the distance.
+ * Finally, print the maximized distance.
+ */
 
 // Time Complexity: O(n * log n)
 public class PlacePrisonerToMaxMinDiffBetweenAnyTwoPrisoner {
