5a5edef7f9e16864664a14ca3220140ac750d6e7
abbhagat <abhinaw.kumar7@gmail.com>
2025-08-23 08:15:47 +0530

    Interview

--- Algorithm/src/recursion/TowerOfHanoi.java
+++ Algorithm/src/recursion/TowerOfHanoi.java
@@ -4,17 +4,19 @@ package recursion;
 // Space Complexity: O(n)
 public class TowerOfHanoi {
 
-    private static void towerOfHanoi(int n, String src, String aux, String dest) {
+    private static void towerOfHanoi(String src, String aux, String dest, int n) {
         if (n == 1) {
             System.out.println(src + " -> " + dest);
             return;
         }
-        towerOfHanoi(n - 1, src, dest, aux);
+        towerOfHanoi(src, dest, aux, n - 1);
         System.out.println(src + " -> " + dest);
-        towerOfHanoi(n - 1, aux, src, dest);
+        towerOfHanoi(aux, src, dest, n - 1);
     }
 
     public static void main(String[] args) {
-        towerOfHanoi(3, "A", "B", "C");
+        towerOfHanoi("A", "B", "C", 2);
+        System.out.println("------------------");
+        towerOfHanoi("A", "B", "C", 3);
     }
 }
