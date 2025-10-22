7d63195c869e59210a01b160411a576891574553
abbhagat <abhinaw.kumar7@gmail.com>
2025-10-22 16:40:11 +0530

    Interview

--- Algorithm/src/pairs/FindPairCountWithSumDivisibleByK.java
+++ Algorithm/src/pairs/FindPairCountWithSumDivisibleByK.java
@@ -12,7 +12,7 @@ import static java.lang.Integer.min;
 // Space Complexity : O(k)
 public class FindPairCountWithSumDivisibleByK {
 
-    private static void findPairs(int[] a, int k) {
+    private static int findPairs(int[] a, int k) {
         Set<String> set = new HashSet<>();
         int count = 0;
         for (int i = 0; i < a.length; i++) {
@@ -28,25 +28,26 @@ public class FindPairCountWithSumDivisibleByK {
                 }
             }
         }
-        System.out.println("Pair Count : " + count);
+        return count;
     }
 
-    private static void findPairCount(int[] a, int k) {
+    private static int findPairCount(int[] a, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
         for (int x : a) {
              int y = x % k;
              int z = (k - y) % k;  // complement
              count += map.getOrDefault(z, 0);
-             map.merge(y, 1, Integer::sum);
+             map.put(y, map.getOrDefault(y, 0) + 1);   // map.merge(y, 1, Integer::sum);
         }
-        System.out.println("Pair Count : " + count);
+        return count;
     }
 
     private static int countPairs(int[] a, int k) {
         int[] freq = new int[k];
         for (int x : a) {
-            freq[x % k]++;  // keeps the count of each remainder
+             int y = x % k;
+             freq[y]++;  // keeps the count of each remainder
         }
         int count = freq[0] * (freq[0] - 1) / 2;  // Counting Pairs with Remainder 0
         for (int i = 1; i <= k / 2 && i != (k - i); i++) {  // Counting Pairs with Remainders i and k-i
@@ -57,11 +58,14 @@ public class FindPairCountWithSumDivisibleByK {
     }
 
     public static void main(String[] args) {
-        findPairs(new int[]{1, 2, 3, 4, 5, 8}, 5);
-        System.out.println(countPairs(new int[]{1, 2, 3, 4, 5, 8}, 5));
-        findPairs(new int[]{1, 2, 3, 4, 5, 8}, 4);
-        System.out.println(countPairs(new int[]{2, 2, 1, 7, 5, 3}, 4));
-        findPairs(new int[]{2, 3, 5, 10}, 5);
-        System.out.println(countPairs(new int[]{2, 3, 5, 10}, 5));
+        int[] a = {1, 2, 3, 4, 5, 8};
+        findPairs(a, 5);
+        System.out.println(findPairs(a, 5) + "\t" + countPairs(a, 5) + "\t" + findPairCount(a,5));
+        System.out.println("------------------------");
+        int[] b = {2, 1, 7, 5, 3};
+        System.out.println(findPairs(b, 5) + "\t" + countPairs(b, 5) + "\t" + findPairCount(b,5));
+        System.out.println("------------------------");
+        int[] c = {2, 3, 5, 10};
+        System.out.println(findPairs(c, 5) + "\t" + countPairs(c, 5) + "\t" + findPairCount(c,5));
     }
 }
