9216856b9d4f7f46ee1c9c67d4ee33ec0ad0fc5a
abbhagat <abhinaw.kumar7@gmail.com>
2025-11-19 08:09:53 +0530

    Interview

--- Algorithm/src/leetcode/PrintMissingRangeInSortedArray.java
+++ Algorithm/src/leetcode/PrintMissingRangeInSortedArray.java
@@ -7,16 +7,10 @@ public class PrintMissingRangeInSortedArray {
     for (int i = 1; i < a.length; i++) {
       int n = a[i] - a[i - 1];
       switch (n) {
-        case 1:
-          continue;
-        case 2:
-          System.out.println(a[i - 1] + 1);
-          break;
-        case 3:
-          System.out.println(a[i - 1] + 1 + " " + (a[i] - 1));
-          break;
-        default:
-          System.out.println(a[i - 1] + 1 + "-" + (a[i] - 1));
+        case 1:  continue;
+        case 2:  System.out.println(a[i - 1] + 1); break;
+        case 3:  System.out.println(a[i - 1] + 1 + " " + (a[i] - 1)); break;
+        default: System.out.println(a[i - 1] + 1 + "-" + (a[i] - 1));
       }
     }
     System.out.println();
