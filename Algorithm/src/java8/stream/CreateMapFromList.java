d4804bf6cf488e849df8e02fe85ad806755081b8
abbhagat <abhinaw.kumar7@gmail.com>
2025-11-29 13:46:26 +0530

    Interview

--- Algorithm/src/java8/stream/CreateMapFromList.java
+++ Algorithm/src/java8/stream/CreateMapFromList.java
@@ -5,7 +5,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import static java.util.stream.Collectors.*;
 
 public class CreateMapFromList {
@@ -21,10 +20,10 @@ public class CreateMapFromList {
 
     List<Employee> employeeList = List.of(emp1, emp2, emp3, emp4, emp5, emp6);
     Map<Integer, String> result = employeeList.stream()
-        .collect(Collectors.toMap(Employee::id, Employee::deptName));
+                                              .collect(Collectors.toMap(Employee::id, Employee::deptName));
     System.out.println(result);
     Map<String, List<Integer>> deptToIdsMap = employeeList.stream()
-        .collect(groupingBy(Employee::deptName, mapping(Employee::id, toList())));
+                                                          .collect(groupingBy(Employee::deptName, mapping(Employee::id, toList())));
     System.out.println(deptToIdsMap);
 
     Map<String, List<Integer>> map = new LinkedHashMap<>();
@@ -37,10 +36,7 @@ public class CreateMapFromList {
     System.out.println(map);
 
     Map<String, List<Integer>> deptListMap = new LinkedHashMap<>();
-    employeeList.forEach(emp -> {
-      String deptName = emp.deptName();
-      deptListMap.computeIfAbsent(deptName, k -> new ArrayList<>()).add(emp.id());
-    });
+    employeeList.forEach(emp -> deptListMap.computeIfAbsent(emp.deptName(), k -> new ArrayList<>()).add(emp.id()));
     System.out.println(deptListMap);
   }
 
