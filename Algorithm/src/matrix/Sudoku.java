29c1758b3cf216a50003ace2d2c55c521919ff14
abbhagat <abhinaw.kumar7@gmail.com>
2025-11-29 12:38:17 +0530

    Interview

--- Algorithm/src/matrix/Sudoku.java
+++ Algorithm/src/matrix/Sudoku.java
@@ -1,66 +1,51 @@
 package matrix;
 
+import static util.CommonUtils.printMatrix;
+
 public class Sudoku {
 
   private static final char[][] board = {
-      {3, 0, 6, 5, 0, 8, 4, 0, 0},
-      {5, 2, 0, 0, 0, 0, 0, 0, 0},
-      {0, 8, 7, 0, 0, 0, 0, 3, 1},
-      {0, 0, 3, 0, 1, 0, 0, 8, 0},
-      {9, 0, 0, 8, 6, 3, 0, 0, 5},
-      {0, 5, 0, 0, 9, 0, 6, 0, 0},
-      {1, 3, 0, 0, 0, 0, 2, 5, 0},
-      {0, 0, 0, 0, 0, 0, 0, 7, 4},
-      {0, 0, 5, 2, 0, 6, 3, 0, 0}
-  };
+                                          {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                                          {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                                          {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                                          {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                                          {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                                          {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                                          {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                                          {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                                          {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+                                        };
 
   private static boolean solveSudoku(char[][] board) {
     for (int i = 0; i < 9; i++) {
       for (int j = 0; j < 9; j++) {
-        if (board[i][j] != '.') {
-          continue;
-        }
-        for (char k = '1'; k <= '9'; k++) {
-          if (isValid(board, i, j, k)) {
-            board[i][j] = k;
-            if (solveSudoku(board)) {
-              return true;
+        if (board[i][j] == '.') {
+          for (char c = '1'; c <= '9'; c++) {
+            if (isValid(board, i, j, c)) {
+              board[i][j] = c;
+              if (solveSudoku(board)) return true;
+              board[i][j] = '.'; // backtrack
             }
-            board[i][j] = '.';
           }
+          return false;
         }
-        return false;
       }
     }
-    return true; //return true if all cells are checked
+    return true;
   }
 
   private static boolean isValid(char[][] board, int row, int col, char c) {
     for (int i = 0; i < 9; i++) {
-      if (board[i][col] != '.' && board[i][col] == c) {
-        return false;
-      }
-
-      if (board[row][i] != '.' && board[row][i] == c) {
-        return false;
-      }
-
-      if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] != '.' &&
-          board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) {
-        return false;
-      }
+      if (board[row][i] == c) return false;
+      if (board[i][col] == c) return false;
+      if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
     }
     return true;
   }
 
   public static void main(String[] args) {
     if (solveSudoku(board)) {
-      for (char[] x : board) {
-        for (int y : x) {
-          System.out.print(y + " ");
-        }
-        System.out.println();
-      }
+      printMatrix(board);
     } else {
       System.out.println("No solution exists");
     }
