902bbeb8005244321245d590d6cb66b35820863d
abbhagat <abhinaw.kumar7@gmail.com>
2025-11-29 13:36:48 +0530

    Interview

--- Algorithm/src/java8/mykong/GroupByStreamExample.java
+++ Algorithm/src/java8/mykong/GroupByStreamExample.java
@@ -10,18 +10,24 @@ import java.util.stream.Collectors;
 public class GroupByStreamExample {
 
   public static void main(String[] args) {
+
     List<String> items = Arrays.asList("apple", "apple", "banana", "apple", "orange", "banana", "papaya");
     Map<String, Long> map = items.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     System.out.println(map);
+
     Map<String, Long> finalMap = new LinkedHashMap<>();
-    map.entrySet().stream()
-        .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-        .forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
+    map.entrySet()
+       .stream()
+       .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+       .forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
     System.out.println(finalMap);
-    map.entrySet().stream()
-        .sorted(Map.Entry.<String, Long>comparingByKey().reversed())
-        .forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
+
+    map.entrySet()
+       .stream()
+       .sorted(Map.Entry.<String, Long>comparingByKey().reversed())
+       .forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
     System.out.println(finalMap);
+
     map.clear();
     map.putAll(finalMap);
     System.out.println(map);
