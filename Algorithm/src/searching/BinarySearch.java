eb533b9baa20b4d01da8af5672baba1f62fe68ac
abbhagat <abhinaw.kumar7@gmail.com>
2025-12-10 07:31:16 +0530

    Interview

--- Algorithm/src/searching/BinarySearch.java
+++ Algorithm/src/searching/BinarySearch.java
@@ -7,16 +7,17 @@ public class BinarySearch {
 
   public static void main(String[] args) {
     int[] a = {20, 3, 45, 15, 6, 19, 8, 2, 1, 35, 123};
+    System.out.println(Arrays.binarySearch(a, 0, a.length - 1, 35));
     Arrays.sort(a);
     boolean isPresent = binarySearch(a, 0, a.length - 1, 35);
     System.out.println(isPresent ? "Present" : "Not Present");
-    System.out.println(Arrays.binarySearch(a, 0, a.length - 1, 35));
   }
 
   public static boolean binarySearch(int[] a, int low, int high, int n) {
     if (low <= high) {
       int mid = (low + high) / 2;
       if (n == a[mid]) {
+        System.out.println("Present at Index :" + mid);
         return true;
       }
       return n < a[mid] ? binarySearch(a, low, mid - 1, n) : binarySearch(a, mid + 1, high, n);
--- Algorithm/src/searching/FindMinInARotatedSortedArray.java
+++ Algorithm/src/searching/FindMinInARotatedSortedArray.java
@@ -4,18 +4,20 @@ package searching;
 public class FindMinInARotatedSortedArray {
 
   private static int findMinimum(int[] a) {
-    int low = 0, high = a.length - 1;
+    int low = 0, high = a.length - 1, mid = 0;
     while (low < high) {
-      int mid = (low + high) / 2;
+      mid = (low + high) / 2;
       if (a[mid] == a[high]) {
         high--;
+      } else if (a[mid] > a[mid + 1]) {
+        return a[mid + 1];
       } else if (a[mid] > a[high]) {
         low = mid + 1;
       } else {
         high = mid;
       }
     }
-    return a[high];
+    return a[mid];
   }
 
   private static int findMinimum(int[] a, int low, int high) {
