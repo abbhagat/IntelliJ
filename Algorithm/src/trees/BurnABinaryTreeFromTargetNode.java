cc158efe22394599ed31a0ee89892f5c5262c1bd
abbhagat <abhinaw.kumar7@gmail.com>
2025-12-12 08:51:38 +0530

    Interview

--- Algorithm/src/trees/BurnABinaryTreeFromTargetNode.java
+++ Algorithm/src/trees/BurnABinaryTreeFromTargetNode.java
@@ -4,20 +4,19 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * Input :
- * 12
- * /  \
- * 13    10
- * /   \
- * 14      15
- * /   \    /  \
- * 21   24  22   23
- * target node = 14
- * Output :
- * 14
- * 21, 24, 10
- * 15, 12
- * 22, 23, 13
+ *                                             12
+ *                                            /  \
+ *                                          13    10
+ *                                        /   \
+ *                                      14     15
+ *                                    /   \   /  \
+ *                                  21    24 22   23
+ *                                  target node = 14
+ *                                  Output :
+ *                                  14
+ *                                  21, 24, 10
+ *                                  15, 12
+ *                                  22, 23, 13
  * Explanation: First node 14 burns, then it gives fire to its neighbors (21, 24, 10) and so on.
  * This process continues until the whole tree burns.
  * First search the target node in a binary tree recursively.
