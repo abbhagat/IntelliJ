e98420bb4cd0d8091e4bb4d1aa31f055b1789f5e
abbhagat <abhinaw.kumar7@gmail.com>
2025-12-31 08:53:40 +0530

    Interview

--- Algorithm/src/stack/BasicCalculatorII.java
+++ Algorithm/src/stack/BasicCalculatorII.java
@@ -4,16 +4,20 @@ import java.util.Stack;
 
 public class BasicCalculatorII {
 
-  private static int calculate(String exp) {
+  private static int calculate(String exp, int[] idx) {
     Stack<Integer> stack = new Stack<>();
     int n = 0;
     char sign = '+';
-    for (int i = 0; i < exp.length(); i++) {
-      char c = exp.charAt(i);
+    while (idx[0] < exp.length()) {
+      char c = exp.charAt(idx[0]);
       if (Character.isDigit(c)) {
         n = n * 10 + (c - '0');
       }
-      if (!Character.isDigit(c) && c != ' ' || i == exp.length() - 1) {
+      if (c == '(') {
+        idx[0]++;                 // skip '('
+        n = calculate(exp, idx);   // evaluate inside parentheses
+      }
+      if (!Character.isDigit(c) && c != ' ' || idx[0] == exp.length() - 1) {
         switch (sign) {
           case '+' -> stack.push(n);
           case '-' -> stack.push(-n);
@@ -23,16 +27,20 @@ public class BasicCalculatorII {
         sign = c;
         n = 0;
       }
+      if (c == ')') {
+        break; // end of current sub-expression
+      }
+      idx[0]++;
     }
     return stack.stream().mapToInt(Integer::intValue).sum();
   }
 
   public static void main(String[] args) {
-    System.out.println("3+2*2    = " + calculate("3+2*2"));
-    System.out.println("3/2      = " + calculate("3/2"));
-    System.out.println("3 +5 / 2 = " + calculate(" 3 +5 / 2"));
-    System.out.println(calculate("1 + 1"));
-    System.out.println(calculate("2 - 1 + 2"));
-    System.out.println(calculate("1 + (4 + 5 + 2) - 3 + (6 + 8)"));
+    System.out.println("3 + 2 * 2 = " + calculate("3+2*2" , new int[0]));
+    System.out.println("3/2  = " + calculate("3/2" , new int[0]));
+    System.out.println("3 + 5/2 = " + calculate(" 3 +5 / 2" , new int[0]));
+    System.out.println("1 + 1 = " + calculate("1 + 1" , new int[0]));
+    System.out.println("2 - 1 + 2 = "+ calculate("2 - 1 + 2" , new int[0]));
+    System.out.println("1 + (4 + 5 + 2) - 3 + (6 + 8) = " + calculate("1 + (4 + 5 + 2) - 3 + (6 + 8)" , new int[0]));
   }
 }
