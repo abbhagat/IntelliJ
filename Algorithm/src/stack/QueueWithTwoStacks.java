bdee00e85da900b1bdf12833a0d75cad546d1fcd
abbhagat <abhinaw.kumar7@gmail.com>
2026-01-02 19:43:40 +0530

    Interview

--- Algorithm/src/stack/QueueWithTwoStacks.java
+++ Algorithm/src/stack/QueueWithTwoStacks.java
@@ -7,6 +7,20 @@ public class QueueWithTwoStacks<E> {
   private final Stack<E> s1 = new Stack<>();
   private final Stack<E> s2 = new Stack<>();
 
+  void deQueue() {
+    while (!s1.isEmpty()) {
+      s2.push(s1.pop());
+    }
+    System.out.println(s2.pop());
+    while (!s2.isEmpty()) {
+      s1.push(s2.pop());
+    }
+  }
+
+  void enQueue(E e) {
+    s1.push(e);
+  }
+
   public static void main(String[] args) {
     QueueWithTwoStacks<Integer> q = new QueueWithTwoStacks<>();
     for (int i = 1; i <= 5; i++) {
@@ -21,19 +35,5 @@ public class QueueWithTwoStacks<E> {
     q.deQueue();
     System.out.println(q.s1);
     System.out.println(q.s2);
-  }
-
-  void deQueue() {
-    while (!s1.isEmpty()) {
-      s2.push(s1.pop());
-    }
-    System.out.println(s2.pop());
-    while (!s2.isEmpty()) {
-      s1.push(s2.pop());
-    }
-  }
-
-  void enQueue(E e) {
-    s1.push(e);
   }
 }
