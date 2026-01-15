cb2369383dbd486c2335bcfb4edca4ca20238352
abbhagat <abhinaw.kumar7@gmail.com>
2026-01-15 09:18:53 +0530

    Interview

--- Algorithm/src/geeksforgeeks/FormLargestNumberFromGivenArray.java
+++ Algorithm/src/geeksforgeeks/FormLargestNumberFromGivenArray.java
@@ -1,31 +1,29 @@
 package geeksforgeeks;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+// "^0+(?!$)" Remove leading zeros but keep one zero if the entire string is zeros
+// 0+ → One or more zeros
+// (?!$) → Negative lookahead
 
 public class FormLargestNumberFromGivenArray {
 
   public static String largestNumber(int[] a) {
     List<String> list = new ArrayList<>();
-    int countZero = 0;
-    for (int x : a) {
-      list.add(String.valueOf(x));
-      if (x == 0) {
-        countZero++;
-      }
-    }
-    if (countZero == a.length) {
-      return "0";
-    }
+    Arrays.stream(a).forEach(x -> list.add(String.valueOf(x)));
     list.sort((x, y) -> (y + x).compareTo(x + y));
-    StringBuilder result = new StringBuilder();
-    list.forEach(result::append);
-    return result.toString();
+    String s = String.join("", list);
+    s = s.replaceFirst("^0+(?!$)", "");
+    return s;
   }
 
   public static void main(String[] args) {
+    System.out.println(largestNumber(new int[]{0, 0, 0, 0, 0}));
     System.out.println(largestNumber(new int[]{3, 30, 34, 5, 9}));
+    System.out.println(largestNumber(new int[]{0, 1, 2, 3, 4}));
+    System.out.println(largestNumber(new int[]{4, 5, 0, 8}));
     System.out.println(largestNumber(new int[]{542, 60}));
-    System.out.println(largestNumber(new int[]{98, 9}));
   }
 }
--- Algorithm/src/geeksforgeeks/FormSmallestNumberFromGivenArray.java
+++ Algorithm/src/geeksforgeeks/FormSmallestNumberFromGivenArray.java
@@ -1,29 +1,26 @@
 package geeksforgeeks;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+// "^0+(?!$)" Remove leading zeros but keep one zero if the entire string is zeros
+// 0+ → One or more zeros
+// (?!$) → Negative lookahead
 
 public class FormSmallestNumberFromGivenArray {
 
   public static String smallestNumber(int[] a) {
     List<String> list = new ArrayList<>();
-    int countZero = 0;
-    for (int x : a) {
-      list.add(Integer.toString(x));
-      if (x == 0) {
-        countZero++;
-      }
-    }
-    if (countZero == a.length) {
-      return "0";
-    }
+    Arrays.stream(a).forEach(x -> list.add(String.valueOf(x)));
     list.sort((x, y) -> (x + y).compareTo(y + x));
-    StringBuilder result = new StringBuilder();
-    list.forEach(result::append);
-    return result.toString();
+    String s = String.join("", list);
+    s = s.replaceFirst("^0+(?!$)", "");
+    return s;
   }
 
   public static void main(String[] args) {
+    System.out.println(smallestNumber(new int[]{0, 0, 0, 0, 0}));
     System.out.println(smallestNumber(new int[]{3, 30, 34, 5, 9}));
     System.out.println(smallestNumber(new int[]{0, 1, 2, 3, 4}));
     System.out.println(smallestNumber(new int[]{4, 5, 0, 8}));
