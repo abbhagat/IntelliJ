dff02fed34de9623f2d33f855e832937a3ae2dfa
abbhagat <abhinaw.kumar7@gmail.com>
2026-01-15 07:59:04 +0530

    Interview

--- Algorithm/src/geeksforgeeks/BeautifulNumber.java
+++ Algorithm/src/geeksforgeeks/BeautifulNumber.java
@@ -6,34 +6,25 @@ import java.util.Set;
 public class BeautifulNumber {
 
   public static boolean isHappyNumber(int n) {
-    if (n == 1) {
-      return true;
-    }
     Set<Integer> set = new HashSet<>();
-    set.add(n);
-    int sum = 0;
-    while (sum != 1) {
-      sum = sum(n);
-      if (set.contains(sum)) {
-        return false;
-      }
-      set.add(sum);
-      n = sum;
+    while (n != 1 && set.add(n)) {
+      n = squareSum(n);
     }
-    return true;
+    return n == 1;
   }
 
-  private static int sum(int n) {
-    int sum = 0;
-    while (n != 0) {
-      int k = n % 10;
-      sum += k * k;
-      n /= 10;
+    private static int squareSum(int n) {
+      int sum = 0;
+      while (n != 0) {
+        int k = n % 10;
+        sum += k * k;
+        n /= 10;
+      }
+      return sum;
     }
-    return sum;
-  }
 
   public static void main(String[] args) {
+    System.out.println(isHappyNumber(19));
     System.out.println(isHappyNumber(31));
     System.out.println(isHappyNumber(32));
     System.out.println(isHappyNumber(33));
