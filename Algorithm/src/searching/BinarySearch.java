a9cb84e7d9ea2c129b07c26d9bcfd3edfc96d1c0
abbhagat <abhinaw.kumar7@gmail.com>
2026-02-01 11:25:43 +0530

    Interview

--- Algorithm/src/searching/BinarySearch.java
+++ Algorithm/src/searching/BinarySearch.java
@@ -1,21 +1,10 @@
 package searching;
 
 import java.util.Arrays;
-
 import static util.CommonUtils.printArray;
 
 // Time Complexity O(n log n)
 public class BinarySearch {
-
-  public static void main(String[] args) {
-    int[] a = {20, 3, 45, 15, 6, 19, 8, 2, 1, 35, 123};
-    printArray(a);
-    System.out.println(Arrays.binarySearch(a, 0, a.length - 1, 35));
-    Arrays.sort(a);
-    printArray(a);
-    int index = binarySearch(a, 0, a.length - 1, 35);
-    System.out.println(index == -1);
-  }
 
   public static int binarySearch(int[] a, int low, int high, int n) {
     if (low <= high) {
@@ -26,5 +15,12 @@ public class BinarySearch {
       return n < a[mid] ? binarySearch(a, low, mid - 1, n) : binarySearch(a, mid + 1, high, n);
     }
     return -1;
+  }
+
+  public static void main(String[] args) {
+    int[] a = {20, 3, 45, 15, 6, 19, 8, 2, 1, 35, 123};
+    Arrays.sort(a);
+    int index = binarySearch(a, 0, a.length - 1, 35);
+    System.out.println("Index : " + index);
   }
 }
