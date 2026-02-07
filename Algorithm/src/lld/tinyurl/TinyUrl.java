a79d561ac0bfc95d97308379bf911626933dadef
abbhagat <abhinaw.kumar7@gmail.com>
2026-02-07 16:49:00 +0530

    Interview

--- Algorithm/src/lld/tinyurl/TinyUrl.java
+++ Algorithm/src/lld/tinyurl/TinyUrl.java
@@ -9,14 +9,45 @@ public class TinyUrl {
   private final Map<Integer, String> indexToUrl;
   private final String BASE_62;
   private final String BASE_URL;
-  private int counter;
+  private int index;
 
   public TinyUrl() {
-    counter = 1;
+    index = 1;
     urlToIndex = new HashMap<>();
     indexToUrl = new HashMap<>();
     BASE_62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     BASE_URL = "http://www.tinyurl.com/";
+  }
+
+  private String encode(String longURL) {
+    if (!urlToIndex.containsKey(longURL)) {
+      urlToIndex.put(longURL, index);
+      indexToUrl.put(index, longURL);
+      index++;
+    }
+    return BASE_URL + base62Encode(urlToIndex.get(longURL));
+  }
+
+  private String base62Encode(int index) {
+    char[] c = new char[7];
+    int i;
+    for (i = 6; index != 0 && i >= 0; i--) {
+      c[i] = BASE_62.charAt(index % 62);     // Base-62 encoding (in reverse)
+      index /= 62;
+    }
+    while (i >= 0) {
+      c[i--] = '0';      // Fill remaining positions with '0'
+    }
+    return new String(c);
+  }
+
+  private String decode(String shortURL) {
+    int index = 0;
+    String base62EncodedCounterVal = shortURL.substring(shortURL.lastIndexOf("/") + 1);
+    for (char c : base62EncodedCounterVal.toCharArray()) {
+      index = index * 62 + BASE_62.indexOf(c);
+    }
+    return indexToUrl.get(index);
   }
 
   public static void main(String[] args) {
@@ -27,39 +58,5 @@ public class TinyUrl {
     System.out.println(shortURL);
     System.out.println(longURL);
     System.out.println(url.equals(longURL));
-  }
-
-  private String encode(String longURL) {
-    if (!urlToIndex.containsKey(longURL)) {
-      urlToIndex.put(longURL, counter);
-      indexToUrl.put(counter, longURL);
-      counter++;
-    }
-    return BASE_URL + base62Encode(urlToIndex.get(longURL));
-  }
-
-  private String base62Encode(int val) {
-    char[] c = new char[7];
-    int i = 6;
-    // Base-62 encoding (in reverse)
-    while (val > 0 && i >= 0) {
-      c[i] = BASE_62.charAt(val % 62);
-      i--;
-      val /= 62;
-    }
-    // Fill remaining positions with '0'
-    while (i >= 0) {
-      c[i--] = '0';
-    }
-    return new String(c);
-  }
-
-  private String decode(String shortURL) {
-    int counter = 0;
-    String base62EncodedCounterVal = shortURL.substring(shortURL.lastIndexOf("/") + 1);
-    for (char x : base62EncodedCounterVal.toCharArray()) {
-      counter = counter * 62 + BASE_62.indexOf(x);
-    }
-    return indexToUrl.get(counter);
   }
 }
