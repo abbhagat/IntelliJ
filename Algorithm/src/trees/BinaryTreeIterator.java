2b24b195f18ba66b242d79865b31b3417c99efdb
abbhagat <abhinaw.kumar7@gmail.com>
2026-02-08 21:04:34 +0530

    Interview

--- Algorithm/src/trees/BinaryTreeIterator.java
+++ Algorithm/src/trees/BinaryTreeIterator.java
@@ -1,7 +1,6 @@
 package trees;
 
 import lombok.Getter;
-
 import java.util.Stack;
 
 interface Iterable {
@@ -52,8 +51,8 @@ class TreeIterator implements Iterable {
 public class BinaryTreeIterator {
 
   public static void main(String[] args) {
-    Node root = new Node(2);
-    root.left = new Node(1);
+    Node root  = new Node(2);
+    root.left  = new Node(1);
     root.right = new Node(3);
     TreeIterator treeIterator = new TreeIterator();
     Stack<Node> stack = treeIterator.getStack();
--- Algorithm/src/trees/BinaryTreeMinimumHeight.java
+++ Algorithm/src/trees/BinaryTreeMinimumHeight.java
@@ -1,7 +1,5 @@
 package trees;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import static java.lang.Integer.min;
 
 /**
@@ -13,63 +11,25 @@ public class BinaryTreeMinimumHeight {
 
   private static int treeMinHeight(Node root) {
     if (root == null) {
-      return -1;
+      return 0;
     }
     int lH = treeMinHeight(root.left);
     int rH = treeMinHeight(root.right);
     return min(lH, rH) + 1;
   }
 
-  public static int minDepth(Node root) {
-    Queue<Node> q = new LinkedList<>();
-    q.add(root);
-    q.add(null);
-    int height = 0;
-    while (!q.isEmpty()) {
-      Node node = q.poll();
-      if (node == null) {
-        height++;
-      }
-      if (node != null) {
-        if (node.left == null && node.right == null) {
-          return height;
-        }
-        if (node.left != null) {
-          q.add(node.left);
-        }
-        if (node.right != null) {
-          q.add(node.right);
-        }
-      } else if (!q.isEmpty()) {
-        q.add(null);
-      }
-    }
-    return 0;
-  }
-
   public static void main(String[] args) {
-            /*
-                    1
-                  /   \
-                2      3
-              /  \    /  \
-             4    5  6    7
-                         / \
-                        8   9
-                             \
-                              10
+      /*
+                 1
+               /   \
+             2      3
+           /
+          4
        */
     Node root                    = new Node(1);
     root.left                    = new Node(2);
     root.right                   = new Node(3);
     root.left.left               = new Node(4);
-    root.left.right              = new Node(5);
-    root.right.left              = new Node(6);
-    root.right.right             = new Node(7);
-    root.right.right.left        = new Node(8);
-    root.right.right.right       = new Node(9);
-    root.right.right.right.right = new Node(10);
-    System.out.println("Tree Min Depth : " + minDepth(root));
     System.out.println("Tree Min Depth : " + treeMinHeight(root));
   }
 }
