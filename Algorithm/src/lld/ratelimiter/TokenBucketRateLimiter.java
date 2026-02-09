9f37219dfd8bbf7d257658082bede470fe164335
abbhagat <abhinaw.kumar7@gmail.com>
2026-02-10 05:23:04 +0530

    Interview

--- Algorithm/src/lld/ratelimiter/TokenBucketRateLimiter.java
+++ Algorithm/src/lld/ratelimiter/TokenBucketRateLimiter.java
@@ -2,9 +2,12 @@ package lld.ratelimiter;
 
 public class TokenBucketRateLimiter {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     RateLimiter limiter = new RateLimiterImpl(5, 1);
     for (int i = 0; i < 10; i++) {
+      if (i == 5 || i == 9) {
+        Thread.sleep(2000);
+      }
       System.out.println("Request " + i + " allowed: " + limiter.allowRequest("user123")
       );
     }
