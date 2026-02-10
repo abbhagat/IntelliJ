3f69e89152e5418d89a07ca1d587fd75dd9f3d8e
abbhagat <abhinaw.kumar7@gmail.com>
2026-02-10 17:50:16 +0530

    Interview

--- Algorithm/src/lld/paymentsystem/PaymentSystemImpl.java
+++ Algorithm/src/lld/paymentsystem/PaymentSystemImpl.java
@@ -5,13 +5,13 @@ import java.util.Map;
 public class PaymentSystemImpl {
 
   public static void main(String[] args) {
-    PaymentRequest request = new PaymentRequest();
-    request.setTransactionId("TXN123");
-    request.setAmount(500);
-    request.setPaymentMethod(PaymentMethod.UPI);
-    request.setPaymentDetails(Map.of("upiId", "abc@upi"));
+    PaymentRequest paymentRequest = new PaymentRequest();
+    paymentRequest.setTransactionId("TXN123");
+    paymentRequest.setAmount(500);
+    paymentRequest.setPaymentMethod(PaymentMethod.UPI);
+    paymentRequest.setPaymentDetails(Map.of("upiId", "abc@upi"));
     PaymentGateway gateway = new PaymentGateway();
-    PaymentResponse response = gateway.processPayment(request);
+    PaymentResponse response = gateway.processPayment(paymentRequest);
     System.out.println(response);
   }
 }
