4038b1ae9064402eae447156ddc622546965cf24
abbhagat <abhinaw.kumar7@gmail.com>
2026-02-12 08:15:58 +0530

    Interview

--- Algorithm/src/lld/apigateway/ApiGatewayImpl.java
+++ Algorithm/src/lld/apigateway/ApiGatewayImpl.java
@@ -1,5 +1,11 @@
 package lld.apigateway;
 
+/**
+ * "API Gateway is a centralized entry point that handles authentication, rate limiting, routing, and load balancing.
+ * Internally, it consists of a Router, AuthManager, RateLimiter, and LoadBalancer.
+ * It forwards validated requests to microservices and returns aggregated responses."
+ *
+ */
 public class ApiGatewayImpl {
 
   public static void main(String[] args) {
