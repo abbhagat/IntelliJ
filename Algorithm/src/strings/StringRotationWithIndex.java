c29143e89484a9173122caa62a4e85971156d80a
abbhagat <abhinaw.kumar7@gmail.com>
2026-02-14 23:33:49 +0530

    Interview

--- Algorithm/src/strings/StringRotationWithIndex.java
+++ Algorithm/src/strings/StringRotationWithIndex.java
@@ -16,23 +16,9 @@ public class StringRotationWithIndex {
     return sb;
   }
 
-  private static String rotateString(String str, int start, int end) {
-    String s1 = str.substring(0, end);
-    String s3 = str.substring(start + 1);
-    String s2 = str.substring(end, start + 1);
-    return s1 + new StringBuilder(s2).reverse() + s3;
-  }
-
-  private static StringBuilder rotateStr(String s, int start, int end) {
-    return new StringBuilder()
-        .append(s, 0, end)
-        .append(new StringBuilder(s.substring(end, start + 1)).reverse())
-        .append(s.substring(start + 1));
-  }
-
   public static void main(String[] args) {
     String s = "abcde";
-    System.out.println(rotate(s, 3, 1) + "\t" + rotateString(s, 3, 1));
-    System.out.println(rotate(s, 4, 2) + "\t" + rotateString(s, 4, 2));
+    System.out.println(rotate(s, 3, 1));
+    System.out.println(rotate(s, 4, 2));
   }
 }
