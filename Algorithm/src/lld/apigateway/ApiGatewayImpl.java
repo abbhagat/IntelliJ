3d9b845672427bd0fbff4bdeaf20bf14662ebf33
abbhagat <abhinaw.kumar7@gmail.com>
2026-02-24 09:52:06 +0530

    Interview

--- Algorithm/src/lld/apigateway/ApiGatewayImpl.java
+++ Algorithm/src/lld/apigateway/ApiGatewayImpl.java
@@ -1,5 +1,8 @@
 package lld.apigateway;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * "API Gateway is a centralized entry point that handles authentication, rate limiting, routing, and load balancing.
  * Internally, it consists of a Router, AuthManager, RateLimiter, and LoadBalancer.
@@ -9,8 +12,19 @@ package lld.apigateway;
 public class ApiGatewayImpl {
 
   public static void main(String[] args) {
-    ApiGateway apiGateway = null;
+
+    Router router = new Router();
+    AuthenticationManager authManager = new AuthenticationManager();
+    RateLimiter rateLimiter = new RateLimiter();
+    Service userService = new UserService();
+    Service orderService = new UserService();
+    LoadBalancer loadBalancer = new LoadBalancer(List.of(userService, orderService));
+    router.registerRoute("/user", userService);
+    router.registerRoute("/order", orderService);
+    ApiGateway apiGateway = new ApiGateway(router, authManager, rateLimiter, loadBalancer);
     Request request = new Request();
+    request.setHeaders(Map.of("Authorization", "valid-token", "clientId", "abhinawb"));
+    request.setPath("/user");
     Response response = apiGateway.handleRequest(request);
     System.out.println(response);
   }
--- Algorithm/src/lld/apigateway/Response.java
+++ Algorithm/src/lld/apigateway/Response.java
@@ -1,14 +1,12 @@
 package lld.apigateway;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Response {
   private int statusCode;
   private String responseBody;
