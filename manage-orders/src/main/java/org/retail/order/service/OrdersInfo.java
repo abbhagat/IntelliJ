72621d34d7993741a920d55a8930c7e43d74a26d
abbhagat <abhinaw.kumar7@gmail.com>
2026-03-03 11:28:45 +0530

    Interview

--- manage-orders/src/main/java/org/retail/order/service/OrdersInfo.java
+++ manage-orders/src/main/java/org/retail/order/service/OrdersInfo.java
@@ -8,6 +8,16 @@ import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Fallback is used to:
+ *  1. Return default response
+ *  2. Log error
+ *  3. Store data temporarily
+ *  4. Avoid crashing system
+ *
+ */
+
+
 @Service
 public class OrdersInfo {
 
@@ -17,10 +27,10 @@ public class OrdersInfo {
   @HystrixCommand(
       fallbackMethod = "saveFallbackOrders",
       commandProperties = {
-          @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
-          @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
-          @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
-          @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "50000")
+          @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"), // If manage-supplier does not respond within 5 seconds Call is aborted and Fallback method is triggered
+          @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),          // Minimum 5 requests must occur before circuit breaker evaluates errors.
+          @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),      // If 50% of requests fail, circuit opens.
+          @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "50000")  // Circuit stays OPEN for 50 seconds. No real calls are made, All requests go directly to fallback After 50 seconds: Hystrix allows 1 test request (Half-Open state) If success → circuit closes If fail → circuit opens again
       },
       threadPoolKey = "ordersInfoThreadPoolKey",
       threadPoolProperties = {
@@ -29,7 +39,7 @@ public class OrdersInfo {
       }
   )
   public void saveSupplier(HttpEntity<Supplier> httpEntity) {
-    restTemplate.postForObject("http://manage-supplier/supplier/saveSupplier", httpEntity, Supplier.class);
+    restTemplate.postForObject("http://manage-supplier/supplier/saveSupplier", httpEntity, Supplier.class); // calls another microservice If it fails or is slow → fallback method is triggered
   }
 
   public void saveFallbackOrders(HttpEntity<Supplier> httpEntity) {
