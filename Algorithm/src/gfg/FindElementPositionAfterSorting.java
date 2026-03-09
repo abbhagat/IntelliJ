c08a1ff854987a7ac3e9fda6b5c6bdcd2e4fadb3
abbhagat <abhinaw.kumar7@gmail.com>
2026-03-10 04:46:42 +0530

    Interview

--- Algorithm/src/gfg/FindElementPositionAfterSorting.java
+++ Algorithm/src/gfg/FindElementPositionAfterSorting.java
@@ -8,22 +8,22 @@ package gfg;
 // Time Complexity: O(n)
 public class FindElementPositionAfterSorting {
 
-  private static int findElementPositionAfterSorting(int[] a, int pos) {
+  private static void findElementPositionAfterSorting(int[] a, int pos) {
     int count = 0, n = a[pos];
     for (int x : a) {
       if (x < n) {  // Find the count of elements in the array which are <= n
         count++;
       }
     }
-    return count;
+    System.out.println("Position Index " + count);
   }
 
   public static void main(String[] args) {
     int[] a = {4, 5, 1, 2, 4};
-    System.out.println("Position Index " + findElementPositionAfterSorting(a, 0));
-    System.out.println("Position Index " + findElementPositionAfterSorting(a, 1));
-    System.out.println("Position Index " + findElementPositionAfterSorting(a, 2));
-    System.out.println("Position Index " + findElementPositionAfterSorting(a, 3));
-    System.out.println("Position Index " + findElementPositionAfterSorting(a, 4));
+    findElementPositionAfterSorting(a, 0);
+    findElementPositionAfterSorting(a, 1);
+    findElementPositionAfterSorting(a, 2);
+    findElementPositionAfterSorting(a, 3);
+    findElementPositionAfterSorting(a, 4);
   }
 }
