71b46eaded92d8e9d6c63c9c3079d4bc7755bda1
abbhagat <abhinaw.kumar7@gmail.com>
2026-03-10 14:01:39 +0530

    Interview

--- Algorithm/src/adobe/SortCharacterByFreqInString.java
+++ Algorithm/src/adobe/SortCharacterByFreqInString.java
@@ -1,6 +1,8 @@
 package adobe;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 public class SortCharacterByFreqInString {
@@ -11,10 +13,10 @@ public class SortCharacterByFreqInString {
     str.chars().forEach(c -> map.put((char) c, map.getOrDefault((char) c, 0) + 1));
     Map<Character, Integer> finalMap = new LinkedHashMap<>();
     map.entrySet()
-       .stream()
-       .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
-       .forEachOrdered(e-> finalMap.put(e.getKey(), e.getValue()));
-    finalMap.forEach((k,v) -> IntStream.range(0, v).forEach(i -> sb.append(k)));
+        .stream()
+        .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
+        .forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
+    finalMap.forEach((k, v) -> IntStream.range(0, v).forEach(i -> sb.append(k)));
     return sb.toString();
   }
 
