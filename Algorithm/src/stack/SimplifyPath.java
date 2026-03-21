70548f8a2b7f5e836777b2df13d70c46ce71e1a9
abbhagat <abhinaw.kumar7@gmail.com>
2026-03-21 10:31:11 +0530

    Interview

--- Algorithm/src/stack/SimplifyPath.java
+++ Algorithm/src/stack/SimplifyPath.java
@@ -5,18 +5,18 @@ import java.util.Stack;
 // Time Complexity: O(n)
 public class SimplifyPath {
 
-  private static String simplifyPath(String path) {
+  private static void simplifyPath(String path) {
     Stack<String> stack = new Stack<>();
-    for (String s : path.split("/", -1)) {
-      if (s.isEmpty() || s.equals(".")) {
+    for (String str : path.split("/")) {
+      if (str.isEmpty() || str.equals(".")) {
         continue;
       }
-      if (s.equals("..")) {
+      if (str.equals("..")) {
         if (!stack.isEmpty()) {
           stack.pop();
         }
       } else {
-        stack.push(s);
+        stack.push(str);
       }
     }
     StringBuilder sb = new StringBuilder();
@@ -24,13 +24,13 @@ public class SimplifyPath {
       sb.insert(0, stack.pop())
         .insert(0, "/");
     }
-    return sb.length() == 0 ? "/" : sb.toString();
+    System.out.println(sb.length() == 0 ? "/" : sb);
   }
 
   public static void main(String[] args) {
-    System.out.println(simplifyPath("/home"));
-    System.out.println(simplifyPath("/a/./b/../../c/"));
-    System.out.println(simplifyPath("/a/../../b/../c//.//"));
-    System.out.println(simplifyPath("/a//b////c/d//././/.."));
+    simplifyPath("/home");
+    simplifyPath("/a/./b/../../c/");
+    simplifyPath("/a/../../b/../c//.//");
+    simplifyPath("/a//b////c/d//././/..");
   }
 }
