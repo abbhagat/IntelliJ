068ac6cb8254df8cc6b77acf58db1e852d0fb15c
abbhagat <abhinaw.kumar7@gmail.com>
2026-03-22 13:36:03 +0530

    Interview

--- Algorithm/src/graph/MinKnightMovesInInfiniteChessBoard.java
+++ Algorithm/src/graph/MinKnightMovesInInfiniteChessBoard.java
@@ -3,6 +3,7 @@ package graph;
 import java.util.HashMap;
 import java.util.Map;
 import static java.lang.Integer.min;
+import static java.lang.Math.abs;
 
 // Time  Complexity : O(x × y)
 // Space Complexity : O(x × y)
@@ -10,7 +11,7 @@ public class MinKnightMovesInInfiniteChessBoard {
 
   private static final Map<String, Integer> map = new HashMap<>();  // Stores already computed results
 
-  private static int dfs(int x, int y) {
+  private static int minMoves(int x, int y) {
     String key = x + "," + y;
     if (map.containsKey(key)) {
       return map.get(key);
@@ -20,14 +21,14 @@ public class MinKnightMovesInInfiniteChessBoard {
     } else if (x + y == 2) {  // This handles tricky cases like: (1,1) (0,2) (2,0) from these coordinates we can move to origin in 2 steps
       return 2;
     } else {
-      int n = 1 + min(dfs(Math.abs(x - 1), Math.abs(y - 2)), dfs(Math.abs(x - 2), Math.abs(y - 1)));  // Chessboard is symmetric in all quadrants, So we only solve for first quadrant
+      int n = 1 + min(minMoves(abs(x - 1), abs(y - 2)), minMoves(abs(x - 2), abs(y - 1)));  // Chessboard is symmetric in all quadrants, So we only solve for first quadrant
       map.put(key, n);
       return n;
     }
   }
 
   private static int minKnightMoves(int x, int y) {
-    return dfs(x, y);
+    return minMoves(x, y);
   }
 
   public static void main(String[] args) {
