2fb9b59bd6d2569871a28a48ba2c225a9140b633
abbhagat <abhinaw.kumar7@gmail.com>
2026-03-22 13:59:14 +0530

    Interview

--- Algorithm/src/games/DeckOfCards.java
+++ Algorithm/src/games/DeckOfCards.java
@@ -1,14 +1,15 @@
 package games;
 
+import java.util.Random;
 import static util.CommonUtils.swap;
 
 public class DeckOfCards {
 
-  private static final String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
   private static final String[] RANKS = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
-  private static final int N = SUITS.length * RANKS.length;
-
-  private static final String[] deck = new String[N];
+  private static final String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
+  private static final int          N = SUITS.length * RANKS.length;
+  private static final String[] deck  = new String[N];
+  private static final Random random  = new Random();
 
   public static void main(String[] args) {
     for (int i = 0; i < RANKS.length; i++) {
@@ -17,7 +18,7 @@ public class DeckOfCards {
       }
     }
     for (int i = 0; i < N; i++) {
-      int j = i + (int) (Math.random() * (N - i));
+      int j = i + random.nextInt(N - i);
       swap(deck, i, j);
     }
     for (String s : deck) {
