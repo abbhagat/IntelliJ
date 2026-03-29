4aedfe607c04a025c6339ff4f9c9114b226c28df
abbhagat <abhinaw.kumar7@gmail.com>
2026-03-29 14:06:26 +0530

    Interview

--- Algorithm/src/jpmorgan/MaxMinWithMinComparison.java
+++ Algorithm/src/jpmorgan/MaxMinWithMinComparison.java
@@ -14,7 +14,7 @@ public class MaxMinWithMinComparison {
     } else {
       i = 1;
     }
-    while (i + 1 < a.length) {
+    while (i < a.length - 1) {
       if (a[i] > a[i + 1]) {
         max = max(max, a[i]);
         min = min(min, a[i + 1]);
