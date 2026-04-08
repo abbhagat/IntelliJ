5aa08f3f25c9125ef6f9e9ea58df2def7b314faf
abbhagat <abhinaw.kumar7@gmail.com>
2026-04-08 07:24:04 +0530

    Interview

--- Algorithm/src/lld/threadpool/ThreadPoolImpl.java
+++ Algorithm/src/lld/threadpool/ThreadPoolImpl.java
@@ -4,7 +4,7 @@ public class ThreadPoolImpl {
 
   public static void main(String[] args) {
     ThreadPool threadPool = new ThreadPool(5);
-    for (int i = 0; i < 10; i++) {
+    for (int i = 1; i <= 10; i++) {
       final int task = i;
       Runnable runnable = () -> System.out.println(Thread.currentThread().getName() + " is executing task " + task);
       threadPool.addTaskToQueue(runnable);
