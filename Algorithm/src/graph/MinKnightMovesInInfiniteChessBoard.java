f796cc813a245fbdb4f113ee9a01505765dd80d3
abbhagat <abhinaw.kumar7@gmail.com>
2026-04-08 06:00:51 +0530

    Interview

--- Algorithm/src/graph/MinKnightMovesInInfiniteChessBoard.java
+++ Algorithm/src/graph/MinKnightMovesInInfiniteChessBoard.java
@@ -21,7 +21,9 @@ public class MinKnightMovesInInfiniteChessBoard {
     } else if (x + y == 2) {  // This handles tricky cases like: (1,1) (0,2) (2,0) from these coordinates we can move to origin in 2 steps
       return 2;
     } else {
-      int n = 1 + min(minMoves(abs(x - 1), abs(y - 2)), minMoves(abs(x - 2), abs(y - 1)));  // Chessboard is symmetric in all quadrants, So we only solve for first quadrant
+      int move1 = minMoves(abs(x - 1), abs(y - 2));
+      int move2 = minMoves(abs(x - 2), abs(y - 1));
+      int n = 1 + min(move1, move2);  // Chessboard is symmetric in all quadrants, So we only solve for first quadrant
       map.put(key, n);
       return n;
     }
