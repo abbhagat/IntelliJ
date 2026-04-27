dc9b8e94643eb8d39b0a70ecbf358a3fa02d16e8
abbhagat <abhinaw.kumar7@gmail.com>
2026-04-27 11:36:12 +0530

    Interview

--- Algorithm/src/adobe/FirstNonRepeatingCharInAStream.java
+++ Algorithm/src/adobe/FirstNonRepeatingCharInAStream.java
@@ -1,12 +1,27 @@
 package adobe;
 
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 import java.util.stream.Stream;
 
 // Time  Complexity : O(n)
 // Space Complexity : O(n)
 public class FirstNonRepeatingCharInAStream {
+
+  private static void firstNoRepeatingChars(char[] a) {
+    Map<Character, Integer> map = new LinkedHashMap<>();
+    for (char x : a) {
+      map.put(x, map.getOrDefault(x, 0) + 1);
+    }
+    for (Map.Entry<Character, Integer> entrySet : map.entrySet()) {
+      if (entrySet.getValue() == 1) {
+        System.out.print(entrySet.getKey() + "  ");
+        break;
+      }
+    }
+  }
 
   private static void firstNonRepeatingChar(Stream<Character> stream) {
     int[] temp = new int[128];
@@ -19,10 +34,12 @@ public class FirstNonRepeatingCharInAStream {
       }
       System.out.println(q.isEmpty() ? -1 : String.valueOf(q.peek()));
     });
+    System.out.println();
   }
 
   public static void main(String[] args) {
     firstNonRepeatingChar(Stream.of('a', 'a', 'b', 'b', 'c', 'd'));
     firstNonRepeatingChar(Stream.of('a', 'b', 'b', 'c', 'c', 'e'));
+    firstNoRepeatingChars(new char[]{'a', 'b', 'b', 'c', 'c', 'e'});
   }
 }
