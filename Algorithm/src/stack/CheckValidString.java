4d1487c660ccf2a31fd40d47a4208eed3758e191
abbhagat <abhinaw.kumar7@gmail.com>
2026-05-22 12:01:04 +0530

    Interview

--- Algorithm/src/stack/CheckValidString.java
+++ Algorithm/src/stack/CheckValidString.java
@@ -15,15 +15,6 @@ import static java.lang.Integer.max;
  * [0, 1, 2, 3] for '(**';
  * [0, 1, 2, 3, 4] for '(***';
  * [0, 1, 2, 3] for '(***)'.
- * Furthermore, we can prove these states always form a contiguous interval.
- * Thus, we only need to know the left and right bounds of this interval.
- * That is, we would keep those intermediate states described above as [lo, hi] = [1, 1], [0, 2], [0, 3], [0, 4], [0, 3].
- * Algorithm
- * Let min, max respectively be the smallest and largest possible number of open left brackets after processing the current character in the string.
- * If we encounter a left bracket (c == '('), then min++, otherwise we could write a right bracket, so min--.
- * If we encounter what can be a left bracket (c != ')'), then hi++, otherwise we must write a right bracket, so hi--.
- * If hi < 0, then the current prefix can't be made valid no matter what our choices are.
- * Also, we can never have less than 0 open left brackets. At the end, we should check that we can have exactly 0 open left brackets.
  */
 
 public class CheckValidString {
