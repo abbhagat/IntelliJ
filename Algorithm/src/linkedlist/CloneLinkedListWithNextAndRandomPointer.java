c6dd263ea8869d88d6ce0d0a96e911a58648d10f
abbhagat <abhinaw.kumar7@gmail.com>
2026-06-13 07:42:27 +0530

    Interview

--- Algorithm/src/linkedlist/CloneLinkedListWithNextAndRandomPointer.java
+++ Algorithm/src/linkedlist/CloneLinkedListWithNextAndRandomPointer.java
@@ -1,5 +1,9 @@
 package linkedlist;
 
+import java.util.Set;
+
+import static linkedlist.TraverseList.traverseList;
+
 /**
 Create the copy of node 1 and insert it between node 1 & node 2 in the original Linked List,
 create a copy of 2 and insert it between 2 & 3. Continue in this fashion, add the copy of N after the Nth node
@@ -15,35 +19,46 @@ Ensure that original->next is NULL and return the cloned list
 public class CloneLinkedListWithNextAndRandomPointer {
 
   private static Node clone(Node head) {
+    if (head == null) {
+      return null;
+    }
+    // Step 1: Insert cloned nodes between original nodes
     Node temp = head;
     while (temp != null) {
       Node node = new Node(temp.num);  // insert additional node after every node of original list
       node.next = temp.next;
       temp.next = node;
+      temp      = node.next;
+    }
+
+    // Step 2: Set random pointers
+    temp = head;
+    while (temp != null) {
+      if (temp.random != null) {
+        temp.next.random = temp.random.next;  // Since every copied node is immediately after its original node.
+      }
       temp = temp.next.next;
     }
+    // Step 3: Separate the two lists
     temp = head;
-    // adjust the random pointers of the newly added nodes
     while (temp != null) {
-      if (temp.next != null) {
-        temp.next.random = temp.random != null ? temp.random.next : null;
+      Node copy = temp.next;
+      temp.next = copy.next;
+      if (copy.next != null) {
+        copy.next = copy.next.next;
       }
-      temp = temp.next.next; // move to the next newly added node by skipping an original node
-    }
-    temp = head;
-    Node copy = head.next;
-    Node node = head.next;  // save the head of copied linked list and separate the original list and copied list
-    while (temp != null) {
-      temp.next = temp.next.next;
-      copy.next = copy.next != null ? copy.next.next : null;
       temp = temp.next;
-      copy = copy.next;
     }
-    return node;
+    return head.next;
   }
 
   public static void main(String[] args) {
-    Node node = clone(new Node(10));
-    System.out.println(node.num);
+    Node node      = new Node(1);
+    node.next      = new Node(2);
+    node.next.next = new Node(3);
+    node.random = node.next.next;
+    node.next.random = node;
+    node.next.next.random = node.next;
+    traverseList(clone(node));
   }
 }
