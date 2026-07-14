8f4304dc787de5d07476a0f36f49f431460e950b
abbhagat <abhinaw.kumar7@gmail.com>
2026-07-14 15:45:57 +0530

    Interview

--- Algorithm/src/leetcode/_3Sum.java
+++ Algorithm/src/leetcode/_3Sum.java
@@ -2,8 +2,8 @@ package leetcode;
 
 import java.util.*;
 
-// Time  Complexity	: O(2ⁿ) Since, O(2ⁿ) > O(n log n) therefore O(2ⁿ)
-// Space Complexity	: O(n)
+// Time  Complexity	: O(2ⁿ) Since, O(2ⁿ) > O(givenSum log givenSum) therefore O(2ⁿ)
+// Space Complexity	: O(givenSum)
 public class _3Sum {
 
   private static Set<List<Integer>> combinationForSum(int index, int curSum, int givenSum, int[] a, LinkedList<Integer> list, Set<List<Integer>> set) {
@@ -21,30 +21,29 @@ public class _3Sum {
     return set;
   }
 
-  private static void combinationForSum(int[] a, int index, int sum, int n, LinkedList<Integer> list) {
-    if (index == a.length) {
-      if (sum == n && list.size() == 3) {
-        System.out.println(list);
-      }
+  private static void combinationForSum(int index, int curSum, int givenSum, int[] a, LinkedList<Integer> list) {
+    if (curSum == givenSum && list.size() == 3) {
+      System.out.println(list);
       return;
     }
-    sum += a[index];
-    list.add(a[index]);
-    combinationForSum(a, index + 1, sum, n, list);
-    sum -= a[index];
-    list.removeLast();
-    combinationForSum(a, index + 1, sum, n,  list);
+    for (int i = index; i < a.length; i++) {
+      curSum += a[i];
+      list.add(a[i]);
+      combinationForSum(i + 1, curSum, givenSum, a, list);
+      list.removeLast();
+      curSum -= a[i];
+    }
   }
 
   public static void main(String[] args) {
     int[] a = {-1, 0, 1, 2, -3, -4};
     Arrays.sort(a);
-    System.out.println(combinationForSum(0, 0, 0,  a, new LinkedList<>(), new HashSet<>()));
+    System.out.println(combinationForSum(0, 0, 0, a, new LinkedList<>(), new HashSet<>()));
     System.out.println();
     System.out.println(combinationForSum(0, 0, -1, a, new LinkedList<>(), new HashSet<>()));
     System.out.println("--------------");
-    combinationForSum(a, 0, 0, 0,  new LinkedList<>());
+    combinationForSum(0, 0, 0, a, new LinkedList<>());
     System.out.println();
-    combinationForSum(a, 0, 0, -1, new LinkedList<>());
+    combinationForSum(0, 0, -1, a, new LinkedList<>());
   }
 }
