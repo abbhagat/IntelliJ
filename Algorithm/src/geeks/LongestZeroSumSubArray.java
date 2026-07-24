7c10639daabdf6ee976b0c116f2d5fcfa2cb29d3
abbhagat <abhinaw.kumar7@gmail.com>
2026-07-24 19:50:39 +0530

    Interview

--- Algorithm/src/geeks/LongestZeroSumSubArray.java
+++ Algorithm/src/geeks/LongestZeroSumSubArray.java
@@ -10,19 +10,19 @@ public class LongestZeroSumSubArray {
 
   private static int longestZeroSumSubArray(int[] a, int n) {
     Map<Integer, Integer> map = new HashMap<>();  // to store key as sum and the value as index i
-    int sum = 0, maxLen = 0;
+    int sum = 0, max = 0;
     for (int i = 0; i < a.length; i++) {
       sum += a[i];
       if (sum == n) {
-        maxLen = i + 1;
+        max = i + 1;
       }
       if (map.containsKey(sum)) {
-        maxLen = max(maxLen, i - map.get(sum));
+        max = max(max, i - map.get(sum));
       } else {
         map.put(sum, i);
       }
     }
-    return maxLen;
+    return max;
   }
 
   public static void main(String[] args) {
