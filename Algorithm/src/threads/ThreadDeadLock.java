0d2d9823709e74a04b16e8a05e628878b4abfc29
abbhagat <abhinaw.kumar7@gmail.com>
2026-07-25 11:29:51 +0530

    Interview

--- Algorithm/src/threads/ThreadDeadLock.java
+++ Algorithm/src/threads/ThreadDeadLock.java
@@ -5,6 +5,26 @@ public class ThreadDeadLock {
   private static final Object LOCK_1 = new Object();
   private static final Object LOCK_2 = new Object();
 
+  Runnable r1 = () -> {
+    synchronized (LOCK_1) {
+      System.out.println("Thread 1: Holding LOCK_1");
+      sleep();
+      synchronized (LOCK_2) {
+        System.out.println("Thread 1: Holding LOCK_2");
+      }
+    }
+  };
+
+  Runnable r2 = () -> {
+    synchronized (LOCK_2) {
+      System.out.println("Thread 2: Holding LOCK_2");
+      sleep();
+      synchronized (LOCK_1) {
+        System.out.println("Thread 2: Holding LOCK_1");
+      }
+    }
+  };
+
   private void sleep() {
     try {
       Thread.sleep(100);
@@ -14,30 +34,8 @@ public class ThreadDeadLock {
   }
 
   private void simulateDeadlock() {
-
-    Runnable r1 = () -> {
-      synchronized (LOCK_1) {
-        System.out.println("Thread 1: Holding LOCK_1");
-        sleep();
-        synchronized (LOCK_2) {
-          System.out.println("Thread 1: Holding LOCK_2");
-        }
-      }
-    };
-
-    Runnable r2 = () -> {
-      synchronized (LOCK_2) {
-        System.out.println("Thread 2: Holding LOCK_2");
-        sleep();
-        synchronized (LOCK_1) {
-          System.out.println("Thread 2: Holding LOCK_1");
-        }
-      }
-    };
-
     Thread t1 = new Thread(r1, "Thread-1");
     Thread t2 = new Thread(r2, "Thread-2");
-
     t1.start();
     t2.start();
   }
