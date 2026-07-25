f3f233f7aef514cd1bdd3fb7e4048188bda504f7
abbhagat <abhinaw.kumar7@gmail.com>
2026-07-25 22:01:35 +0530

    Interview

---
+++ Algorithm/src/stack/BalancedBracketMinSwaps.java
@@ -0,0 +1,24 @@
+package stack;
+
+public class BalancedBracketMinSwaps {
+
+  public static int minSwaps(String s) {
+    int open = 0, swaps = 0;
+    for (char c : s.toCharArray()) {
+      open += c == '(' ? 1 : -1;
+      if (open < 0) {  // More ')' than '(' encountered
+        swaps++;
+        open = 1;
+      }
+    }
+    return swaps;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(minSwaps("))(("));       // 1
+    System.out.println(minSwaps("()"));         // 0
+    System.out.println(minSwaps("(()())"));     // 0
+    System.out.println(minSwaps(")))(("));      // 2
+    System.out.println(minSwaps("()))(("));     // 1
+  }
+}
