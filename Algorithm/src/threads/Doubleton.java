ee1208ac9cc8f3625649942b9a718fb0e91d1169
abbhagat <abhinaw.kumar7@gmail.com>
2026-07-25 11:04:15 +0530

    Interview

--- Algorithm/src/threads/Doubleton.java
+++ Algorithm/src/threads/Doubleton.java
@@ -2,59 +2,29 @@ package threads;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.concurrent.atomic.AtomicInteger;
 
-public class Doubleton implements Cloneable, Serializable {
+public final class Doubleton implements Serializable, Cloneable {
 
-  @Serial
-  private static final long serialVersionUID = 1L;
+  private static final Doubleton INSTANCE1 = new Doubleton(0);
+  private static final Doubleton INSTANCE2 = new Doubleton(1);
+  private static int index;
+  private final int id;
 
-  private static Doubleton instance1, instance2;
-  private static volatile int index;
-
-  private Doubleton() {
-    if (instance1 != null && instance2 != null) {
-      throw new RuntimeException("Can't instantiate doubleton twice");
-    }
+  private Doubleton(int id) {
+    this.id = id;
   }
 
   public static synchronized Doubleton getInstance() {
-    instance1 = (null == instance1) ? new Doubleton() : instance1;
-    instance2 = (null == instance2) ? new Doubleton() : instance2;
-    return index++ % 2 == 0 ? instance1 : instance2;
+    return ((index++ & 1) == 0) ? INSTANCE1 : INSTANCE2;
   }
 
   @Serial
-  public Object readResolve() {
-    return getInstance();
+  private Object readResolve() {
+    return id == 0 ? INSTANCE1 : INSTANCE2;
   }
 
   @Override
-  public Object clone() {
-    return getInstance();
-  }
-
-  public static void main(String[] args) {
-    designpattern.Doubleton obj1 = designpattern.Doubleton.getInstance();
-    designpattern.Doubleton obj2 = designpattern.Doubleton.getInstance();
-    designpattern.Doubleton obj3 = designpattern.Doubleton.getInstance();
-    designpattern.Doubleton obj4 = designpattern.Doubleton.getInstance();
-    designpattern.Doubleton obj5 = designpattern.Doubleton.getInstance();
-    designpattern.Doubleton obj6 = designpattern.Doubleton.getInstance();
-    System.out.println("Printing the hashcode to check memory addresses");
-    System.out.println("obj1.hashCode() : " + obj1.hashCode());
-    System.out.println("obj3.hashCode() : " + obj3.hashCode());
-    System.out.println("obj5.hashCode() : " + obj5.hashCode());
-    System.out.println("obj2.hashCode() : " + obj2.hashCode());
-    System.out.println("obj4.hashCode() : " + obj4.hashCode());
-    System.out.println("obj6.hashCode() : " + obj6.hashCode());
-    System.out.println("Printing the Object's Attributes");
-    System.out.println("obj1 : " + obj1);
-    System.out.println("obj2 : " + obj2);
-    System.out.println("obj3 : " + obj3);
-    System.out.println("obj4 : " + obj4);
-    System.out.println("obj5 : " + obj5);
-    System.out.println("obj6 : " + obj6);
-    System.gc();
+  protected Object clone() throws CloneNotSupportedException {
+    throw new CloneNotSupportedException();
   }
 }
