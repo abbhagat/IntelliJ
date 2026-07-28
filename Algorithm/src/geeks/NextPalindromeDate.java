30d7550b2c9f92d734cc1b3e0a1d17eada7b7aa2
abbhagat <abhinaw.kumar7@gmail.com>
2026-07-28 10:43:23 +0530

    Interview

--- Algorithm/src/geeks/NextPalindromeDate.java
+++ Algorithm/src/geeks/NextPalindromeDate.java
@@ -1,11 +1,8 @@
 package geeks;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Date;
 
 // Time Complexity : O(N) If the next palindrome date is N days
 // Space Complexity: O(1)
