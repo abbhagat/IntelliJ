08ac4ce3cf0bee386b74bd8b6b44f55ce9119500
abbhagat <abhinaw.kumar7@gmail.com>
2026-07-28 10:45:27 +0530

    Interview

--- Algorithm/src/geeks/NextPalindromeDate.java
+++ Algorithm/src/geeks/NextPalindromeDate.java
@@ -3,20 +3,20 @@ package geeks;
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import static util.CommonUtils.isPalindrome;
 
 // Time Complexity : O(N) If the next palindrome date is N days
 // Space Complexity: O(1)
 public class NextPalindromeDate {
 
-  private static final DateTimeFormatter INPUT_FORMAT  = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+  private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
   private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("yyyyMMdd");
 
   private static String nextPalindromeDate(String inputDate) {
     LocalDate date = LocalDate.parse(inputDate, INPUT_FORMAT);
     while (true) {
       String dateStr = date.format(OUTPUT_FORMAT);
-      StringBuilder revDateStr = new StringBuilder(dateStr).reverse();
-      if (dateStr.contentEquals(revDateStr)) {
+      if (isPalindrome(dateStr)) {
         return dateStr;
       }
       date = date.plusDays(1);
