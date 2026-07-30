0dcf2f265251ea7904039a71cee1b73c54c973a6
abbhagat <abhinaw.kumar7@gmail.com>
2026-07-30 09:46:19 +0530

    Interview

--- Algorithm/src/lld/shoppingcart/PricingService.java
+++ Algorithm/src/lld/shoppingcart/PricingService.java
@@ -4,12 +4,12 @@ public class PricingService {
 
   private static final double TAX_RATE = 0.1; // 10%
 
-  public double calculateTotal(ShoppingCart cart, DiscountStrategy discountStrategy) {
-    double subtotal = cart.getCartItemMap()
-                          .values()
-                          .stream()
-                          .mapToDouble(CartItem::getTotalPrice)
-                          .sum();
+  public double calculateTotal(ShoppingCart shoppingCart, DiscountStrategy discountStrategy) {
+    double subtotal = shoppingCart.getCartItemMap()
+                                  .values()
+                                  .stream()
+                                  .mapToDouble(CartItem::getTotalPrice)
+                                  .sum();
     double discounted = discountStrategy.applyDiscount(subtotal);
     return discounted + (discounted * TAX_RATE);
   }
