ed2bcf297cc327dea085cbaa3c523863a5bc7295
abbhagat <abhinaw.kumar7@gmail.com>
2026-07-31 19:18:40 +0530

    Interview

--- Algorithm/src/adobe/LongestSubsequenceSubstring.java
+++ Algorithm/src/adobe/LongestSubsequenceSubstring.java
@@ -7,31 +7,30 @@ public class LongestSubsequenceSubstring {
   public static int longestSubsequenceSubstring(String s1, String s2) {
     int max = 0;
     for (int i = 0; i < s2.length(); i++) {
-      StringBuilder sb = new StringBuilder();
+      String s = "";
       for (int j = i; j < s2.length(); j++) {
-        sb.append(s2.charAt(j));
-        if (isSubsequence(s1, sb)) {
-          max = max(max, sb.length());
+        s += s2.charAt(j);
+        if (isSubsequence(s1, s)) {
+          max = max(max, s.length());
         }
       }
     }
     return max;
   }
 
-  private static boolean isSubsequence(String s, CharSequence c) {
-    int i = 0, j = 0;
-    while (i < s.length() && j < c.length()) {
-      if (s.charAt(i) == c.charAt(j)) {
+  private static boolean isSubsequence(String s1, String s2) {
+    int j = 0;
+    for (int i = 0; i < s1.length() && j < s2.length(); i++) {
+      if (s1.charAt(i) == s2.charAt(j)) {
         j++;
       }
-      i++;
     }
-    return j == c.length();
+    return j == s2.length();
   }
 
   public static void main(String[] args) {
-    System.out.println(longestSubsequenceSubstring("abcd", "abdc"));      //3
-    System.out.println(longestSubsequenceSubstring("abcde", "ace"));       //2 ("ce")
+    System.out.println(longestSubsequenceSubstring("abcd", "abdc"));       //3
+    System.out.println(longestSubsequenceSubstring("abcde", "ace"));       //3 ("ace")
     System.out.println(longestSubsequenceSubstring("abcdef", "zabxy"));    //2 ("ab")
     System.out.println(longestSubsequenceSubstring("abc", "xyz"));         //0
     System.out.println(longestSubsequenceSubstring("banana", "ananas"));   //5 ("anana")
