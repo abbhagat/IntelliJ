5afe2e82a5fe13356d0bba38bb8e5c6840034f33
abbhagat <abhinaw.kumar7@gmail.com>
2026-08-01 00:03:42 +0530

    Interview

--- Algorithm/src/lld/restaurant/BookingService.java
+++ Algorithm/src/lld/restaurant/BookingService.java
@@ -1,18 +1,19 @@
 package lld.restaurant;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class BookingService {
 
-  private final ReservationService repository;
+  private final ReservationService reservationService;
 
   // One lock for each table
-  private final ConcurrentHashMap<String, ReentrantLock> locks = new ConcurrentHashMap<>();
+  private final Map<String, ReentrantLock> locks = new ConcurrentHashMap<>();
 
   public BookingService(ReservationService reservationService) {
-    this.repository = reservationService;
+    this.reservationService = reservationService;
   }
 
   public Booking book(Table table, String customer, LocalDateTime start, LocalDateTime end) {
@@ -20,13 +21,13 @@ public class BookingService {
     ReentrantLock lock = locks.computeIfAbsent(key, keyId -> new ReentrantLock());
     lock.lock();  // The code below this will not execute for same Table booking because lock is acquired by another Thread
     try {
-      for (Booking booking : repository.getReservations()) {
+      for (Booking booking : reservationService.getReservations()) {
         if (booking.table().tableId().contentEquals(table.tableId()) && overlap(start, end, booking.startTime(), booking.endTime())) {
           throw new RuntimeException("Table already booked");
         }
       }
-      Booking booking = new Booking(repository.getReservations().size() + 1, table, customer,  start, end);
-      repository.save(booking);
+      Booking booking = new Booking(reservationService.getReservations().size() + 1, table, customer,  start, end);
+      reservationService.save(booking);
       return booking;
     } finally {
       lock.unlock();
