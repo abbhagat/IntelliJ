6e63c2945974494041e4055651e36391218fc1bd
abbhagat <abhinaw.kumar7@gmail.com>
2026-08-01 19:16:28 +0530

    Interview

--- Algorithm/src/doublylinkedlist/CovertDLLToBST.java
+++ Algorithm/src/doublylinkedlist/CovertDLLToBST.java
@@ -8,15 +8,15 @@ public class CovertDLLToBST {
 
   private static Node head;
 
-  private static Node sortedListToBST(int n) {                // n is the total no of nodes in the LinkedList
+  private static Node sortedListToBST(int n) {          // n is the total no of nodes in the LinkedList
     if (n <= 0) {
       return null;
     }
-    Node left = sortedListToBST(n / 2);               // Recursively construct the left subtree
-    Node root = head;                                   // head_ref now refers to middle node, make middle node as root of BST
-    root.prev = left;                                  // Set pointer to left subtree
+    Node left = sortedListToBST(n / 2);            // Recursively construct the left subtree
+    Node root = head;                                 // head now refers to middle node, make middle node as root of BST
+    root.prev = left;                                 // Set pointer to left subtree
     head = head.next;                                // Change head pointer of Linked List for parent recursive calls
-    Node right = sortedListToBST(n - n / 2 - 1);  // Recursively construct the right subtree and link it with root.
+    Node right = sortedListToBST(n - n / 2 - 1); // Recursively construct the right subtree and link it with root.
     root.next = right;
     return root;                                    // The number of nodes in right subtree  is total nodes - nodes in left subtree - 1 (for root)
   }
