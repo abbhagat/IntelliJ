1a97c2ccf5650d4dffd09f49d00c161d99c57b58
abbhagat <abhinaw.kumar7@gmail.com>
2026-08-05 20:57:26 +0530

    Interview

--- Algorithm/src/gfg/ThrottlingGateway.java
+++ Algorithm/src/gfg/ThrottlingGateway.java
@@ -6,17 +6,15 @@ import java.util.List;
 public class ThrottlingGateway {
 
   public static long droppedRequests(List<Integer> requestTime) {
-    long dropped = 0;
+    long count = 0;
     for (int i = 0; i < requestTime.size(); i++) {
-      if (i >= 3 && requestTime.get(i) == requestTime.get(i - 3)) {
-        dropped++;  // If the current request has the same timestamp as the request 3 positions before it, then there are already 3 requests in that second.
-      } else if (i >= 20 && requestTime.get(i) - requestTime.get(i - 20) < 10) {
-        dropped++; // If the current request and the request 20 positions before it are less than 10 seconds apart, then this is the 21st request within a 10-second window.
-      } else if (i >= 60 && requestTime.get(i) - requestTime.get(i - 60) < 60) {  // If the current request and the request 60 positions before it are less than 60 seconds apart, then this is the 61st request within a 60-second window.
-        dropped++; // If the current request and the request 60 positions before it are less than 60 seconds apart, then this is the 61st request within a 60-second window.
+      if     (i >=  3 && requestTime.get(i) == requestTime.get(i - 3)          // If the current request has the same timestamp as the request 3 positions before it, then there are already 3 requests in that second window.
+          || (i >= 20 && requestTime.get(i) -  requestTime.get(i - 20) < 10)   // If the current request and the request 20 positions before it are less than 10 seconds apart, then this is the 21st request within a 10-second window.
+          || (i >= 60 && requestTime.get(i) -  requestTime.get(i - 60) < 60)) {// If the current request and the request 60 positions before it are less than 60 seconds apart, then this is the 61st request within a 60-second window.
+        count++;
       }
     }
-    return dropped;
+    return count;
   }
 
   public static void main(String[] args) {
