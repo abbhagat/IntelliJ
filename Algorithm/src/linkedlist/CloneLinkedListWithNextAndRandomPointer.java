f1636a6beb1d5c6dbe4d1ce16327ad9c936ec8eb
abbhagat <abhinaw.kumar7@gmail.com>
2026-08-12 19:25:08 +0530

    Interview

--- Algorithm/src/linkedlist/CloneLinkedListWithNextAndRandomPointer.java
+++ Algorithm/src/linkedlist/CloneLinkedListWithNextAndRandomPointer.java
@@ -25,11 +25,11 @@ public class CloneLinkedListWithNextAndRandomPointer {
       temp = temp.next.next;
     }
     // Step 3: Separate the two lists
-    temp = head;
-    Node clone = head.next;
+    temp       = head;
+    Node clone = temp.next;
     while (temp != null) {
       Node copy = temp.next;
-      temp.next = copy.next;
+      temp.next = temp.next.next;
       if (copy.next != null) {
         copy.next = copy.next.next;
       }
@@ -39,11 +39,11 @@ public class CloneLinkedListWithNextAndRandomPointer {
   }
 
   public static void main(String[] args) {
-    Node node      = new Node(1);
-    node.next      = new Node(2);
-    node.next.next = new Node(3);
-    node.random = node.next.next;
-    node.next.random = node;
+    Node node             = new Node(1);
+    node.next             = new Node(2);
+    node.next.next        = new Node(3);
+    node.random           = node.next.next;
+    node.next.random      = node;
     node.next.next.random = node.next;
     traverseList(clone(node));
   }
