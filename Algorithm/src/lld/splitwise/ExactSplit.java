80ba0e4d3b235079f89fad044ddec0d4769360ae
abbhagat <abhinaw.kumar7@gmail.com>
2026-08-14 06:43:31 +0530

    Interview

--- Algorithm/src/lld/splitwise/ExactSplit.java
+++ Algorithm/src/lld/splitwise/ExactSplit.java
@@ -4,16 +4,13 @@ public class ExactSplit extends Split {
 
   @Override
   public void validateExpense(Expense expense) {
-
     if (expense.getSplits() == null || expense.getSplits().isEmpty()) {
       throw new RuntimeException("Exact split amounts are required");
     }
-
     double total = expense.getSplits()
-        .stream()
-        .mapToDouble(Split::getAmount)
-        .sum();
-
+                          .stream()
+                          .mapToDouble(Split::getAmount)
+                          .sum();
     if (Double.compare(total, expense.getAmount()) != 0) {
       throw new RuntimeException("Invalid Exact Split");
     }
--- Algorithm/src/lld/splitwise/PercentSplit.java
+++ Algorithm/src/lld/splitwise/PercentSplit.java
@@ -3,24 +3,21 @@ package lld.splitwise;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 public class PercentSplit extends Split {
 
-  @Getter
-  @Setter
   private double percent;
 
   @Override
   public void validateExpense(Expense expense) {
-
     if (expense.getSplits() == null || expense.getSplits().isEmpty()) {
       throw new RuntimeException("Percentage splits are required");
     }
-
     double totalPercent = expense.getSplits()
-        .stream()
-        .mapToDouble(split -> ((PercentSplit) split).getPercent())
-        .sum();
-
+                                 .stream()
+                                 .mapToDouble(split -> ((PercentSplit) split).getPercent())
+                                 .sum();
     if (Double.compare(totalPercent, 100.0) != 0) {
       throw new RuntimeException("Percentages must add up to 100");
     }
@@ -28,14 +25,9 @@ public class PercentSplit extends Split {
 
   @Override
   public void splitExpense(Expense expense) {
-
     for (Split split : expense.getSplits()) {
-
       PercentSplit percentSplit = (PercentSplit) split;
-
-      double amount =
-          expense.getAmount() * percentSplit.getPercent() / 100;
-
+      double amount = expense.getAmount() * percentSplit.getPercent() / 100;
       percentSplit.setAmount(amount);
     }
   }
