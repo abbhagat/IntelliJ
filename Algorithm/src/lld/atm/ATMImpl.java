c255ef7795d79720de30ef2828bbe034d998c1e9
abbhagat <abhinaw.kumar7@gmail.com>
2026-08-20 08:50:37 +0530

    Interview

--- Algorithm/src/lld/atm/ATMImpl.java
+++ Algorithm/src/lld/atm/ATMImpl.java
@@ -5,18 +5,26 @@ package lld.atm;
 public class ATMImpl {
 
   public static void main(String[] args) {
-    ATMService ATMService = new ATMService();
+
+    ATMService atmService = new ATMService();
+
     ATMCard ATMCard = new ATMCard("1234", 1234);
+
     Account account = new Account("ACC-1", 10000);
-    ATMService.addAccount(ATMCard.getCardNumber(), account);
-    ATM atm = new ATM(ATMService);
+
+    atmService.addAccount(ATMCard.getCardNumber(), account);
+
+    ATM atm = new ATM(atmService);
+
     atm.insertCard(ATMCard);
+
     if (atm.enterPin(1234)) {
       System.out.println("Balance: " + atm.checkBalance());
       atm.withdraw(2000);
       atm.deposit(500);
       System.out.println("Balance: " + atm.checkBalance());
     }
+
     atm.ejectCard();
   }
 }
