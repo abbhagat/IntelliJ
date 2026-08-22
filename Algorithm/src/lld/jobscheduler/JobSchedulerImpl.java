251002bec816860ac4231e5482fe19a469d3dba8
abbhagat <abhinaw.kumar7@gmail.com>
2026-08-22 09:26:53 +0530

    Interview

--- Algorithm/src/lld/jobscheduler/JobSchedulerImpl.java
+++ Algorithm/src/lld/jobscheduler/JobSchedulerImpl.java
@@ -3,19 +3,24 @@ package lld.jobscheduler;
 public class JobSchedulerImpl {
 
   public static void main(String[] args) throws InterruptedException {
-    JobScheduler scheduler = new JobScheduler();
-    // Normal job
-    scheduler.schedule(new Job("job-1",() -> System.out.println("Job 1 executed"), System.currentTimeMillis() + 3000,0));
-    // Job with retry
-    scheduler.schedule(new Job("job-2", () -> {System.out.println("Job 2 executed"); throw new RuntimeException("Job failed");},System.currentTimeMillis() + 1000, 2));
-    // Job that we will cancel
-    scheduler.schedule(new Job("job-3", () -> System.out.println( "Job 3 executed"), System.currentTimeMillis() + 5000,0));
-    scheduler.cancel("job-3");
-    // Check status
+    Job job       = new Job("job-1",() -> System.out.println("Job 1 executed"), System.currentTimeMillis() + 3000,0);
+    Job retryJob  = new Job("job-2", () -> {System.out.println("Job 2 executed"); throw new RuntimeException("Job failed");},System.currentTimeMillis() + 1000, 2);
+    Job cancelJob = new Job("job-3", () -> System.out.println( "Job 3 executed"), System.currentTimeMillis() + 5000,0);
+
+    JobScheduler jobScheduler = new JobScheduler();
+
+    jobScheduler.schedule(job);
+    jobScheduler.schedule(retryJob);
+    jobScheduler.schedule(cancelJob);
+
+    jobScheduler.cancel("job-3");
+
     Thread.sleep(4000);
-    System.out.println("Job 1: " + scheduler.getStatus("job-1"));
-    System.out.println("Job 2: " + scheduler.getStatus("job-2"));
-    System.out.println("Job 3: " + scheduler.getStatus("job-3"));
-    scheduler.shutdown();
+
+    System.out.println("Job 1: " + jobScheduler.getStatus("job-1"));
+    System.out.println("Job 2: " + jobScheduler.getStatus("job-2"));
+    System.out.println("Job 3: " + jobScheduler.getStatus("job-3"));
+
+    jobScheduler.shutdown();
   }
 }
