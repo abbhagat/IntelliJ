c3990e409f66be11d10a448bd0eda46111642af5
abbhagat <abhinaw.kumar7@gmail.com>
2026-08-24 12:00:40 +0530

    Interview

--- Algorithm/src/lld/threadpool/ThreadPool.java
+++ Algorithm/src/lld/threadpool/ThreadPool.java
@@ -11,8 +11,8 @@ class ThreadPool implements IThreadPool {
   private volatile boolean isStopped;
 
   public ThreadPool(int poolSize) {
-    this.queue   = new LinkedBlockingQueue<>();
-    this.threads = new Thread[poolSize];
+    this.queue     = new LinkedBlockingQueue<>();
+    this.threads   = new Thread[poolSize];
     this.isStopped = false;
     for (int i = 0; i < poolSize; i++) {
       this.threads[i] = new Worker(queue);
@@ -25,14 +25,14 @@ class ThreadPool implements IThreadPool {
     if (this.isStopped) {
       throw new IllegalStateException("ThreadPool is stopped");
     }
-    this.queue.offer(task);
+    queue.offer(task);
   }
 
   // A hint to the scheduler that the current thread is willing to yield its current use of a processor.
   // The scheduler is free to ignore this hint.
   @Override
   public void awaitCompletion() {
-    while (!this.queue.isEmpty()) {
+    while (!queue.isEmpty()) {
       Thread.yield();
     }
   }
