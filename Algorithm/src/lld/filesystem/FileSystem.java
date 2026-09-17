e43c6f57a189a9da7fcb6f0128df38360229c6f8
abbhagat <abhinaw.kumar7@gmail.com>
2026-09-17 09:48:06 +0530

    Interview

--- Algorithm/src/lld/filesystem/FileSystem.java
+++ Algorithm/src/lld/filesystem/FileSystem.java
@@ -16,6 +16,26 @@ public class FileSystem {
     traversePath(path);
   }
 
+  public List<String> ls(String path) {
+    FileSystemNode node = traversePath(path);
+    if (node.isDirectory()) {
+      Directory directory = (Directory) node;
+      return directory.getAllFileSystemNodes()
+                      .stream()
+                      .map(FileSystemNode::getName)
+                      .collect(Collectors.toList());
+    }
+    return List.of(node.getName());
+  }
+
+  public void delete(String path) {
+    FileSystemNode node = traversePath(path);
+    if (node == root) {
+      throw new IllegalArgumentException("Cannot delete root");
+    }
+    node.getParent().remove(node.getName());
+  }
+
   public void createFile(String path) {
     String[] parts      = Arrays.stream(path.split("/")).filter(s -> !s.isEmpty()).toArray(String[]::new);
     Directory parentDir = getParentDir(parts);
@@ -73,26 +93,6 @@ public class FileSystem {
   public String read(String path) {
     File file = (File) traversePath(path);
     return file.read();
-  }
-
-  public List<String> ls(String path) {
-    FileSystemNode node = traversePath(path);
-    if (node.isDirectory()) {
-      Directory directory = (Directory) node;
-      return directory.getAllFileSystemNodes()
-                      .stream()
-                      .map(FileSystemNode::getName)
-                      .collect(Collectors.toList());
-    }
-    return List.of(node.getName());
-  }
-
-  public void delete(String path) {
-    FileSystemNode node = traversePath(path);
-    if (node == root) {
-      throw new IllegalArgumentException("Cannot delete root");
-    }
-    node.getParent().remove(node.getName());
   }
 
 }
