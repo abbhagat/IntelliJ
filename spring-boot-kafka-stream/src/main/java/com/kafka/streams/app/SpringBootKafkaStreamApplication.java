4d15d01f8ff09498c2488b65cd72e0043ef23b28
abbhagat <abhinaw.kumar7@gmail.com>
2026-09-26 13:13:38 +0530

    Interview

---
+++ spring-boot-kafka-stream/src/main/java/com/kafka/streams/app/SpringBootKafkaStreamApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.streams.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaStreamApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootKafkaStreamApplication.class, args);
+  }
+
+}
---
+++ spring-boot-kafka-stream/src/test/java/com/kafka/streams/app/SpringBootKafkaStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.streams.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaStreamApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
